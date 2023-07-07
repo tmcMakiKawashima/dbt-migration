709969a7240559e25694619856a23df078c72c57
fumihito.miura_ctc <fumihito.miura@ctc-g.co.jp>
2023-07-07 17:30:58 +0900

    update LDTS for snapshot

--- snapshots/BD/valuechain/scd_tbdaihl.sql
+++ snapshots/BD/valuechain/scd_tbdaihl.sql
@@ -5,7 +5,7 @@
         unique_key="KYOUHAN ||'-'|| HATUHINB ||'-'|| HATUMKBN ||'-'|| CHUMON ||'-'|| ODERSYU ||'-'|| USERCD ||'-'|| KAISYA ||'-'|| TCHUMON ||'-'|| HATTYUHI",
 
         strategy='timestamp',
-        updated_at='LDTS',
+        updated_at='LDTSB',
         invalidate_hard_deletes=True,
     )
 }}
