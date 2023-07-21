8be56498fd1cda580eebb9b17e78e32c032a587d
Yoshifumi Ueda <da-ueda-04.00@dajp1-dxpf-ws-bstn2.co.jp>
2023-07-21 17:28:48 +0900

    国内新生産実績データマートmodel作成

---
+++ models/DD/supplydemand/stg/stg_st_hanbaikt_frmkbn.sql
@@ -0,0 +1,8 @@
+with stg_st_hanbaikt_frmkbn as (
+  select
+    *,
+    RANK() over (partition by MD5_HANBAIKT_LINK order by LDTS desc) aggkey
+  from {{source('supplydemand_db_public','raw_st_hanbaikt_frmkbn')}}
+)
+select * from stg_st_hanbaikt_frmkbn
+where aggkey = 1
---
+++ models/DD/supplydemand/stg/stg_st_jpsalecar_frmkbn.sql
@@ -0,0 +1,8 @@
+with stg_st_jpsalecar_frmkbn as (
+  select
+    *,
+    RANK() over (partition by MD5_JPSALECAR_HUB order by LDTS desc) aggkey
+  from {{source('supplydemand_db_public','raw_st_jpsalecar_frmkbn')}}
+)
+select * from stg_st_jpsalecar_frmkbn
+where aggkey = 1
