0cdb9ab8545b8da257ce0e3a678425819bf8902e
Yoshifumi Ueda <da-ueda-04.00@dajp1-dxpf-ws-bstn2.co.jp>
2023-07-25 16:19:48 +0900

    新車明細データマートmodelのコミット漏れ対応

---
+++ models/DE/PARTS/dim/dm_sinsya_meisai.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='table') }}
+
+with dm_sinsya_meisai as(
+  select
+    ST_MD5_SINSYAODNO_LINK,
+    HKATA,
+    FLNO,
+    TSHAMEIA,
+    TSHAMEIB,
+    TSHAMEIC,
+    TSHAMEID,
+    HDKB,
+    YOUTOKBN,
+    FTDD,
+    HIYTDD,
+    TOYTDD,
+    TODD,
+    NSQBDY,
+    NSDD,
+    CUNO,
+    EDANO,
+    YM
+  from {{ref('tmp20_dm_sinsya_meisai')}}
+  where rnk = 1
+)
+select * from dm_sinsya_meisai
