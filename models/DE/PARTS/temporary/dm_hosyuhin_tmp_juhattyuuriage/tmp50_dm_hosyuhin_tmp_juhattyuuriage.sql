3feca463501d15b35d6b9f166edde2f781aa5623
Kuboi Erika <e-kuboi@toyotasystems.com>
2023-10-31 20:20:46 +0900

    コード微修正

--- models/DE/PARTS/temporary/dm_hosyuhin_tmp_juhattyuuriage/tmp50_dm_hosyuhin_tmp_juhattyuuriage.sql
+++ models/DE/PARTS/temporary/dm_hosyuhin_tmp_juhattyuuriage/tmp50_dm_hosyuhin_tmp_juhattyuuriage.sql
@@ -1,16 +1,15 @@
-{# TAS納期管理ファイルの仕様確定次第追加 #}
 with
-    temp40 as (select * from {{ ref("tmp40_DM_HOSYUHIN_TMP_JUHATTYUURIAGE") }}),
-    atopshimuke as (select * from {{ ref("stg_DVNP054A") }}),
-    atopjuchu as (select * from {{ ref("stg_O_CVS11JYUCHUU") }}),
-    hinban as (select * from {{ ref("stg_DM_PNO") }}),
-    tasshimuke as (select * from {{ ref("stg_tbsmksk") }}),
+    temp40 as (select * from {{ ref('tmp40_dm_hosyuhin_tmp_juhattyuuriage') }}),
+    atopjuchu as (select * from {{ ref('stg_o_cvs11jyuchuu') }}),
+    hinban as (select * from {{ ref('stg_dm_pno') }}),
+    tasshimuke as (select * from {{ ref('stg_tbsmksk') }}),
     -- 納期仕向
     tasshimuke_noki as (
         select * from {{ ref("stg_tbsmksk_noki") }} where deletedate = '00000000'
     ),
     --
-    tbuserm as (select * from {{ ref("stg_tbuserm") }})
+    atopshimuke as (select * from {{ ref('stg_dvnp054a') }}),
+    tbuserm as (select * from {{ ref('stg_tbuserm') }})
 select
     temp40.*,
     tbuserm.hanbaiten,
@@ -22,51 +21,44 @@ select
     -- 納期仕向
     case
         when
-
             (
-                (
-                    (substr(temp40.chumon, 1, 1) = 'P' or substr(temp40.chumon, 1, 1) = 'Q')
-                    and not (noki.kyouhan is null)
-                    and temp40.odrsbetu = '11'
-                    and (noki.hikizaikbn = '1' or noki.hikizaikbn = '3')
+                ( -- 注文No上１桁が’P'または’Q'かつ納期仕向先テーブルが結合されている
+                    (substr(temp40.chumon, 1, 1) in ('P', 'Q')) and (noki.kyouhan is not null)
+                  -- オーダー種別が’11’かつ納期仕向先テーブルの引当・在補区分が’1’または’3’
+                    and temp40.odrsbetu = '11' and (noki.hikizaikbn in ('1','3'))
                 )
-                or (
-                    (substr(temp40.chumon, 1, 1) = 'P' or substr(temp40.chumon, 1, 1) = 'Q')
-                    and not (noki.kyouhan is null)
-                    and temp40.odrsbetu = '12'
-                    and (noki.hikizaikbn = '2' or noki.hikizaikbn = '3')
+                or 
+                ( -- 注文No上１桁が’P'または’Q'かつ納期仕向先テーブルが結合されている
+                    (substr(temp40.chumon, 1, 1) in ('P', 'Q')) and (noki.kyouhan is not null)
+                  -- オーダー種別が’12’かつ納期仕向先テーブルの引当・在補区分が’2’または’3’
+                    and temp40.odrsbetu = '12' and (noki.hikizaikbn in ('2', '3'))
                 )
             )
-        then iff(noki.nokismkskcd is null, '', noki.nokismkskcd)
-        else iff(tasshimuke.sishacd is null, '', tasshimuke.sishacd)
+        then iff(noki.nokismkskcd is null, '', noki.nokismkskcd) -- 納期仕向先テーブル.納期仕向先コード
+        else iff(tasshimuke.sishacd is null, '', tasshimuke.sishacd) -- 仕向先テーブル.支社コード
     end as sishacd
-from temp40
-left outer join atopjuchu on temp40.hinban = atopjuchu.jhinban
-left outer join hinban on temp40.hinban = hinban.pno
-left outer join
-    tasshimuke
-    on temp40.kyouhan = tasshimuke.kyouhan
-    and temp40.nyukkten = tasshimuke.kyoten
-left outer join
-    atopshimuke
-    on trim(
-        concat(
-            tasshimuke.kyouhan, iff(tasshimuke.sishacd is null, '', tasshimuke.sishacd)
-        )
-    )
-    = atopshimuke.dlrcd
-left outer join
-    tbuserm on temp40.kyouhan = tbuserm.kyouhan and temp40.usercd = tbuserm.usrcod
--- 納期仕向
-left outer join
-    tasshimuke_noki noki
-    on temp40.kyouhan = noki.kyouhan
-    and temp40.nyukkten = noki.nyukkten
-    and temp40.makercd = noki.makercd
-    and case
-        when temp40.makercd = '70000'
-        then temp40.ktenkbn = noki.ktenkbn
-        else '' = noki.ktenkbn
-    end
     --
-    
+from temp40
+    left outer join atopjuchu
+        on temp40.hinban = atopjuchu.jhinban
+    left outer join hinban
+        on temp40.hinban = hinban.pno
+    left outer join tasshimuke
+        on temp40.kyouhan = tasshimuke.kyouhan
+        and temp40.nyukkten = tasshimuke.kyoten
+    left outer join atopshimuke
+        on concat(
+            tasshimuke.kyouhan, tasshimuke.sishacd
+        ) = atopshimuke.dlrcd
+    left outer join tbuserm
+        on temp40.kyouhan = tbuserm.kyouhan
+        and temp40.usercd = tbuserm.usrcod
+    -- 納期仕向
+    left outer join
+        tasshimuke_noki noki
+        on temp40.kyouhan = noki.kyouhan
+        and temp40.nyukkten = noki.nyukkten
+        and temp40.makercd = noki.makercd
+        and iff(temp40.makercd = '70000', temp40.ktenkbn, '') 
+            = noki.ktenkbn -- 輸送区分
+    --
