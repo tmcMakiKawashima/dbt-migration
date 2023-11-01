cab5b279284fb74539a0c796977d821fed050a34
Watanabe Mina <m-watanabe@toyotasystems.com>
2023-11-01 20:50:47 +0900

    仕向先マスタ改修

--- models/DE/PARTS/temporary/nyusyukko_x_shimuke.sql
+++ models/DE/PARTS/temporary/nyusyukko_x_shimuke.sql
@@ -1,47 +1,88 @@
 with nyusyukko as (
     select
-        KYOUHAN KYOUHAN_NYUKO, -- 共販店コード
-        HINBAN HINBAN_NYUKO, -- 品番
-        DENNOJ DENNOJ_NYUKO, -- 伝票№ 自拠点(イシュ№）
-        SUM(SYUKAJ) NYUKOSU, -- 更新数  自拠点出荷処理数
-        MAX(
-            LPAD(rtrim("SDATE-Y"),length("SDATE-Y"),'0') ||
-            LPAD(rtrim("SDATE-M"),length("SDATE-M"),'0') ||
-            LPAD(rtrim("SDATE-D"),length("SDATE-D"),'0') 
-           ) NYUKOYMD, -- 処理日付
-        LPAD(rtrim("JDATE-Y"),length("JDATE-Y"),'0') ||
-        LPAD(rtrim("JDATE-M"),length("JDATE-M"),'0') ||
-        LPAD(rtrim("JDATE-D"),length("JDATE-D"),'0') 
-         JDATE_NYUKO, -- 受注日
-        UKEKTEN UKEKTEN_NYUKO, -- 受入拠点
-        RIGHT(REMARK2, 5) CHUMON_NO_NYUKO --リマーク２ ※右から5桁目
+        kyouhan kyouhan_nyuko, -- 共販店コード
+        hinban hinban_nyuko, -- 品番
+        dennoj dennoj_nyuko, -- 伝票№ 自拠点(イシュ№）
+        sum(syukaj) nyukosu, -- 更新数  自拠点出荷処理数
+        max(
+            lpad(rtrim("SDATE-Y"),length("SDATE-Y"),'0') ||
+            lpad(rtrim("SDATE-M"),length("SDATE-M"),'0') ||
+            lpad(rtrim("SDATE-D"),length("SDATE-D"),'0') 
+           ) nyukoymd, -- 処理日付
+        lpad(rtrim("JDATE-Y"),length("JDATE-Y"),'0') ||
+        lpad(rtrim("JDATE-M"),length("JDATE-M"),'0') ||
+        lpad(rtrim("JDATE-D"),length("JDATE-D"),'0') 
+         jdate_nyuko, -- 受注日
+        ukekten ukekten_nyuko, -- 受入拠点
+        right(remark2, 5) chumon_no_nyuko, --リマーク２ ※右から5桁目
+        substr(remark2, 1, 2) ordersyu_nyuko, --リマーク２　※先頭2桁
+        makercd --メーカーコード
     from {{ ref('stg_tbnsyus') }} -- 入出庫ファイル
-    where GDENK in ('71', '72', '73', '74', '75', '78') -- 外部伝区
-    and DSYUBETS = '6' -- データ種別:内部伝区
+    where gdenk in ('71', '72', '73', '74', '75', '78') -- 外部伝区
+    and dsyubets = '6' -- データ種別:内部伝区
     group by
-            KYOUHAN, -- 共販店コード
-            HINBAN, -- 品番
-            DENNOJ, -- 伝票№ 自拠点(イシュ№）
-            JDATE_NYUKO , -- 受注日
-            CHOKSO, -- 一般直送区分
-            UKEKTEN, -- 受入拠点
-            CHUMON_NO_NYUKO -- リマーク２ ※右から5桁目
+            kyouhan, -- 共販店コード
+            hinban, -- 品番
+            dennoj, -- 伝票№ 自拠点(イシュ№）
+            jdate_nyuko , -- 受注日
+            chokso, -- 一般直送区分
+            ukekten, -- 受入拠点
+            chumon_no_nyuko, -- リマーク２ ※右から5桁目
+            ordersyu_nyuko, --リマーク２ ※先頭2桁
+            makercd --メーカーコード
 ),
 shimuke as (
     select * from {{ref('stg_tbsmksk')}} -- 仕向先テーブル
-    )
+    ),
+shimuke_noki as (
+    select * from {{ref('stg_tbsmksk_noki')}}
+    where deletedate = '00000000'
+)
 select 
-     nyusyukko.KYOUHAN_NYUKO -- 共販店コード
-    ,nyusyukko.HINBAN_NYUKO -- 品番
-    ,nyusyukko.DENNOJ_NYUKO -- 伝票№ 自拠点(イシュ№）
-    ,nyusyukko.NYUKOSU -- 入庫数
-    ,nyusyukko.NYUKOYMD -- 入庫日
-    ,nyusyukko.JDATE_NYUKO -- 受注日
-    ,nyusyukko.CHUMON_NO_NYUKO --リマーク２ ※右から5桁目
-    -- 支社コード ※[入出庫ファイル]共販店コード　＋　[TAS仕向先テーブル]支社コード
-    ,nyusyukko.KYOUHAN_NYUKO || shimuke.SISHACD SHIMUKESAKI_NYUKO
-    ,shimuke.KYOUHAN as check_KYOUHAN --共販店コード nullチェック用
+     nyusyukko.kyouhan_nyuko -- 共販店コード
+    ,nyusyukko.hinban_nyuko -- 品番
+    ,nyusyukko.dennoj_nyuko -- 伝票№ 自拠点(イシュ№）
+    ,nyusyukko.nyukosu -- 入庫数
+    ,nyusyukko.nyukoymd -- 入庫日
+    ,nyusyukko.jdate_nyuko -- 受注日
+    ,nyusyukko.chumon_no_nyuko --リマーク２ ※右から5桁目
+    ,
+    -- 納期仕向
+    case 
+        when
+            (
+                (
+                    (
+                        substr(nyusyukko.dennoj_nyuko, 1, 1) = 'P'
+                        or substr(nyusyukko.dennoj_nyuko, 1, 1) = 'Q'
+                    )
+                    and not (noki.kyouhan is null)
+                    and ordersyu_nyuko = '11'
+                    and (noki.hikizaikbn = '1' or noki.hikizaikbn ='3')
+                )
+                or (
+                    (
+                        substr(nyusyukko.dennoj_nyuko, 1, 1) = 'P'
+                        or substr(nyusyukko.dennoj_nyuko, 1, 1) = 'Q'
+                    )
+                    and not (noki.kyouhan is null)
+                    and ordersyu_nyuko = '12'
+                    and (noki.hikizaikbn = '2' or noki.hikizaikbn = '3')
+                )
+            )
+        then
+            noki.nokismkskcd
+        else
+            shimuke.sishacd
+    end as sishacd --支社コード
+    --
+    ,shimuke.kyouhan as check_kyouhan --共販店コード nullチェック用
 from nyusyukko
 left outer join shimuke
-on nyusyukko.KYOUHAN_NYUKO = shimuke.KYOUHAN -- 共販店コード
-and nyusyukko.UKEKTEN_NYUKO = shimuke.KYOTEN -- 受入拠点/入庫拠点
+on nyusyukko.kyouhan_nyuko = shimuke.kyouhan -- 共販店コード
+and nyusyukko.ukekten_nyuko = shimuke.kyoten -- 受入拠点/入庫拠点
+left outer join shimuke_noki noki
+on nyusyukko.kyouhan_nyuko = noki.kyouhan --共販店コード
+and nyusyukko.ukekten_nyuko = noki.nyukkten --受入拠点/入庫拠点
+and nyusyukko.makercd = noki.makercd --メーカーコード
+and case when nyusyukko.makercd <> '70000' then '' = noki.ktenkbn end --拠点区分
