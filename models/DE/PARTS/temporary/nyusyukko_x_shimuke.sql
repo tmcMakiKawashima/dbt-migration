6f306907a04d7f73eafa4c0d9074376d7a98e8d4
Watanabe Mina <m-watanabe@toyotasystems.com>
2023-11-06 11:45:55 +0900

    QA No84対応

--- models/DE/PARTS/temporary/nyusyukko_x_shimuke.sql
+++ models/DE/PARTS/temporary/nyusyukko_x_shimuke.sql
@@ -85,4 +85,9 @@ left outer join shimuke_noki noki
 on nyusyukko.kyouhan_nyuko = noki.kyouhan --共販店コード
 and nyusyukko.ukekten_nyuko = noki.nyukkten --受入拠点/入庫拠点
 and nyusyukko.makercd = noki.makercd --メーカーコード
-and case when nyusyukko.makercd <> '70000' then '' = noki.ktenkbn end --拠点区分
+and 
+    case
+        when nyusyukko.makercd <> '70000'
+            then '' = noki.ktenkbn
+        else 1
+    end --拠点区分
