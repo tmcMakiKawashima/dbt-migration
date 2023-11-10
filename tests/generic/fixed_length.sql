79767486117963a15793e48801f8dafbb760eb0c
fumihito.miura_ctc <fumihito.miura@ctc-g.co.jp>
2023-11-10 19:55:21 +0900

    Configure severity

--- tests/generic/fixed_length.sql
+++ tests/generic/fixed_length.sql
@@ -1,5 +1,7 @@
 {% test fixed_length(model, column_name, length) %}
 
+    {{ config(severity = 'error') }}
+
     select *
     from {{ model }}
     where length( {{ column_name }} ) != {{ length }}
