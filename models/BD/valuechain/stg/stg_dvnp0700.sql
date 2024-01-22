7a4069f419f87a66e5e4a1c5a4c9ca48e283180e
Watanabe Mina <m-watanabe@toyotasystems.com>
2024-01-22 18:18:04 +0900

    snapshotのupdated_atをmttimeに変更

--- models/BD/valuechain/stg/stg_dvnp0700.sql
+++ models/BD/valuechain/stg/stg_dvnp0700.sql
@@ -1,52 +1,52 @@
 with stg_dvnp0700 as (
     select
-        rtrim(ORDRKEY,' 　')::VARCHAR(11) as ORDRKEY,  -- 英数字
-        rtrim(TANSKKEY,' 　')::VARCHAR(11) as TANSKKEY,  -- 英数字
-        rtrim(SHINBAN,' 　')::VARCHAR(20) as SHINBAN,  -- 英数字
-        rtrim(SZHINBAN,' 　')::VARCHAR(20) as SZHINBAN,  -- 英数字
-        rtrim(KAKNOUKBN,' 　')::VARCHAR(1) as KAKNOUKBN,  -- 英数字
-        rtrim(BOYUSNFLG,' 　')::VARCHAR(1) as BOYUSNFLG,  -- 英数字
-        rtrim(BOYUSNFTRKTIME,' 　')::VARCHAR(12) as BOYUSNFTRKTIME,  -- 英数字
-        rtrim(KNKYUDO1,' 　')::VARCHAR(1) as KNKYUDO1,  -- 英数字
-        rtrim(BOSKSTIMEJUN,' 　')::VARCHAR(12) as BOSKSTIMEJUN,  -- 英数字
-        rtrim(KNKYUDO2,' 　')::VARCHAR(2) as KNKYUDO2,  -- 英数字
-        rtrim(YUSNRNBN,' 　')::VARCHAR(6) as YUSNRNBN,  -- 英数字
-        rtrim(MARTFLG,' 　')::VARCHAR(1) as MARTFLG,  -- 英数字
-        rtrim(PTOPFLG,' 　')::VARCHAR(1) as PTOPFLG,  -- 英数字
-        rtrim(YUSNPTRN,' 　')::VARCHAR(1) as YUSNPTRN,  -- 英数字
-        rtrim(BOSELFLG,' 　')::VARCHAR(1) as BOSELFLG,  -- 英数字
-        to_decimal(IFF(rtrim(SKSIJSU) = '',0,rtrim(SKSIJSU)))::DECIMAL(6) as SKSIJSU,  -- 数量／金額／数値
-        rtrim(SKSIJITIME,' 　')::VARCHAR(12) as SKSIJITIME,  -- 英数字
-        rtrim(TNSKSJSU,' 　')::VARCHAR(6) as TNSKSJSU,  -- 英数字
-        rtrim(HZIKSJSU,' 　')::VARCHAR(6) as HZIKSJSU,  -- 英数字
-        rtrim(NONYUYTISU,' 　')::VARCHAR(6) as NONYUYTISU,  -- 英数字
-        to_decimal(IFF(rtrim(BOSU) = '',0,rtrim(BOSU)))::DECIMAL(6) as BOSU,  -- 数量／金額／数値
-        to_decimal(IFF(rtrim(BORUISUU) = '',0,rtrim(BORUISUU)))::DECIMAL(6) as BORUISUU,  -- 数量／金額／数値
-        rtrim(BOSKSTIME,' 　')::VARCHAR(12) as BOSKSTIME,  -- 英数字
-        rtrim(BOKTIME,' 　')::VARCHAR(12) as BOKTIME,  -- 英数字
-        rtrim(GNBKPFLG,' 　')::VARCHAR(1) as GNBKPFLG,  -- 英数字
-        rtrim(PENDID,' 　')::VARCHAR(2) as PENDID,  -- 英数字
-        rtrim(PENSU,' 　')::VARCHAR(6) as PENSU,  -- 英数字
-        rtrim(PENTIME,' 　')::VARCHAR(12) as PENTIME,  -- 英数字
-        rtrim(PEKTIME,' 　')::VARCHAR(12) as PEKTIME,  -- 英数字
-        to_decimal(IFF(rtrim(CANSU) = '',0,rtrim(CANSU)))::DECIMAL(6) as CANSU,  -- 数量／金額／数値
-        rtrim(CANTIME,' 　')::VARCHAR(12) as CANTIME,  -- 英数字
-        to_decimal(IFF(rtrim(PLBSURYO) = '',0,rtrim(PLBSURYO)))::DECIMAL(6) as PLBSURYO,  -- 数量／金額／数値
-        rtrim(PLBTIME,' 　')::VARCHAR(12) as PLBTIME,  -- 英数字
-        rtrim(CASENO,' 　')::VARCHAR(8) as CASENO,  -- 英数字
-        rtrim(CASECD2,' 　')::VARCHAR(2) as CASECD2,  -- 英数字
-        to_decimal(IFF(rtrim(SKZUMSU) = '',0,rtrim(SKZUMSU)))::DECIMAL(6) as SKZUMSU,  -- 数量／金額／数値
-        rtrim(SKZUMYMD,' 　')::VARCHAR(8) as SKZUMYMD,  -- 英数字
-        rtrim(SKZTIME,' 　')::VARCHAR(12) as SKZTIME,  -- 英数字
-        rtrim(SKZFLG,' 　')::VARCHAR(1) as SKZFLG,  -- 英数字
-        rtrim(CANZMFLG,' 　')::VARCHAR(1) as CANZMFLG,  -- 英数字
-        rtrim(BUNKATUKBN,' 　')::VARCHAR(1) as BUNKATUKBN,  -- 英数字
-        rtrim(MTUSERID,' 　')::VARCHAR(16) as MTUSERID,  -- 英数字
-        rtrim(MTTIME,' 　')::VARCHAR(26) as MTTIME,  -- 英数字
-        rtrim(JUCHUYMD,' 　')::VARCHAR(8) as JUCHUYMD,  -- 英数字
-        rtrim(DUMMY,' 　')::VARCHAR(9) as DUMMY,  -- 英数字
-        LDTS, -- B層のLDTS
-        RANK() over (partition by ORDRKEY, TANSKKEY, JUCHUYMD order by MTTIME desc, LDTS desc) aggkey
+        rtrim(ordrkey,' 　')::varchar(11) as ordrkey,  -- 英数字
+        rtrim(tanskkey,' 　')::varchar(11) as tanskkey,  -- 英数字
+        rtrim(shinban,' 　')::varchar(20) as shinban,  -- 英数字
+        rtrim(szhinban,' 　')::varchar(20) as szhinban,  -- 英数字
+        rtrim(kaknoukbn,' 　')::varchar(1) as kaknoukbn,  -- 英数字
+        rtrim(boyusnflg,' 　')::varchar(1) as boyusnflg,  -- 英数字
+        rtrim(boyusnftrktime,' 　')::varchar(12) as boyusnftrktime,  -- 英数字
+        rtrim(knkyudo1,' 　')::varchar(1) as knkyudo1,  -- 英数字
+        rtrim(boskstimejun,' 　')::varchar(12) as boskstimejun,  -- 英数字
+        rtrim(knkyudo2,' 　')::varchar(2) as knkyudo2,  -- 英数字
+        rtrim(yusnrnbn,' 　')::varchar(6) as yusnrnbn,  -- 英数字
+        rtrim(martflg,' 　')::varchar(1) as martflg,  -- 英数字
+        rtrim(ptopflg,' 　')::varchar(1) as ptopflg,  -- 英数字
+        rtrim(yusnptrn,' 　')::varchar(1) as yusnptrn,  -- 英数字
+        rtrim(boselflg,' 　')::varchar(1) as boselflg,  -- 英数字
+        to_decimal(iff(rtrim(sksijsu) = '',0,rtrim(sksijsu)))::decimal(6) as sksijsu,  -- 数量／金額／数値
+        rtrim(sksijitime,' 　')::varchar(12) as sksijitime,  -- 英数字
+        rtrim(tnsksjsu,' 　')::varchar(6) as tnsksjsu,  -- 英数字
+        rtrim(hziksjsu,' 　')::varchar(6) as hziksjsu,  -- 英数字
+        rtrim(nonyuytisu,' 　')::varchar(6) as nonyuytisu,  -- 英数字
+        to_decimal(iff(rtrim(bosu) = '',0,rtrim(bosu)))::decimal(6) as bosu,  -- 数量／金額／数値
+        to_decimal(iff(rtrim(boruisuu) = '',0,rtrim(boruisuu)))::decimal(6) as boruisuu,  -- 数量／金額／数値
+        rtrim(boskstime,' 　')::varchar(12) as boskstime,  -- 英数字
+        rtrim(boktime,' 　')::varchar(12) as boktime,  -- 英数字
+        rtrim(gnbkpflg,' 　')::varchar(1) as gnbkpflg,  -- 英数字
+        rtrim(pendid,' 　')::varchar(2) as pendid,  -- 英数字
+        rtrim(pensu,' 　')::varchar(6) as pensu,  -- 英数字
+        rtrim(pentime,' 　')::varchar(12) as pentime,  -- 英数字
+        rtrim(pektime,' 　')::varchar(12) as pektime,  -- 英数字
+        to_decimal(iff(rtrim(cansu) = '',0,rtrim(cansu)))::decimal(6) as cansu,  -- 数量／金額／数値
+        rtrim(cantime,' 　')::varchar(12) as cantime,  -- 英数字
+        to_decimal(iff(rtrim(plbsuryo) = '',0,rtrim(plbsuryo)))::decimal(6) as plbsuryo,  -- 数量／金額／数値
+        rtrim(plbtime,' 　')::varchar(12) as plbtime,  -- 英数字
+        rtrim(caseno,' 　')::varchar(8) as caseno,  -- 英数字
+        rtrim(casecd2,' 　')::varchar(2) as casecd2,  -- 英数字
+        to_decimal(iff(rtrim(skzumsu) = '',0,rtrim(skzumsu)))::decimal(6) as skzumsu,  -- 数量／金額／数値
+        rtrim(skzumymd,' 　')::varchar(8) as skzumymd,  -- 英数字
+        rtrim(skztime,' 　')::varchar(12) as skztime,  -- 英数字
+        rtrim(skzflg,' 　')::varchar(1) as skzflg,  -- 英数字
+        rtrim(canzmflg,' 　')::varchar(1) as canzmflg,  -- 英数字
+        rtrim(bunkatukbn,' 　')::varchar(1) as bunkatukbn,  -- 英数字
+        rtrim(mtuserid,' 　')::varchar(16) as mtuserid,  -- 英数字
+        try_to_timestamp_ntz(mttime, 'yyyy-mm-dd-hh24.mi.ss.ff9') as mttime, -- timestamp型
+        rtrim(juchuymd,' 　')::varchar(8) as juchuymd,  -- 英数字
+        rtrim(dummy,' 　')::varchar(9) as dummy,  -- 英数字
+        ldts, -- B層のLDTS
+        rank() over (partition by ordrkey, tanskkey, juchuymd order by mttime desc, ldts desc) aggkey
     from {{ ref('substr_dvnp0700') }}
 )
 select * from stg_dvnp0700
