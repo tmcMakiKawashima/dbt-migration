bcf8d9553fa44034df547c0f5947e7a031e675b6
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-01-31 19:44:17 +0900

    Revert "Merge branch 'feature-33-EF-DM_HOSYUHIN_NOUKISHITEI' into 'main'"
    This reverts commit a10cef5939d26d2b75fb5d9544a2dd1c9c4b0777, reversing
    changes made to eda3e3741ae3887d582d7c39967f223c3f807ddc.

--- models/DE/PARTS/dim/dm_hosyuhin_juchunoki_kaito.sql
+++ models/DE/PARTS/dim/dm_hosyuhin_juchunoki_kaito.sql
@@ -3,9 +3,7 @@ with ippan as (
     where substr(m_ordeno, 1, 1) in ('P', 'Q')
 ),
 tyoku as (
-    select * exclude(m_nonuhikiatnis, m_jsksyseiymd) 
-      from {{ ref('dm_tmc_tyokuso_jutyunokikaito') }}
-     where substr(m_ordeno, 1, 1) in ('P', 'Q')
-       and m_jsksyseiymd = ''
+    select * exclude(m_nonuhikiatnis) from {{ ref('dm_tmc_tyokuso_jutyunokikaito') }}
+    where substr(m_ordeno, 1, 1) in ('P', 'Q')
 )
 select * from ippan union all select * from tyoku
