aa73ecc2f1fbeeca0b4e580baefae4907a11c953
Takeru KIDO <takeru_kido_za@mail.toyota.co.jp>
2024-02-01 16:38:56 +0900

    VIN整備販売店マスタ連携ファイル(B→D)新設

---
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3210.sql
@@ -0,0 +1,8 @@
+with substr_ktrla025zz0kil3210 as (
+    select
+        substr(raw_data, 1, 5)::varchar(5) as dlrcd,
+        substr(raw_data, 6, 5)::varchar(5) as tmpnm,
+        ldts
+    from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3210') }}
+)
+select * from substr_ktrla025zz0kil3210
