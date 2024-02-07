0d0b485556ef5dfffa91604e5550af7b8e7f3e89
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-02-07 18:16:25 +0900

    Revert "Revert "Merge branch 'feature-33-EF-DM_HOSYUHIN_NOUKISHITEI' into 'main'""
    This reverts commit bcf8d9553fa44034df547c0f5947e7a031e675b6.

--- tests/singular/de/parts/dm/dm_hosyuhin_juchunoki_kaito_not_match_count.sql
+++ tests/singular/de/parts/dm/dm_hosyuhin_juchunoki_kaito_not_match_count.sql
@@ -10,7 +10,7 @@ select c.cnt - (a.cnt  + b.cnt) as cnt
       select count(*) as cnt
       from {{ref('dm_tmc_tyokuso_jutyunokikaito')}}
       where substr(m_ordeno, 1, 1) in ('P', 'Q')
-      
+        and m_jsksyseiymd = ''
     ) b, (
       select count(*) as cnt
       from {{ref('dm_hosyuhin_juchunoki_kaito')}}
