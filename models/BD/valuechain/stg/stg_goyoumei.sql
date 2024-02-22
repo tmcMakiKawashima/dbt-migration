607f94549cebaa7fa713aec7bf7934731dc9d2b6
Takeru KIDO <takeru_kido_za@mail.toyota.co.jp>
2024-02-22 15:40:55 +0900

    VIN整備ご用命連携ファイル(B→D)命名規則など修正

---
+++ models/BD/valuechain/stg/stg_goyoumei.sql
@@ -0,0 +1,14 @@
+with stg_goyoumei as (
+    select
+        rtrim(delflg, ' 　')::varchar(1) as delflg,
+        rtrim(nyukono, ' 　')::varchar(9) as nyukono,
+        rtrim(meisaino, ' 　')::varchar(4) as meisaino,
+        rtrim(hosyokbn, ' 　')::varchar(1) as hosyokbn,
+        rtrim(t1w, ' 　')::varchar(4) as t1w,
+        rtrim(goyomeisansyocd, ' 　')::varchar(10) as goyomeisansyocd,
+        ldts,
+        rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
+    from {{ ref('substr_ktrla025zz0kil3204') }}
+)
+select * from stg_goyoumei
+where aggkey = 1
--- models/BD/valuechain/substr/substr_ktrla025zz0kil3204.sql
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3204.sql
@@ -1,11 +1,11 @@
 with substr_ktrla025zz0kil3204 as (
     select
-        substr(raw_data, 1, 1)::varchar(1) as mtkbn,
-        substr(raw_data, 2, 9)::varchar(9) as strno,
-        substr(raw_data, 11, 4)::varchar(4) as dtlno,
-        substr(raw_data, 15, 1)::varchar(1) as grtpa,
-        substr(raw_data, 16, 4)::varchar(4) as t1wcd,
-        substr(raw_data, 20, 10)::varchar(10) as refcd,
+        substr(raw_data, 1, 1)::varchar(1) as delflg,
+        substr(raw_data, 2, 9)::varchar(9) as nyukono,
+        substr(raw_data, 11, 4)::varchar(4) as meisaino,
+        substr(raw_data, 15, 1)::varchar(1) as hosyokbn,
+        substr(raw_data, 16, 4)::varchar(4) as t1w,
+        substr(raw_data, 20, 10)::varchar(10) as goyomeisansyocd,
         ldts
     from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3204') }}
 )
