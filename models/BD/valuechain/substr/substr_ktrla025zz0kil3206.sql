4a38ce510b12ea05d5c73df1a17378c86e3f12ec
Takeru KIDO <takeru_kido_za@mail.toyota.co.jp>
2024-02-22 17:43:41 +0900

    VIN整備整備歴連携ファイル(B→D)命名規則など修正

--- models/BD/valuechain/substr/substr_ktrla025zz0kil3206.sql
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3206.sql
@@ -1,9 +1,9 @@
 with substr_ktrla025zz0kil3206 as (
     select
-        substr(raw_data, 1, 1)::varchar(1) as mtkbn,
-        substr(raw_data, 2, 9)::varchar(9) as strno,
-        substr(raw_data, 11, 4)::varchar(4) as dtlno,
-        substr(raw_data, 15, 11)::varchar(11) as seibinmcd,
+        substr(raw_data, 1, 1)::varchar(1) as delflg,
+        substr(raw_data, 2, 9)::varchar(9) as nyukono,
+        substr(raw_data, 11, 4)::varchar(4) as meisaino,
+        substr(raw_data, 15, 11)::varchar(11) as sobinamesansyocd,
         substr(raw_data, 26, 8)::varchar(8) as seibicd,
         ldts
     from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3206') }}
