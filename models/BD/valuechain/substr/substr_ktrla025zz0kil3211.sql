e39d06022d77122d722c11937f220d352407ba67
Takeru KIDO <takeru_kido_za@mail.toyota.co.jp>
2024-02-22 18:13:48 +0900

    VIN整備販売店店舗マスタ連携ファイル(B→D)命名規則など修正

--- models/BD/valuechain/substr/substr_ktrla025zz0kil3211.sql
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3211.sql
@@ -1,10 +1,10 @@
 with substr_ktrla025zz0kil3211 as (
     select
-        substr(raw_data, 1, 5)::varchar(5) as dlrcd,
-        substr(raw_data, 6, 3)::varchar(3) as tmpcd,
-        substr(raw_data, 9, 20)::varchar(20) as tmpnm,
-        substr(raw_data, 29, 6)::varchar(6) as startmt,
-        substr(raw_data, 35, 6)::varchar(6) as stopmt,
+        substr(raw_data, 1, 5)::varchar(5) as hanbaitencd,
+        substr(raw_data, 6, 3)::varchar(3) as tenpocd,
+        substr(raw_data, 9, 20)::varchar(20) as tenponame,
+        substr(raw_data, 29, 6)::varchar(6) as startym,
+        substr(raw_data, 35, 6)::varchar(6) as endym,
         ldts
     from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3211') }}
 )
