602c491182ffccb60ed76575cfe7efa90cfed476
Takeru KIDO <takeru_kido_za@mail.toyota.co.jp>
2024-02-27 15:42:48 +0900

    VIN整備整備歴連携ファイル(B→D)更新区分修正

--- models/BD/valuechain/stg/stg_seibireki.sql
+++ models/BD/valuechain/stg/stg_seibireki.sql
@@ -1,6 +1,6 @@
 with stg_seibireki as (
     select
-        rtrim(delflg, ' 　')::varchar(1) as delflg,
+        iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
         rtrim(nyukono, ' 　')::varchar(9) as nyukono,
         rtrim(meisaino, ' 　')::varchar(4) as meisaino,
         rtrim(sobinamesansyocd, ' 　')::varchar(11) as sobinamesansyocd,
