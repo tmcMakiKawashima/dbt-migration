9cdadf3cf41ed83f992c867578a4c7b4d5ce3903
Watanabe Mina <m-watanabe@toyotasystems.com>
2024-03-27 16:29:00 +0900

    規約に沿って修正

--- models/DE/vinhis/spec/temporary/dm_vinhis_specification_oem/tmp20_dm_vinhis_specification_oem.sql
+++ models/DE/vinhis/spec/temporary/dm_vinhis_specification_oem/tmp20_dm_vinhis_specification_oem.sql
@@ -7,14 +7,14 @@ with
         select
             syadai_kt, -- 車台型式
             frmno, -- フレームNO
-            sketa as sketa_cd, -- スペック桁
-            skigo as skigo_cd, -- スペック記号
-        from {{ref('stg_oemseisan200')}}
+            sketa, -- スペック桁
+            skigo, -- スペック記号
+        from {{ref('stg_oemseisan200')}} -- 仕入実績生産スペック200桁縦
     )
 select
     tmp10.*,
-    oem200.sketa_cd,
-    oem200.skigo_cd
+    oem200.sketa,
+    oem200.skigo
 from tmp10
 left outer join oem200
     on tmp10.syadai_kt = oem200.syadai_kt
--- models/DE/vinhis/spec/temporary/dm_vinhis_specification_oem/tmp30_dm_vinhis_specification_oem.sql
+++ models/DE/vinhis/spec/temporary/dm_vinhis_specification_oem/tmp30_dm_vinhis_specification_oem.sql
@@ -7,20 +7,20 @@ with
         select
             syasyu, -- 車種コード
             sketa, -- スペック桁
-            shiyodai as daibun_cd, -- 大分類コード
-            smeikanji as dai_kanji_name, -- スペック名称漢字
-            smeikana as dai_kana_name, -- スペック名称カナ
-            smeieiji as dai_eiji_name, -- スペック名称英字
-        from {{ref('stg_specname')}}
+            shiyodai, -- 大分類コード
+            smeikanji, -- スペック名称漢字
+            smeikana, -- スペック名称カナ
+            smeieiji, -- スペック名称英字
+        from {{ref('stg_specname')}} -- スペック名称
         where skigo = ' '
     )
 select
     tmp20.*,
-    spec.daibun_cd,
-    spec.dai_kanji_name,
-    spec.dai_kana_name,
-    spec.dai_eiji_name
+    spec.shiyodai,
+    spec.smeikanji,
+    spec.smeikana,
+    spec.smeieiji
 from tmp20
 left outer join spec
     on tmp20.syasyu_cd = spec.syasyu
-    and tmp20.sketa_cd = spec.sketa
+    and tmp20.sketa = spec.sketa
