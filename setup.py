a59b3de9533c2d9d16cb662d49eaa9a8abaa5346
Administrator <admin@example.com>
2024-04-12 08:26:41 +0000

    Update 4 files
    - /repo.py
    - /workspace.yaml
    - /setup.py
    - /Dockerfile

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="da_dxpf_edp_dagster_edp_infra_ope_sample",
     packages=find_packages(exclude=["da_dxpf_edp_dagster_edp_infra_ope_sample_tests"]),
     install_requires=[
-        "dagster",
-        "dagster-cloud"
+        "dagster==1.7.0",
+        "dagster-cloud=1.7.0"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
