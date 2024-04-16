1c21a1fa8ce63d79cddbba9af96e5e4aa188eb64
Administrator <admin@example.com>
2024-04-16 06:52:33 +0000

    Update 64 files
    - /pyproject.toml
    - /setup.py
    - /setup.cfg
    - /dagster_cloud.yaml
    - /logs/dbt.log
    - /tmp5xv368dr/history/runs.db
    - /tmp5xv368dr/history/runs/1775a147-c7e7-407f-a302-f4c4daa51091.db
    - /tmp5xv368dr/history/runs/b9f63e14-04f2-409e-8d4c-a0e43629ad34.db
    - /tmp5xv368dr/history/runs/e394457f-032d-4b55-8963-c372f1943f29.db
    - /tmp5xv368dr/history/runs/f04e23e5-1e56-4858-af95-8a6cec87420e.db
    - /tmp5xv368dr/history/runs/f73c7eda-e7e2-40bd-93a5-8d053bea5d34.db
    - /tmp5xv368dr/history/runs/fb250a60-7f01-4d5f-b94d-7535bb0458d3.db
    - /tmp5xv368dr/history/runs/index.db
    - /tmp5xv368dr/history/runs/aa591d48-ff35-44e9-9fa7-b575c8a699c4.db
    - /tmp5xv368dr/history/runs/520d7144-449d-487f-a6c5-f789802b93bb.db
    - /tmp5xv368dr/history/runs/5e693d7c-3e5c-4274-ab30-0917e62ae59e.db
    - /tmp5xv368dr/schedules/schedules.db
    - /tmp5xv368dr/storage/1775a147-c7e7-407f-a302-f4c4daa51091/compute_logs/gtkulygw.complete
    - /tmp5xv368dr/storage/1775a147-c7e7-407f-a302-f4c4daa51091/compute_logs/gtkulygw.err
    - /tmp5xv368dr/storage/1775a147-c7e7-407f-a302-f4c4daa51091/compute_logs/gtkulygw.out
    - /tmp5xv368dr/storage/520d7144-449d-487f-a6c5-f789802b93bb/compute_logs/gsvwfmnm.complete
    - /tmp5xv368dr/storage/520d7144-449d-487f-a6c5-f789802b93bb/compute_logs/gsvwfmnm.err
    - /tmp5xv368dr/storage/520d7144-449d-487f-a6c5-f789802b93bb/compute_logs/gsvwfmnm.out
    - /tmp5xv368dr/storage/aa591d48-ff35-44e9-9fa7-b575c8a699c4/compute_logs/rbpjfobf.complete
    - /tmp5xv368dr/storage/aa591d48-ff35-44e9-9fa7-b575c8a699c4/compute_logs/rbpjfobf.err
    - /tmp5xv368dr/storage/aa591d48-ff35-44e9-9fa7-b575c8a699c4/compute_logs/rbpjfobf.out
    - /tmp5xv368dr/storage/b9f63e14-04f2-409e-8d4c-a0e43629ad34/compute_logs/bwbjuhzr.complete
    - /tmp5xv368dr/storage/b9f63e14-04f2-409e-8d4c-a0e43629ad34/compute_logs/bwbjuhzr.err
    - /tmp5xv368dr/storage/b9f63e14-04f2-409e-8d4c-a0e43629ad34/compute_logs/bwbjuhzr.out
    - /tmp5xv368dr/storage/e394457f-032d-4b55-8963-c372f1943f29/compute_logs/emnzubnh.complete
    - /tmp5xv368dr/storage/e394457f-032d-4b55-8963-c372f1943f29/compute_logs/emnzubnh.err
    - /tmp5xv368dr/storage/e394457f-032d-4b55-8963-c372f1943f29/compute_logs/emnzubnh.out
    - /tmp5xv368dr/storage/f04e23e5-1e56-4858-af95-8a6cec87420e/compute_logs/pcdlcgzm.complete
    - /tmp5xv368dr/storage/f04e23e5-1e56-4858-af95-8a6cec87420e/compute_logs/pcdlcgzm.err
    - /tmp5xv368dr/storage/f04e23e5-1e56-4858-af95-8a6cec87420e/compute_logs/pcdlcgzm.out
    - /tmp5xv368dr/storage/f73c7eda-e7e2-40bd-93a5-8d053bea5d34/compute_logs/fzwmtqcf.complete
    - /tmp5xv368dr/storage/f73c7eda-e7e2-40bd-93a5-8d053bea5d34/compute_logs/fzwmtqcf.err
    - /tmp5xv368dr/storage/f73c7eda-e7e2-40bd-93a5-8d053bea5d34/compute_logs/fzwmtqcf.out
    - /tmp5xv368dr/storage/fb250a60-7f01-4d5f-b94d-7535bb0458d3/compute_logs/puuflcww.complete
    - /tmp5xv368dr/storage/fb250a60-7f01-4d5f-b94d-7535bb0458d3/compute_logs/puuflcww.err
    - /tmp5xv368dr/storage/fb250a60-7f01-4d5f-b94d-7535bb0458d3/compute_logs/puuflcww.out
    - /tmpqo01s8f0/history/runs.db
    - /tmpqo01s8f0/history/runs/index.db
    - /tmpqo01s8f0/schedules/schedules.db
    - /tmpzhs8gocv/history/runs.db
    - /tmpzhs8gocv/history/runs/index.db
    - /tmpzhs8gocv/schedules/schedules.db
    - /dagster_project1/__init__.py
    - /dagster_project1/assets.py
    - /dagster_project1/__pycache__/assets.cpython-312.pyc
    - /dagster_project1/__pycache__/__init__.cpython-312.pyc
    - /dagster_project1_tests/test_assets.py
    - /dagster_project1_tests/__init__.py
    - /dbt_project/.gitignore
    - /dbt_project/dbt_project.yml
    - /dbt_project/README.md
    - /dbt_project/analyses/.gitkeep
    - /dbt_project/macros/.gitkeep
    - /dbt_project/models/example/my_first_dbt_model.sql
    - /dbt_project/models/example/my_second_dbt_model.sql
    - /dbt_project/models/example/schema.yml
    - /dbt_project/seeds/.gitkeep
    - /dbt_project/snapshots/.gitkeep
    - /dbt_project/tests/.gitkeep

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="da_dxpf_edp_dagster_edp_infra_ope_sample",
-    packages=find_packages(exclude=["da_dxpf_edp_dagster_edp_infra_ope_sample_tests"]),
+    name="dagster_project1",
+    packages=find_packages(exclude=["dagster_project1_tests"]),
     install_requires=[
         "dagster==1.7.0",
         "dagster-cloud==1.7.0"
