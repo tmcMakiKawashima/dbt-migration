ae7b837643baf7c2a61d620d770683b83f804b54
Administrator <admin@example.com>
2024-04-16 07:04:04 +0000

    Update 2 files
    - /dagster_cloud.yaml
    - /setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     packages=find_packages(exclude=["dagster_project1_tests"]),
     install_requires=[
         "dagster==1.7.0",
-        "dagster-cloud==1.7.0"
+        "dagster-cloud==1.7.0",
+        "dagster-dbt:0.23.1"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
