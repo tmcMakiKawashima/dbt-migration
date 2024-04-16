3b43a50e23292b1a3451f736b4cf0728d95a7e91
Administrator <admin@example.com>
2024-04-16 07:33:45 +0000

    Update file setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     install_requires=[
         "dagster==1.7.0",
         "dagster-cloud==1.7.0",
-        "dagster-dbt==0.23.0"
+        "dagster-dbt==0.23.0",
+        "dbt-snowflake==1.7.3"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
