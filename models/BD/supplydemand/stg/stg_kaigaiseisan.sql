bec89ea2fd98831c946f9c6fe9b2668611b70efc
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-05-14 19:18:44 +0900

    incremental mergeに変更適用

--- models/BD/supplydemand/stg/stg_kaigaiseisan.sql
+++ models/BD/supplydemand/stg/stg_kaigaiseisan.sql
@@ -1,3 +1,11 @@
+{{
+    config (
+        materialized = 'incremental',
+        unique_key = ['frmno', 'wmi', 'vds', 'mdlyr', 'vin_vds_cd'],
+        incremental_strategy = 'merge'
+    )
+}}
+
 with stg_kaigaiseisan as (
     select
         lok_y4::varchar(1) as lok_y4,
@@ -242,5 +250,10 @@ with stg_kaigaiseisan as (
                 order by ldts desc
             ) aggkey
     from {{ref('substr_da5a212b')}}
+
+    {% if is_incremental() %}
+        where ldts > (select max(ldts) from {{ this }})
+    {% endif %}
+
 )
 select * from stg_kaigaiseisan where aggkey = 1
