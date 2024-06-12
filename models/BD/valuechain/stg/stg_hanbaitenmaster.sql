b3bbe5b190986d991091e4d428233b7b39594317
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-12 19:07:42 +0900

    Refactor code to comply with new standards

--- models/BD/valuechain/stg/stg_hanbaitenmaster.sql
+++ models/BD/valuechain/stg/stg_hanbaitenmaster.sql
@@ -1,10 +1,20 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'hanbaitencd',
+        incremental_strategy = 'delete+insert'
+    )
+}}
 with stg_hanbaitenmaster as (
     select
-        rtrim(hanbaitencd, ' 　')::varchar(5) as hanbaitencd,
-        rtrim(hanbaitenname, ' 　')::varchar(5) as hanbaitenname,
+        rtrim(R001, ' 　')::varchar(5) as hanbaitencd,
+        rtrim(R002, ' 　')::varchar(5) as hanbaitenname,
         ldts,
         rank() over (partition by hanbaitencd order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3210') }}
+
+    {% if is_incremental() %}
+        where ldts > (select max(ldts) from {{ this }})
+    {% endif %}
 )
-select * from stg_hanbaitenmaster
-where aggkey = 1
+select * from stg_hanbaitenmaster where aggkey = 1
