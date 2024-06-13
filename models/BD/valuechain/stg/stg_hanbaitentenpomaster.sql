9466df6f92d8455487f34132cc6f23dc6666c91d
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-13 18:22:52 +0900

    fix 標準に合わせてtrim削除

--- models/BD/valuechain/stg/stg_hanbaitentenpomaster.sql
+++ models/BD/valuechain/stg/stg_hanbaitentenpomaster.sql
@@ -7,11 +7,11 @@
 }}
 with stg_hanbaitentenpomaster as (
     select
-        rtrim(M001, ' 　')::varchar(5) as hanbaitencd,
-        rtrim(M002, ' 　')::varchar(3) as tenpocd,
-        rtrim(M003, ' 　')::varchar(20) as tenponame,
-        rtrim(M004, ' 　')::varchar(6) as startym,
-        rtrim(M005, ' 　')::varchar(6) as endym,
+        rtrim(M001, ' 　')::varchar(5) as hanbaitencd, -- 右ブランク
+        rtrim(M002, ' 　')::varchar(3) as tenpocd, -- 右ブランク
+        M003::varchar(20) as tenponame,
+        M004::varchar(6) as startym,
+        M005::varchar(6) as endym,
         ldts,
         rank() over (partition by hanbaitencd, tenpocd order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3211') }}
