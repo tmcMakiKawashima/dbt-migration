8ab3d4300540bf288f8c4776507948c6ff6d8ed2
nishino <saya_nishino_zb@mail.toyota.co.jp>
2024-06-13 14:31:45 +0900

    BD-号口車種コード

---
+++ models/BD/valuechain/substr/substr_tpjfva40.sql
@@ -0,0 +1,11 @@
+with substr_tpjfva40 as (
+  select
+    substr(raw_data, 2, 4)::varchar(4) as syasyug,
+    substr(raw_data, 6, 3)::varchar(3) as syameino,
+    substr(raw_data, 9, 8)::varchar(8) as touroku,
+    substr(raw_data,17, 8)::varchar(8) as kousin,
+    substr(raw_data,25, 7)::varchar(7) as tantou,
+    ldts
+  from {{ source('snowpipe_db_valuechain', 'raw_goguchisyasyucd')}}
+)
+select * from substr_tpjfva40
