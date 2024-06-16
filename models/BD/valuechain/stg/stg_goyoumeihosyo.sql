0fe8bbb6d926865f64c51221fa43f9e61af6b203
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-06-17 04:30:20 +0900

    testの実装及びrtrimの削除

--- models/BD/valuechain/stg/stg_goyoumeihosyo.sql
+++ models/BD/valuechain/stg/stg_goyoumeihosyo.sql
@@ -9,15 +9,15 @@
 with stg_goyoumeihosyo as (
     select
         iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
-        rtrim(R001, ' 　')::varchar(9) as nyukono,
-        rtrim(R002, ' 　')::varchar(4) as meisaino,
-        rtrim(R003, ' 　')::varchar(8) as sainyukoymd,
-        rtrim(R004, ' 　')::varchar(1) as tyoushiukagaikekkacd,
-        rtrim(R005, ' 　')::varchar(2) as genincd,
-        rtrim(R006, ' 　')::varchar(2) as hosyokbn,
-        rtrim(R007, ' 　')::varchar(12) as hosyoseikyusyono,
-        rtrim(R008, ' 　')::varchar(1) as huguaisaigenumu,
-        rtrim(R009, ' 　')::varchar(50) as geninmeisyo,
+        R001::varchar(9) as nyukono,
+        R002::varchar(4) as meisaino,
+        R003::varchar(8) as sainyukoymd,
+        R004::varchar(1) as tyoushiukagaikekkacd,
+        R005::varchar(2) as genincd,
+        R006::varchar(2) as hosyokbn,
+        rtrim(R007, ' 　')::varchar(12) as hosyoseikyusyono, -- 右blank
+        R008::varchar(1) as huguaisaigenumu,
+        rtrim(R009, ' 　')::varchar(50) as geninmeisyo, -- 右blank
         ldts,
         rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3205') }}
