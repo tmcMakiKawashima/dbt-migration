b4dd313f8369c5722577cdd2bfcc949ad7631f2b
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-19 12:03:21 +0900

    fix 規約に沿って修正

--- models/BD/valuechain/stg/stg_hanbaitentenpomaster.sql
+++ models/BD/valuechain/stg/stg_hanbaitentenpomaster.sql
@@ -7,11 +7,11 @@
 }}
 with stg_hanbaitentenpomaster as (
     select
-        rtrim(M001, ' 　')::varchar(5) as hanbaitencd, -- 右ブランク
-        rtrim(M002, ' 　')::varchar(3) as tenpocd, -- 右ブランク
-        M003::varchar(20) as tenponame,
-        M004::varchar(6) as startym,
-        M005::varchar(6) as endym,
+        rtrim(m001, ' 　')::varchar(5) as hanbaitencd, -- 右ブランク
+        rtrim(m002, ' 　')::varchar(3) as tenpocd, -- 右ブランク
+        m003::varchar(20) as tenponame,
+        m004::varchar(6) as startym,
+        m005::varchar(6) as endym,
         ldts,
         rank() over (partition by hanbaitencd, tenpocd order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3211') }}
