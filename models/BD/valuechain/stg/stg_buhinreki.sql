89ae2dec891284f7e14f783d7cd501653530fb62
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-19 10:57:25 +0900

    fix 規約に沿って修正

--- models/BD/valuechain/stg/stg_buhinreki.sql
+++ models/BD/valuechain/stg/stg_buhinreki.sql
@@ -9,11 +9,11 @@
 with stg_buhinreki as (
     select
         iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
-        R001::varchar(9) as nyukono,
-        R002::varchar(4) as meisaino,
-        R003::varchar(4) as buhinsuryo,
-        R005::varchar(11) as buhinmeisyosansyocd,
-        R006::varchar(15) as hinban,
+        r001::varchar(9) as nyukono,
+        r002::varchar(4) as meisaino,
+        r003::varchar(4) as buhinsuryo,
+        r005::varchar(11) as buhinmeisyosansyocd,
+        r006::varchar(15) as hinban,
         ldts,
         rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3207') }}
--- models/BD/valuechain/substr/substr_ktrla025zz0kil3207.sql
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3207.sql
@@ -1,11 +1,11 @@
 with substr_ktrla025zz0kil3207 as (
     select
         substr(raw_data, 1, 1)::varchar(1) as delflg,
-        substr(raw_data, 2, 9)::varchar(9) as R001,
-        substr(raw_data, 11, 4)::varchar(4) as R002,
-        substr(raw_data, 15, 4)::varchar(4) as R003,
-        substr(raw_data, 19, 11)::varchar(11) as R005,
-        substr(raw_data, 30, 15)::varchar(15) as R006,
+        substr(raw_data, 2, 9)::varchar(9) as r001,
+        substr(raw_data, 11, 4)::varchar(4) as r002,
+        substr(raw_data, 15, 4)::varchar(4) as r003,
+        substr(raw_data, 19, 11)::varchar(11) as r005,
+        substr(raw_data, 30, 15)::varchar(15) as r006,
         ldts
     from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3207') }}
 )
