4c05f79e6dc65009b2006404bd6799d35dbbfb36
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-06-19 11:47:13 +0900

    コード規約に従って修正

--- models/BD/valuechain/stg/stg_goyoumeihosyo.sql
+++ models/BD/valuechain/stg/stg_goyoumeihosyo.sql
@@ -9,15 +9,15 @@
 with stg_goyoumeihosyo as (
     select
         iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
-        R001::varchar(9) as nyukono,
-        R002::varchar(4) as meisaino,
-        R003::varchar(8) as sainyukoymd,
-        R004::varchar(1) as tyoushiukagaikekkacd,
-        R005::varchar(2) as genincd,
-        R006::varchar(2) as hosyokbn,
-        rtrim(R007, ' 　')::varchar(12) as hosyoseikyusyono, -- 右blank
-        R008::varchar(1) as huguaisaigenumu,
-        rtrim(R009, ' 　')::varchar(50) as geninmeisyo, -- 右blank
+        r001::varchar(9) as nyukono,
+        r002::varchar(4) as meisaino,
+        r003::varchar(8) as sainyukoymd,
+        r004::varchar(1) as tyoushiukagaikekkacd,
+        r005::varchar(2) as genincd,
+        r006::varchar(2) as hosyokbn,
+        rtrim(r007, ' 　')::varchar(12) as hosyoseikyusyono, -- 右blank
+        r008::varchar(1) as huguaisaigenumu,
+        rtrim(r009, ' 　')::varchar(50) as geninmeisyo, -- 右blank
         ldts,
         rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3205') }}
