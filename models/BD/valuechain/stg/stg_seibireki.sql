27abc047a0e91f8b16fdffc4cfda186b7174a2d0
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-06-19 11:50:15 +0900

    コード規約に従って修正

--- models/BD/valuechain/stg/stg_seibireki.sql
+++ models/BD/valuechain/stg/stg_seibireki.sql
@@ -9,10 +9,10 @@
 with stg_seibireki as (
     select
         iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
-        R001::varchar(9) as nyukono,
-        R002::varchar(4) as meisaino,
-        R003::varchar(11) as sobinamesansyocd,
-        R004::varchar(8) as seibicd,
+        r001::varchar(9) as nyukono,
+        r002::varchar(4) as meisaino,
+        r003::varchar(11) as sobinamesansyocd,
+        r004::varchar(8) as seibicd,
         ldts,
         rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3206') }}
