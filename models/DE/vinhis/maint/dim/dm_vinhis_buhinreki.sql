202c9a7f460048e54c3f1bddd4baba737add279f
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2024-06-20 11:04:04 +0900

    Revert "VIN x 部品歴 修正"
    This reverts commit 11c3ee04ef933f09214a84a59e169dda1e7c8456.

--- models/DE/vinhis/maint/dim/dm_vinhis_buhinreki.sql
+++ models/DE/vinhis/maint/dim/dm_vinhis_buhinreki.sql
@@ -1,16 +1,16 @@
 with
     tmp10_dm_vinhis_buhinreki as (
-        select * from {{ ref('tmp10_dm_vinhis_buhinreki') }}
+        select * from {{ ref("tmp10_dm_vinhis_buhinreki") }}
     ),
     stg_hinbanmeisyomaster as (
         select
-            hinban, -- 品番
-            hinmei -- 品名
-        from {{ ref('stg_hinbanmeisyomaster') }} -- 品番情報マスタ
+            parts_cd, -- 品番コード
+            parts_name -- 品番名称
+        from {{ source('customerservice_db_public', 'stg_hinbanmeisyomaster') }} -- 整備名称情報マスタ
     )
 select
     tmp10_dm_vinhis_buhinreki.syadai_kt, -- 車台型式
-    tmp10_dm_vinhis_buhinreki.frm_no, -- フレーム連番
+    tmp10_dm_vinhis_buhinreki.frm_no, -- フレームNo
     tmp10_dm_vinhis_buhinreki.nyuko_dlrcd, -- 入庫販売店コード
     tmp10_dm_vinhis_buhinreki.nyuko_dlrcd_name, -- 入庫販売店名称
     tmp10_dm_vinhis_buhinreki.uketuke_tenpo_cd, -- 受付店舗コード
@@ -20,12 +20,12 @@ select
     tmp10_dm_vinhis_buhinreki.nyuko_plan_date, -- 入庫予定日
     tmp10_dm_vinhis_buhinreki.uketuke_date, -- 受付日
     tmp10_dm_vinhis_buhinreki.soukou_km, -- 走行km
-    tmp10_dm_vinhis_buhinreki.jyutyu_no, -- 受注NO
+    tmp10_dm_vinhis_buhinreki.jutyu_no, -- 受注No
     tmp10_dm_vinhis_buhinreki.payment_date, -- 精算年月日
     tmp10_dm_vinhis_buhinreki.nyuko_no, -- 入庫番号
-    tmp10_dm_vinhis_buhinreki.nyuko_detail_no, -- 明細行番号
-    tmp10_dm_vinhis_buhinreki.parts_cd, -- 品番コード
-    stg_hinbanmeisyomaster.hinmei as parts_name, -- 品番名称
-    tmp10_dm_vinhis_buhinreki.parts_amount -- 部品数量
+    tmp10_dm_vinhis_buhinreki.meisaino as nyuko_detail_no, -- 明細行番号
+    tmp10_dm_vinhis_buhinreki.hinban as parts_cd, -- 品番コード
+    stg_hinbanmeisyomaster.parts_name as parts_name, -- 品番名称
+    tmp10_dm_vinhis_buhinreki.buhinsuryo as parts_amount -- 部品数量
 from tmp10_dm_vinhis_buhinreki
-left outer join stg_hinbanmeisyomaster on tmp10_dm_vinhis_buhinreki.parts_cd = stg_hinbanmeisyomaster.hinban
+left outer join stg_hinbanmeisyomaster on tmp10_dm_vinhis_buhinreki.hinban = stg_hinbanmeisyomaster.parts_cd
