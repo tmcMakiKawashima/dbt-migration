c5d3d7c3197234505c9564fa3a0d6b290532c7f7
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-22 17:53:50 +0900

    fix 列の並びを修正

--- models/BD/valuechain/stg/stg_buhinreki.sql
+++ models/BD/valuechain/stg/stg_buhinreki.sql
@@ -8,12 +8,12 @@
 
 with stg_buhinreki as (
     select
-        iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
         r001::varchar(9) as nyukono,
         r002::varchar(4) as meisaino,
         r003::varchar(4) as buhinsuryo,
         r005::varchar(11) as buhinmeisyosansyocd,
         r006::varchar(15) as hinban,
+        iff(rtrim(delflg, ' 　') = 'D', '1', '0')::varchar(1) as delflg,
         ldts,
         rank() over (partition by nyukono, meisaino order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3207') }}
