54a2a7d5ce21369c444560249e0dab82bb890821
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-25 12:39:34 +0900

    fix incremental mergeからfull refreshに修正

--- models/BD/valuechain/stg/stg_hanbaitenmaster.sql
+++ models/BD/valuechain/stg/stg_hanbaitenmaster.sql
@@ -2,8 +2,7 @@ with stg_hanbaitenmaster as (
     select
         rtrim(r001, ' 　')::varchar(5) as hanbaitencd, --右ブランク
         r002::varchar(5) as hanbaitenname,
-        ldts,
-        rank() over (partition by hanbaitencd order by ldts desc) aggkey
+        ldts
     from {{ ref('substr_ktrla025zz0kil3210') }}
 )
 select * from stg_hanbaitenmaster
