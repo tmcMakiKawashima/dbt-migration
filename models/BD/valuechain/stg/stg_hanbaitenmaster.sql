1282c308ca447108d8c97e3488efcbf388684861
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-06-25 12:20:51 +0900

    fix incremental mergeからfull refreshに修正

--- models/BD/valuechain/stg/stg_hanbaitenmaster.sql
+++ models/BD/valuechain/stg/stg_hanbaitenmaster.sql
@@ -1,10 +1,3 @@
-{{
-    config(
-        materialized = 'incremental',
-        unique_key = 'hanbaitencd',
-        incremental_strategy = 'delete+insert'
-    )
-}}
 with stg_hanbaitenmaster as (
     select
         rtrim(r001, ' 　')::varchar(5) as hanbaitencd, --右ブランク
@@ -12,9 +5,6 @@ with stg_hanbaitenmaster as (
         ldts,
         rank() over (partition by hanbaitencd order by ldts desc) aggkey
     from {{ ref('substr_ktrla025zz0kil3210') }}
-
-    {% if is_incremental() %}
-        where ldts > (select max(ldts) from {{ this }})
-    {% endif %}
 )
-select * from stg_hanbaitenmaster where aggkey = 1
+select * from stg_hanbaitenmaster
+where ldts = (select max(ldts) from stg_hanbaitenmaster)
