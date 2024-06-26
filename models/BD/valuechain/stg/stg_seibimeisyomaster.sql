e602e62c8bfab8ca365a2d74596dea24000aa4e8
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-06-26 09:32:00 +0900

    設計書に従いincremental mergeを削除

--- models/BD/valuechain/stg/stg_seibimeisyomaster.sql
+++ models/BD/valuechain/stg/stg_seibimeisyomaster.sql
@@ -1,11 +1,3 @@
-{{
-    config(
-        materialized = 'incremental',
-        unique_key = ['seibi_cd','mgr_kbn'],
-        incremental_strategy = 'merge'
-    )
-}}
-
 with stg_seibimeisyomaster as (
     select
         rtrim(seibi_cd, ' 　')::varchar(8) as seibi_cd, -- 右blank
@@ -16,10 +8,6 @@ with stg_seibimeisyomaster as (
         ldts,
         rank() over (partition by seibi_cd, mgr_kbn order by ldts desc) aggkey
     from {{ source('customerservice_db_public', 'stg_seibimeisyomaster') }} -- 整備名称情報マスタ
-
-    {% if is_incremental() %}
-        where ldts > (select max(ldts) from {{ this }})
-    {% endif %}
 )
-
-select * from stg_seibimeisyomaster where aggkey = 1
+select * from stg_seibimeisyomaster
+where ldts = (select max(ldts) from stg_seibimeisyomaster)
