0e2d65d992d1337ce277132ad277700d7649a233
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-07-02 13:56:49 +0900

    準本番確認のため、masking policy処理変更

--- models/BD/valuechain/stg/stg_hinbanmeisyomaster.sql
+++ models/BD/valuechain/stg/stg_hinbanmeisyomaster.sql
@@ -1,5 +1,8 @@
 {{
     config(
+        pre_hook=[
+          "{{ dbt_snow_mask.create_masking_policy('models')}}"
+        ],
         post_hook=[
           "{{ dbt_snow_mask.apply_masking_policy('models') }}"
         ]
