742b05ec9145e8b0d096ce77337bd3737bd77413
da-shoabe-04.00 <sho.abe@ctc-g.co.jp>
2024-07-10 13:46:17 +0900

    stgへの抽出条件にrow_numberを追加

--- models/BD/valuechain/substr/substr_ktrla025zz0kil3207.sql
+++ models/BD/valuechain/substr/substr_ktrla025zz0kil3207.sql
@@ -6,7 +6,8 @@ with substr_ktrla025zz0kil3207 as (
         substr(raw_data, 19, 11)::varchar(11) as r005,
         substr(raw_data, 30, 15)::varchar(15) as r006,
         substr(raw_data, 1, 1)::varchar(1) as delflg,
-        ldts
+        ldts,
+        row_number
     from {{ source('snowpipe_db_valuechain', 'raw_ktrla025zz0kil3207') }}
 )
 select * from substr_ktrla025zz0kil3207
