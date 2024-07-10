cce3c985c237cf64c2e0628089d310f700913d83
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-07-10 13:50:46 +0900

    マスキングポリシー作成ファイルを削除

--- macros/create_masking_policy_cost_mask.sql
+++
@@ -1,4 +0,0 @@
-{% macro create_masking_policy_cost_mask(node_database,node_schema) %}
-CREATE MASKING POLICY IF NOT EXISTS {{node_database}}.{{node_schema}}.cost_mask AS (val number) 
-  RETURNS number -> 9999999
-{% endmacro %}
--- macros/create_masking_policy_sell_price_mask.sql
+++
@@ -1,4 +0,0 @@
-{% macro create_masking_policy_sell_price_mask(node_database,node_schema) %}
-CREATE MASKING POLICY IF NOT EXISTS {{node_database}}.{{node_schema}}.sell_price_mask AS (val number) 
-  RETURNS number -> 9999999
-{% endmacro %}
