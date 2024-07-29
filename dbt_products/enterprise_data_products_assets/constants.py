5ef9964774fc23186be2bca15bd8616089da5af3
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-07-29 17:13:57 +0900

    depsを移動

--- dbt_products/enterprise_data_products_assets/constants.py
+++ dbt_products/enterprise_data_products_assets/constants.py
@@ -5,15 +5,14 @@ from dagster_dbt import DbtCliResource
 
 dbt_project_dir = Path(__file__).joinpath("..", "..", "enterprise_data_products").resolve()
 dbt = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
+dbt.cli(["deps"]).wait()
 
 # dbtのマニフェストを読み込む
 # 読み込んだ「dbt_manifest_path」をasset.py側で参照する利用方法となる
 # If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
 # Otherwise, we expect a manifest to be present in the project's target directory.
 if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-    dbt.cli(["deps"]).wait()
     dbt_parse_invocation = dbt.cli(["parse"], manifest={}).wait()
     dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
 else:
-    dbt.cli(["deps"]).wait()
     dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
