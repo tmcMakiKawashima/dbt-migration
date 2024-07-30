0a96cf5c310d73c8d64399659fb2d8fa461ddbba
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-07-30 19:27:28 +0900

    depsを移動

--- dbt_products/enterprise_data_products_assets/constants.py
+++ dbt_products/enterprise_data_products_assets/constants.py
@@ -5,8 +5,7 @@ from dagster_dbt import DbtCliResource
 
 dbt_project_dir = Path(__file__).joinpath("..", "..", "enterprise_data_products").resolve()
 dbt = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
-
-dbt.cli(["deps"]).wait()
+# dbt.cli(["deps"]).wait()
 
 # dbtのマニフェストを読み込む
 # 読み込んだ「dbt_manifest_path」をasset.py側で参照する利用方法となる
