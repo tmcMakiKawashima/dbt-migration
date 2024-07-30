c66eb4596b8f29d98e62848135db935f8346e4d4
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-07-30 20:38:43 +0900

    add env, deps, target_path

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import os
 from dagster_dbt import DbtProject
 from pathlib import Path
 
-from dagster_dbt import DbtCliResource
 
 def get_env():
     if os.getenv("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT", "") == "1":
@@ -15,9 +14,6 @@ def get_env():
 dbt_project_path = Path(__file__).joinpath("..", "dbt_products", "enterprise_data_products").resolve()
 # dbt_project_path = Path(__file__).parent.parent.joinpath("dagster_dbt")
 # DBT_PROJECT_DIR = os.fspath(dbt_project_path)
-
-dbt = DbtCliResource(project_dir=os.fspath(dbt_project_path))
-dbt.cli(["deps"]).wait()
 
 
 dbt_project = DbtProject(
