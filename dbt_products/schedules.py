65aea96922b7c832d98d4f50dea9a581a2c3b2ea
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-07-31 19:44:23 +0900

    add hokyunoki_mieruka

--- dbt_products/schedules.py
+++ dbt_products/schedules.py
@@ -9,6 +9,18 @@ from .enterprise_data_products_assets.assets import DbtConfig, dbt_products_asse
 schedules = [
     build_schedule_from_dbt_selection(
         [dbt_products_assets],
+        job_name="job_build_hokyunoki_mieruka",
+        schedule_name="HOKYUNOKI_MIERUKA",
+        cron_schedule="30 19 * * *",
+        execution_timezone="Asia/Tokyo",
+        dbt_select="+dm_yohin_daiatari +dm_hosyuhin_noukishitei",
+        config=RunConfig(ops={
+            "dbt_products_assets": DbtConfig(dbt_vars={"DBT_JOB_NAME": ""})}),
+        # default_status=DefaultScheduleStatus.RUNNING,
+        tags={"ecs/cpu": "256", "ecs/memory": "1024"},
+    ),
+    build_schedule_from_dbt_selection(
+        [dbt_products_assets],
         job_name="dm_vinhis_specification_kokunai",
         schedule_name="VIN_SOUBI_KOKUNAI",
         cron_schedule="30 16 * * *",
