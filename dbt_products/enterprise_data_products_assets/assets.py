22f93b5ee58b9955a12924a59fbb13f24727ed2c
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-08-07 17:11:11 +0900

    add dbt test source

--- dbt_products/enterprise_data_products_assets/assets.py
+++ dbt_products/enterprise_data_products_assets/assets.py
@@ -7,13 +7,23 @@ from .constants import dbt_manifest_path
 
 
 class DbtConfig(Config):
+    # 環境変数にdbt test実行結果格納のスキーマ指定用の値を設定
     dbt_vars: dict = {
         "DBT_JOB_NAME": ""
     }
+    # dbt test --select source:* で指定する範囲を設定
+    source_test_list: list = []
 
 
 @dbt_assets(manifest=dbt_manifest_path)
 def dbt_products_assets(context: AssetExecutionContext, dbt: DbtCliResource, config: DbtConfig):
+    # dbt test --select source:* 実行
+    if len(config.source_test_list) > 0:
+        dbt_test_args = ["test", "--select"]
+        dbt_test_args += config.source_test_list
+        yield from dbt.cli(dbt_test_args, manifest=dbt_manifest_path).stream()
+        
+    # dbt build 実行
     dbt_build_args = ["build"]
     dbt_build_args += ["--target", os.getenv('dbt_profile_enterprise')]
     if len(config.dbt_vars["DBT_JOB_NAME"]) > 0:
--- dbt_products/schedules.py
+++ dbt_products/schedules.py
@@ -16,7 +16,9 @@ schedules = [
         execution_timezone="Asia/Tokyo",
         dbt_select="+dm_yohin_daiatari +dm_hosyuhin_noukishitei",
         config=RunConfig(ops={"dbt_products_assets": 
-                              DbtConfig(dbt_vars={"DBT_JOB_NAME": ""})
+                              DbtConfig(dbt_vars={"DBT_JOB_NAME": ""},
+                                        source_test_list=["source:*,+dm_yohin_daiatari",
+                                                          "source:*,+dm_hosyuhin_noukishitei"])
                              }
                         ),
         # default_status=DefaultScheduleStatus.RUNNING,
@@ -32,7 +34,8 @@ schedules = [
         execution_timezone="Asia/Tokyo",
         dbt_select="+dm_vinhis_specification_kokunai",
         config=RunConfig(ops={"dbt_products_assets": 
-                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_kokunai"})
+                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_kokunai"},
+                                        source_test_list=["source:*,+dm_vinhis_specification_kokunai"])
                              }
                         ),
         # default_status=DefaultScheduleStatus.RUNNING,
@@ -48,7 +51,8 @@ schedules = [
         execution_timezone="Asia/Tokyo",
         dbt_select="+dm_vinhis_specification_kaigai",
         config=RunConfig(ops={"dbt_products_assets": 
-                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_kaigai"})
+                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_kaigai"},
+                                        source_test_list=["source:*,+dm_vinhis_specification_kaigai"])
                              }
                         ),
         # default_status=DefaultScheduleStatus.RUNNING,
@@ -64,7 +68,8 @@ schedules = [
         execution_timezone="Asia/Tokyo",
         dbt_select="+dm_vinhis_specification_oem",
         config=RunConfig(ops={"dbt_products_assets": 
-                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_oem"})
+                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_oem"},
+                                        source_test_list=["source:*,+dm_vinhis_specification_oem"])
                              }
                         ),
         # default_status=DefaultScheduleStatus.RUNNING,
@@ -83,5 +88,23 @@ schedules = [
         # default_status=DefaultScheduleStatus.RUNNING,
         tags={"ecs/cpu": "256", "ecs/memory": "1024",
               "job_name": "job_snapshots"},
+    ),
+    # VIN装備（海外, OEM）※テスト用
+    build_schedule_from_dbt_selection(
+        [dbt_products_assets],
+        job_name="job_build_dm_vinhis_specification_kaigai_oem_test",
+        schedule_name="VIN_SOUBI_KAIGAI_OEM_TEST",
+        cron_schedule="30 16 * * *",
+        execution_timezone="Asia/Tokyo",
+        dbt_select="+dm_vinhis_specification_kaigai +dm_vinhis_specification_oem",
+        config=RunConfig(ops={"dbt_products_assets": 
+                              DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_vinhis_specification_kaigai_oem_test"},
+                                        source_test_list=["source:*,+dm_vinhis_specification_kaigai",
+                                                          "source:*,+dm_vinhis_specification_oem"])
+                             }
+                        ),
+        # default_status=DefaultScheduleStatus.RUNNING,
+        tags={"ecs/cpu": "256", "ecs/memory": "1024",
+              "job_name": "job_build_dm_vinhis_specification_kaigai"},
     ),
 ]
