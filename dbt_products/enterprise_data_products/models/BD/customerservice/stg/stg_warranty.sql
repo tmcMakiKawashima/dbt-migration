0d22be93f3ae4c3249bceaf3db1b2c159fe1fd57
SakakibaraTomoko <tomoko_sakakibara_zb@mail.toyota.co.jp>
2024-09-23 17:41:40 +0900

    fix 修正方針に沿い修正

--- dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty.sql
+++ dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty.sql
@@ -165,7 +165,7 @@ with stg_warranty as (
         mttime::timestamp_ntz as mttime, -- timestamp型
         _fivetran_synced::timestamp_ntz as ldts -- timestamp型
     from {{ source('fivetran_database_customerservice', 'raw_cep0001wrntdata') }}
-    where _fivetran_deleted = 'FALSE'
+    where _fivetran_deleted = 'false'
            {% if is_incremental() %}
                and ldts > (select max(ldts) from {{this}})
            {% endif %}
