1825ab70ee98be1d4b1ea7e72e3ea0bc8cc96d5a
nishino <saya_nishino_zb@mail.toyota.co.jp>
2024-09-25 16:09:09 +0900

    IQAS_市情報部外配布_規約に沿い修正

--- dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_shijoho_bugaihaihu.sql
+++ dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_shijoho_bugaihaihu.sql
@@ -26,7 +26,7 @@ with stg_shijoho_bugaihaihu as (
         mttime::timestamp_ntz as mttime, --timestamp型
         _fivetran_synced::timestamp_ntz as ldts --timestamp型
     from {{ source('fivetran_database_customerservice', 'raw_cep0715ftr_outside') }}
-     where _fivetran_deleted = 'FALSE'
+     where _fivetran_deleted = 'false'
 
 {% if is_incremental() %}
     and _fivetran_synced > (select max(ldts) from {{this}})
