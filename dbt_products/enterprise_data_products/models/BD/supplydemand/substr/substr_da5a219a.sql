6126536f6c6e35de14f7b6d9965f8e242de93643
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-09-26 20:36:17 +0900

    タブを半角スペースへ修正

--- dbt_products/enterprise_data_products/models/BD/supplydemand/substr/substr_da5a219a.sql
+++ dbt_products/enterprise_data_products/models/BD/supplydemand/substr/substr_da5a219a.sql
@@ -1,8 +1,8 @@
 with substr_da5a219a as (
     select
         substr(raw_data, 1, 1)::varchar(1) as mtkbn,
-        substr(raw_data, 2,	6)::varchar(6) as syadai_kt,
-        substr(raw_data, 8,	7)::varchar(7) as frmno,
+        substr(raw_data, 2, 6)::varchar(6) as syadai_kt,
+        substr(raw_data, 8, 7)::varchar(7) as frmno,
         substr(raw_data, 15, 5)::varchar(5) as hnbaitncd,
         substr(raw_data, 20, 4)::varchar(4) as haisya_y,
         substr(raw_data, 24, 2)::varchar(2) as haisya_m,
