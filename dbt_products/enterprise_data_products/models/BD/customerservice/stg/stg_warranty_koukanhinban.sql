96e3f56484a562e5b34164ea0dd5115cbfb1c7eb
SakakibaraTomoko <tomoko_sakakibara_zb@mail.toyota.co.jp>
2024-09-26 14:58:46 +0900

    fix スキーマ名

--- dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty_koukanhinban.sql
+++ dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty_koukanhinban.sql
@@ -25,7 +25,7 @@ with stg_warranty_koukanhinban as (
         mtuser::varchar(18) as mtuser, --なし
         mttime::timestamp_ntz as mttime, --timestamp型
         _fivetran_synced::timestamp_ntz as ldts --timestamp型
-    from {{ source('fivetran_database_customerservice', 'raw_cep0004parts') }}
+    from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep0004parts') }}
      where _fivetran_deleted = 'false'
 
 {% if is_incremental() %}                                                                                                              -- ② 増分の取り方を指定
