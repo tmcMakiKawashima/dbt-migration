ac6f568b81b083507cad66785fd2cff17d1bbf37
SakakibaraTomoko <tomoko_sakakibara_zb@mail.toyota.co.jp>
2024-09-26 15:37:22 +0900

    fix スキーマ名

--- dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty_chinesecomment.sql
+++ dbt_products/enterprise_data_products/models/BD/customerservice/stg/stg_warranty_chinesecomment.sql
@@ -28,7 +28,7 @@ with stg_warranty_chinesecomment as (
         mtuser::varchar(18) as mtuser, -- なし
         mttime::timestamp_ntz as mttime, -- timestamp型
         _fivetran_synced::timestamp_ntz as ldts -- timestamp型
-    from {{ source('fivetran_database_customerservice', 'raw_cep0768warr_cn_cmt') }}
+    from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep0768warr_cn_cmt') }}
     where _fivetran_deleted = 'false'
            {% if is_incremental() %}
                and ldts > (select max(ldts) from {{this}})
