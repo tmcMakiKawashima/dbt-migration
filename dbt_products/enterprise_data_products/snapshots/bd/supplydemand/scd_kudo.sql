3e718934660134e1ca84aad78b4683ba6a3db6fd
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2024-10-16 08:57:37 +0900

    生産日報STEP3成果物追加

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_kudo.sql
@@ -0,0 +1,22 @@
+{% snapshot scd_kudo %}
+{{
+config(
+unique_key="concat_ws('-',
+send_date,
+syasyu_cd,
+sno,
+syamei,
+dom_exp_cd,
+iff(model_name_cd is null, '', model_name_cd::varchar),
+iff(unit_cd is null, '', unit_cd::varchar),
+katashiki,
+powertrain_cd,
+iff(pp_sms is null, '', pp_sms)
+)",
+strategy='timestamp',
+updated_at='ldts',
+invalidate_hard_deletes=true,
+)
+}}
+select * from {{ ref('stg_kudo') }}
+{% endsnapshot %}
