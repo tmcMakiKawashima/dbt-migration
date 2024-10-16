e686be050eca71b372d115d8f386185b705ab0d4
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2024-10-16 09:05:08 +0900

    生産日報STEP3成果物追加

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_seisanpattern_syasyumei.sql
@@ -0,0 +1,15 @@
+{% snapshot scd_seisanpattern_syasyumei %}
+{{
+config(
+unique_key="concat_ws('-',
+send_date,
+jpn_local_div,
+model_name_cd
+)",
+strategy='timestamp',
+updated_at='ldts',
+invalidate_hard_deletes=true,
+)
+}}
+select * from {{ ref('stg_seisanpattern_syasyumei') }}
+{% endsnapshot %}
