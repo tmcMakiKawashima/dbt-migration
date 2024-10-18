39de2887f7d4ea2680b5f532ced4d2f3bfe79f94
Watanabe Mina <m-watanabe@toyotasystems.com>
2024-10-18 05:32:47 +0000

    補給品番基本情報提供_部位中分類検索情報 新規作成

---
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_buicyubunruikensakujoho.sql
@@ -0,0 +1,14 @@
+with stg_buicyubunruikensakujoho as (
+    select
+        mntkbn::varchar(1) as mntkbn,
+        ctlgcd::varchar(6) as ctlgcd,
+        daibnrcd::varchar(4) as daibnrcd,
+        chubnrcd::varchar(4) as chubnrcd,
+        rtrim(hansno, ' 　')::varchar(7) as hansno,
+        ldts,
+        line_number,
+        rank() over(partition by ctlgcd, daibnrcd, chubnrcd order by ldts desc, mntkbn asc, line_number desc) aggkey
+    from {{ref('substr_k9jfv218')}}
+)
+
+select * exclude(line_number, aggkey) from stg_buicyubunruikensakujoho where aggkey = 1
