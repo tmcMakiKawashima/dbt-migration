2fdc82dd9502ec64910b95502bacd73405dc3e5b
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2024-10-21 11:06:40 +0900

    規約違反:可読性

--- dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_syameigroup_nippo.sql
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_syameigroup_nippo.sql
@@ -1,22 +1,26 @@
 {% snapshot scd_syameigroup_nippo %}
+
 {{
-config(
-unique_key="concat_ws('-',
-send_date,
-ymd,
-jpn_local_div,
-areacd,
-country_cd,
-psc,
-affiliate_cd,
-plant_cd,
-line_cd,
-daily_report_car_grcd
-)",
-strategy='timestamp',
-updated_at='ldts',
-invalidate_hard_deletes=true,
-)
+    config(
+    unique_key="concat_ws('-',
+                send_date,
+                ymd,
+                jpn_local_div,
+                areacd,
+                country_cd,
+                psc,
+                affiliate_cd,
+                plant_cd,
+                line_cd,
+                daily_report_car_grcd
+                )",
+
+    strategy='timestamp',
+    updated_at='ldts',
+    invalidate_hard_deletes=true,
+    )
 }}
+
 select * from {{ ref('stg_syameigroup_nippo') }}
+
 {% endsnapshot %}
