ea254b3a2b643b865f8765e2fb14dc021eb02893
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2024-10-21 11:02:42 +0900

    規約違反:可読性

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_seisanpattern_syasyumei.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_seisanpattern_syasyumei.sql
@@ -1,28 +1,30 @@
-{{ config(
-materialized='incremental',
-incremental_strategy = 'append',
-pre_hook = "
-{% if is_incremental() %}
-delete from {{this}}
-{% endif %}
-"
-) }}
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy = 'append',
+        pre_hook = "
+            {% if is_incremental() %}
+                delete from {{this}}
+            {% endif %}
+        "
+    )
+}}
 
 with stg_seisanpattern_syasyumei as (
-select 
-send_date::varchar(20) as send_date,
-to_number(jpn_local_div)::number(38,0) as jpn_local_div,
-jpn_local::varchar(255) as jpn_local,
-to_number(model_name_cd)::number(38,0) as model_name_cd,
-model_name::varchar(255) as model_name,
-ldts::timestamp_ntz(9) as ldts,
-row_number() over (
-partition by
-send_date,jpn_local_div,model_name_cd
-order by line_number desc
-) aggkey
-from {{ source('snowpipe_db_supplydemand','raw_m_vehicle_class_name') }}
-where ldts = (select max(ldts) from {{ source('snowpipe_db_supplydemand','raw_m_vehicle_class_name') }})
+    select 
+        send_date::varchar(20) as send_date,
+        to_number(jpn_local_div)::number(38,0) as jpn_local_div,
+        jpn_local::varchar(255) as jpn_local,
+        to_number(model_name_cd)::number(38,0) as model_name_cd,
+        model_name::varchar(255) as model_name,
+        ldts::timestamp_ntz(9) as ldts,
+        row_number() over (
+            partition by
+                send_date,jpn_local_div,model_name_cd
+            order by line_number desc
+        ) aggkey
+    from {{ source('snowpipe_db_supplydemand','raw_m_vehicle_class_name') }}
+    where ldts = (select max(ldts) from {{ source('snowpipe_db_supplydemand','raw_m_vehicle_class_name') }})
 )
 select * exclude(aggkey)
 from stg_seisanpattern_syasyumei
--- dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_seisanpattern_syasyumei.sql
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_seisanpattern_syasyumei.sql
@@ -1,15 +1,19 @@
 {% snapshot scd_seisanpattern_syasyumei %}
+
 {{
-config(
-unique_key="concat_ws('-',
-send_date,
-jpn_local_div,
-model_name_cd
-)",
-strategy='timestamp',
-updated_at='ldts',
-invalidate_hard_deletes=true,
-)
+    config(
+        unique_key="concat_ws('-',
+                    send_date,
+                    jpn_local_div,
+                    model_name_cd
+                    )",
+
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=true,
+    )
 }}
+
 select * from {{ ref('stg_seisanpattern_syasyumei') }}
+
 {% endsnapshot %}
