af2c8d2545e158b9a75239d39e784200f0ee9c36
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-08 00:31:56 +0900

    MIKADO_JNET_部門_新規作成

---
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bumon.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = ['bmncd'],
+    incremental_strategy = 'append',
+    pre_hook="
+      {% if is_incremental() %}
+      delete from {{this}}
+      {% endif %}
+    "
+  )
+}}
+
+with stg_bumon as (
+    select
+        bmncd::varchar(2) as bmncd,
+        bmnstaymd::varchar(8) as bmnstaymd,
+        bmnendymd::varchar(8) as bmnendymd,
+        bmnnam::varchar(30) as bmnnam,
+        rskbmnnam::varchar(6) as rskbmnnam,
+        bmnno::varchar(2) as bmnno,
+        ldts,
+        row_number() over(partition by bmncd
+                       order by ldts desc, line_number desc) as aggkey
+    from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20055')}}
+    where bmnstaymd =< to_varchar(current_date,'yyyymmdd')
+    {% if is_incremental() %}
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+    {% endif %}
+)
+select *  exclude(aggkey) from stg_bumon where aggkey = 1
