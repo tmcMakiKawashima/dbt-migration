8627c2f0288238fea04254d3b083e3f5a176dd45
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-07 23:34:54 +0900

    MIKADO_JNET_職種_新規作成

---
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_syokusyu.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = ['sykscd'],
+    incremental_strategy = 'append',
+    pre_hook="
+      {% if is_incremental() %}
+      delete from {{this}}
+      {% endif %}
+    "
+  )
+}}
+
+with stg_syokusyu as (
+    select
+        sykscd::varchar(2) as sykscd,
+        syksnam::varchar(20) as syksnam,
+        ldts,
+        row_number() over(partition by sykscd
+                       order by ldts desc, line_number desc) as aggkey
+    from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20064')}}
+    {% if is_incremental() %}
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+    {% endif %}
+)
+select *  exclude(aggkey) from stg_syokusyu where aggkey = 1
