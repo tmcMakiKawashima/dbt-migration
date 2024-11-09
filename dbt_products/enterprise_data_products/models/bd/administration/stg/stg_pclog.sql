c0bd2671e3f03703cbc32da1d9db0bf48810b7b9
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 21:19:54 +0900

    MIKADO_JNET_ＰＣ起動＿停止_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_pclog.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_pclog.sql
@@ -144,7 +144,7 @@ with stg_pclog as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20062')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20062')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_pclog where aggkey = 1
