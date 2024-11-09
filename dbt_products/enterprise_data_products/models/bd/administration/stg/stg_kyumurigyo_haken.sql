db8080eda4dcd7c8d65b18a47de970cdb23d1d71
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 20:00:04 +0900

    MIKADO_JNET_休務予定＿派遣_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kyumurigyo_haken.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kyumurigyo_haken.sql
@@ -35,7 +35,7 @@ with stg_kyumurigyo_haken as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20059')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20059')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_kyumurigyo_haken where aggkey = 1
