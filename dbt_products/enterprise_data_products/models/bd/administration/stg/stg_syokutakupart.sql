29296a675ad1036c35b98237af9324b617c09893
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 12:49:44 +0900

    MIKADO_JNET_嘱託パート区分マスター_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_syokutakupart.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_syokutakupart.sql
@@ -21,7 +21,7 @@ with stg_syokutakupart as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20065')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20065')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_syokutakupart where aggkey = 1
