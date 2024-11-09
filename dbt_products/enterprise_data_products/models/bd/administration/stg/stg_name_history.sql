780cffb5d7a57ef4ade0e8ca083e3ca271ab1fa1
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 11:32:13 +0900

    MIKADO_JNET_氏名変更履歴_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_name_history.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_name_history.sql
@@ -31,7 +31,7 @@ with stg_name_history as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20069')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20069')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_name_history where aggkey = 1
