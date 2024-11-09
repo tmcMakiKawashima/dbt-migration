cfe4f46dae626c19318a7247e1a910238e528c63
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 20:30:13 +0900

    MIKADO_JNET_月次勤怠実績明細_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kintai_monthly.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kintai_monthly.sql
@@ -72,7 +72,7 @@ with stg_kintai_monthly as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20060')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20060')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_kintai_monthly where aggkey = 1
