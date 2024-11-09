c5d99345aa6f7ff95a4ef05aaf8f24cf87fcc46f
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 22:32:51 +0900

    MIKADO_JNET_部門_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bumon.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bumon.sql
@@ -21,11 +21,11 @@ with stg_bumon as (
         bmnno::varchar(2) as bmnno,
         ldts,
         row_number() over(partition by bmncd
-                       order by ldts desc, line_number desc) as aggkey
+                       order by ldts desc, bmnstaymd desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20055')}}
-    where bmnstaymd =< to_varchar(current_date,'yyyymmdd')
+    where bmnstaymd <= to_varchar(current_date,'yyyymmdd')
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20055')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_bumon where aggkey = 1
