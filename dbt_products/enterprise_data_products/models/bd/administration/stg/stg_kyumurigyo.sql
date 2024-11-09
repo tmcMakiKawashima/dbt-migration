599471dd908a6a4bb3019cd3e5eb10a6d7e6388f
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 15:40:22 +0900

    MIKADO_JNET_休務離業予定申請_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kyumurigyo.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_kyumurigyo.sql
@@ -40,7 +40,7 @@ with stg_kyumurigyo as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20057')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20057')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_kyumurigyo where aggkey = 1
