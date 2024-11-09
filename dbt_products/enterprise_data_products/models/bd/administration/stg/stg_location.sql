39040c179a108dbd8ed14fa97a4ee8f09e0994d2
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-11-09 14:55:42 +0900

    MIKADO_JNET_ロケーション_新規作成2

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_location.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_location.sql
@@ -28,7 +28,7 @@ with stg_location as (
                        order by ldts desc, line_number desc) as aggkey
     from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20043')}}
     {% if is_incremental() %}
-        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20070')}})
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20043')}})
     {% endif %}
 )
 select *  exclude(aggkey) from stg_location where aggkey = 1
