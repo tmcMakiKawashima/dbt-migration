750d17eaac8094c94d4cc264cdeb92d4feaa1c7e
tomoyuki.okada_ctc <tomoyuki.okada.487@ctc-g.co.jp>
2024-12-09 16:08:25 +0900

    EPC_調達品番情報 D層改修

---
+++ dbt_products/enterprise_data_products/snapshots/bd/valuechain/scd_tyotatsuhinbanjoho.sql
@@ -0,0 +1,20 @@
+{% snapshot scd_tyotatsuhinbanjoho %}
+
+{{
+    config(
+        unique_key="concat_ws('-',
+                    hinban,
+                    dntikicd,
+                    cthin,
+                    tktermk,
+                    tktermm)",
+
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=true,
+    )
+}}
+
+select * from {{ ref('stg_tyotatsuhinbanjoho') }}
+
+{% endsnapshot %}
