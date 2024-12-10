dab2f85650068ec90576734574625e1fb083a783
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-12-10 15:35:54 +0900

    snapshot削除、室課マスタ/係Gマスタ再追加、スケジューラを各テーブル単位へ変更

---
+++ dbt_products/schedules_items/administration/public/schedule_stg_mikado_jnet_kakarigroup.py
@@ -0,0 +1,21 @@
+from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
+from dagster import DefaultScheduleStatus, RunConfig
+from dagster_dbt import build_schedule_from_dbt_selection
+
+# MIKADO JNET
+schedule_stg_mikado_jnet = build_schedule_from_dbt_selection(
+    [dbt_products_assets],
+    job_name="job_build_stg_mikado_jnet_kakarigroup",
+    schedule_name="MIKADO_JNET",
+    cron_schedule="45 06 * * *",
+    execution_timezone="Asia/Tokyo",
+    dbt_select="+stg_kakarigroup",
+    config=RunConfig(ops={"dbt_products_assets":
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_jnet_kakarigroup"},
+                                    source_test_list=["source:*,+stg_kakarigroup"])
+                          }
+                     ),
+    default_status=DefaultScheduleStatus.RUNNING,
+    tags={"ecs/cpu": "256", "ecs/memory": "1024",
+          "job_name": "job_build_stg_mikado_jnet_kakarigroup"},
+)
---
+++ dbt_products/schedules_items/administration/public/schedule_stg_mikado_jnet_syokutakupart.py
@@ -0,0 +1,21 @@
+from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
+from dagster import DefaultScheduleStatus, RunConfig
+from dagster_dbt import build_schedule_from_dbt_selection
+
+# MIKADO JNET
+schedule_stg_mikado_jnet = build_schedule_from_dbt_selection(
+    [dbt_products_assets],
+    job_name="job_build_stg_mikado_jnet_syokutakupart",
+    schedule_name="MIKADO_JNET",
+    cron_schedule="45 06 * * *",
+    execution_timezone="Asia/Tokyo",
+    dbt_select="+stg_syokutakupart",
+    config=RunConfig(ops={"dbt_products_assets":
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_jnet_syokutakupart"},
+                                    source_test_list=["source:*,+stg_syokutakupart"])
+                          }
+                     ),
+    default_status=DefaultScheduleStatus.RUNNING,
+    tags={"ecs/cpu": "256", "ecs/memory": "1024",
+          "job_name": "job_build_stg_mikado_jnet_syokutakupart"},
+)
