23bfdcb232d0aa9ed835b26a919fd7ebd4a6713a
Watanabe Mina <m-watanabe@toyotasystems.com>
2024-12-16 11:51:35 +0900

    refテーブルを変更

--- dbt_products/enterprise_data_products/models/de/vinhis/spec/temporary/dm_vinhis_specification_kokunai/tmp55_dm_vinhis_specification_kokunai.sql
+++ dbt_products/enterprise_data_products/models/de/vinhis/spec/temporary/dm_vinhis_specification_kokunai/tmp55_dm_vinhis_specification_kokunai.sql
@@ -3,16 +3,16 @@ with
     tmp50 as (select * from {{ ref('tmp50_dm_vinhis_specification_kokunai') }}),
     syaryo_index as (
         select
-            frame_cls, -- フレーム区分
-            frame_no, -- フレームNo
-            sales_katashiki -- 販売型式
-        from {{source('vinhis_db_iqas','raw_dm_e010index')}} -- 車両INDEX
+            syadai_kt, -- フレーム区分
+            frmno, -- フレームNo
+            hanbai_kt -- 販売型式
+        from {{ref('stg_syaryoindex')}} -- 車両INDEX
     )
 select
     tmp50.* exclude(hkata), 
     -- tmp50の販売型式がNULLの場合、車両INDEXの販売型式を選択する
-    coalesce(tmp50.hkata, syaryo_index.sales_katashiki) as hkata
+    coalesce(tmp50.hkata, syaryo_index.hanbai_kt) as hkata
 from tmp50
 left outer join syaryo_index
-  on tmp50.syadai_kt = syaryo_index.frame_cls
- and tmp50.frmno = syaryo_index.frame_no
+  on tmp50.syadai_kt = syaryo_index.syadai_kt
+ and tmp50.frmno = syaryo_index.frmno
