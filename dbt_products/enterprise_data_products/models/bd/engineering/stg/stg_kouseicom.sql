81cee7cf8f8ddc522b2a9283853cbd35ebe50033
Nishino <saya_nishino_zb@mail.toyota.co.jp>
2024-12-16 15:44:26 +0900

    SMS構成-BD-構成コメント

---
+++ dbt_products/enterprise_data_products/models/bd/engineering/stg/stg_kouseicom.sql
@@ -0,0 +1,31 @@
+{{ 
+  config(
+    materialized='incremental',
+    unique_key = ['jigyoutai','syasyu','siyoubui','oyahin','gc','kohin','comkbn','torokujunk'],
+    incremental_strategy = 'merge'
+  )
+ }}
+
+with stg_kouseicom as (
+    select
+        jigyoutai::varchar(2) as jigyoutai, --なし
+        syasyu::varchar(4) as syasyu,  --なし
+        siyoubui::varchar(10) as siyoubui,  --なし
+        oyahin::varchar(10) as oyahin,  --なし
+        gc::varchar(2) as gc,  --なし
+        kohin::varchar(10) as kohin,  --なし
+        comkbn::varchar(2) as comkbn,  --なし
+        com::varchar(100) as com,  --なし
+        torokujunk::varchar(9) as torokujunk,  --なし
+        torokujunm::varchar(9) as torokujunm,  --なし
+        mttime::varchar(16) as mttime,  --なし
+        _fivetran_synced::timestamp_ntz as ldts --timestamp型
+    from {{ source('fivetran_database_oraclerds_orcl_sms_dxpfy2d', 'raw_cyp03kouseicom') }}
+     where _fivetran_deleted = 'false'
+
+{% if is_incremental() %}
+    and _fivetran_synced > (select max(ldts) from {{this}})
+{% endif %}
+
+    )
+select * from stg_kouseicom
