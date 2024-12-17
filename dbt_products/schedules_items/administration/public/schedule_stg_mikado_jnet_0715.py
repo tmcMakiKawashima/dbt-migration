7522882e97fc94481a1e5fb8be8249d7b42ea83e
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2024-12-17 17:45:05 +0900

    ジョブのまとめ方の変更

--- dbt_products/schedules_items/administration/public/schedule_stg_mikado_jnet_0715.py
+++ dbt_products/schedules_items/administration/public/schedule_stg_mikado_jnet_0715.py
@@ -3,19 +3,19 @@ from dagster import DefaultScheduleStatus, RunConfig
 from dagster_dbt import build_schedule_from_dbt_selection
 
 # MIKADO JNET
-schedule_stg_mikado_jnet_syokusyu = build_schedule_from_dbt_selection(
+schedule_stg_mikado_jnet_0715 = build_schedule_from_dbt_selection(
     [dbt_products_assets],
-    job_name="job_build_stg_mikado_jnet_syokusyu",
-    schedule_name="MIKADO_JNET_SYOKUSYU",
-    cron_schedule="45 06 * * *",
+    job_name="job_build_stg_mikado_jnet_0715",
+    schedule_name="MIKADO_JNET_0715",
+    cron_schedule="15 07 * * *",
     execution_timezone="Asia/Tokyo",
-    dbt_select="+stg_syokusyu",
+    dbt_select="+stg_businesstrip",
     config=RunConfig(ops={"dbt_products_assets":
-                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_jnet_syokusyu"},
-                                    source_test_list=["source:*,+stg_syokusyu"])
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_jnet_businesstrip"},
+                                    source_test_list=["source:*,+stg_businesstrip"])
                           }
                      ),
     default_status=DefaultScheduleStatus.RUNNING,
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
-          "job_name": "job_build_stg_mikado_jnet_syokusyu"},
+          "job_name": "job_build_stg_mikado_jnet_0715"},
 )
