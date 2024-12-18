9a5f6f50c7ee17071e6a3238537607219373b5b5
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2024-12-18 16:24:20 +0900

    結合条件を修正

--- dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_figbunkaiinfo.sql
+++ dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_figbunkaiinfo.sql
@@ -4,13 +4,16 @@ with
     ),
     stg_illustcomment as (
         select
+            langkbn, -- 言語区分
             ilstcmtno, -- イラストコメントNO
             ilstcmt -- イラストコメント
         from {{ ref('stg_illustcomment') }} -- イラストコメント
     )
 select
     tmp20_dm_figbunkaiinfo.*,
-    stg_illustcomment.ilstcmt
+    stg_illustcomment.ilstcmt,
+    current_timestamp::timestamp_ntz as ldts -- 作成日時
 from tmp20_dm_figbunkaiinfo
 inner join stg_illustcomment
   on tmp20_dm_figbunkaiinfo.ilstcmtno = stg_illustcomment.ilstcmtno
+ and tmp20_dm_figbunkaiinfo.langkbn = stg_illustcomment.langkbn
