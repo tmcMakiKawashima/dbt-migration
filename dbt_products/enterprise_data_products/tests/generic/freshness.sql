1f1e07d6fa924d93824f82a1b4460a98533899d5
Kuboi Erika <e-kuboi@toyotasystems.com>
2024-12-23 10:01:36 +0900

    fix

--- dbt_products/enterprise_data_products/tests/generic/freshness.sql
+++ dbt_products/enterprise_data_products/tests/generic/freshness.sql
@@ -1,9 +1,8 @@
 {% test freshness(model, column_name, hour, skip_dayname) %}
 
     {{ config(severity = 'error') }}
-
-    select * from  {{ model }}
-     where datediff(hour, {{ column_name }}, current_timestamp)
+    select max({{ column_name }}) as max_col from {{ model }}
+    having datediff(hour, max({{ column_name }}), current_timestamp)
            > iff(dayname(current_timestamp) in {{ skip_dayname }}, null, {{ hour }})
-
+           
 {% endtest %}
