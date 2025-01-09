69f0c2633ca172712ea61833964efcfe62f80825
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-01-09 11:19:22 +0900

    change hybrid_table

--- dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_trimcodejoho.sql
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_trimcodejoho.sql
@@ -1,3 +1,18 @@
+{{
+    config (
+        post_hook=
+            'create or replace hybrid table valuechain_db.public.stg_trimcodejoho (
+                ctlgcd varchar(6) not null,
+                syasyu_cd varchar(4) not null,
+                hinban varchar(12) not null,
+                trmcd varchar(2) not null,
+                figno varchar(4) not null,
+                ldts timestamp_ntz(9),
+                constraint stg_trimcodejoho_ctlgcd_syasyu_cd_hinban_trmcd_figno_uk primary key (ctlgcd, syasyu_cd, hinban, trmcd, figno) rely
+            ) as select * from {{this}}'
+    )
+}}
+
 with stg_trimcodejoho as (
     select
         mntkbn::varchar(1) as mntkbn, 
