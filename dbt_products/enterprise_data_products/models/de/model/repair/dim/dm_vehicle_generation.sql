b7cfd20c01e5000bf2147b8261e74cd4fb4ba319
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-01-10 14:26:00 +0900

    change hybrid_table

--- dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_vehicle_generation.sql
+++ dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_vehicle_generation.sql
@@ -1,3 +1,32 @@
+{{
+    config (
+        post_hook=
+            'create or replace hybrid table model_db.repair.dm_vehicle_generation (
+                cd_model_nm varchar(5) not null,
+                no_ofr_alt_pub varchar(16) not null,
+                dd_mdly varchar(4) not null,
+                cd_fll_mdl varchar(20) not null,
+                cd_vtype varchar(6) not null,
+                no_pub varchar(10) not null,
+                dd_pubbnd_trmfrym varchar(6) not null,
+                cd_pubbnd varchar(6),
+                no_pub_termfrid varchar(3),
+                kb_lang varchar(1),
+                kb_pub_type varchar(1),
+                kb_cont_type varchar(2),
+                cd_trgt varchar(1),
+                cd_brand varchar(2),
+                dt_server_opn varchar(12),
+                kb_pub_form varchar(1),
+                kb_model_brand varchar(1),
+                mj_j_model_nm varchar(60),
+                mj_e_model_nm varchar(60),
+                ldts timestamp_ntz(9),
+                constraint dm_vehicle_generation_cd_model_nm_no_ofr_alt_pub_dd_mdly_cd_fll_mdl_cd_vtype_no_pub_dd_pubbnd_trmfrym_uk primary key (cd_model_nm, no_ofr_alt_pub, dd_mdly, cd_fll_mdl, cd_vtype, no_pub, dd_pubbnd_trmfrym) rely
+            ) as select * from {{this}}'
+    )
+}}
+
 with
     tmp10_dm_vehicle_generation as (
         select * from {{ ref('tmp10_dm_vehicle_generation') }}
