090ecab8fdaa4a9717921ac30b41deb3ece466e9
tomoyuki.okada_ctc <tomoyuki.okada.487@ctc-g.co.jp>
2025-01-10 14:56:36 +0900

    T-LINX_EPC_FIG分解情報_ハイブリッドテーブル実装

--- dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_figbunkaiinfo.sql
+++ dbt_products/enterprise_data_products/models/de/model/repair/dim/dm_figbunkaiinfo.sql
@@ -1,3 +1,27 @@
+{{
+    config (
+        post_hook=
+            'create or replace hybrid table model_db.repair.dm_figbunkaiinfo (
+                 ctlgcd varchar(6) not null,
+                 figno varchar(4) not null,
+                 bnkren4 varchar(4) not null,
+                 hansno varchar(7) not null,
+                 jissijikik varchar(6) not null,
+                 jissijikim varchar(6) not null,
+                 epckataptno varchar(6),
+                 ilstcmtno varchar(5) not null,
+                 katahgnjiki varchar(148),
+                 ilstqty varchar(8),
+                 langkbn varchar(3) not null,
+                 figmei varchar(240),
+                 bnkimgflmei varchar(8),
+                 ilstcmt varchar(140),
+                 ldts timestamp_ntz(9),
+                 constraint dm_figbunkaiinfo_ctlgcd_figno_langkbn_bnkren4_hansno_jissijikik_jissijikim_ilstcmtno_uk primary key (ctlgcd, figno, langkbn, bnkren4, hansno, jissijikik, jissijikim, ilstcmtno) rely 
+            ) as select *, current_timestamp as ldts from {{this}}'
+    )
+}}
+
 with
     tmp20_dm_figbunkaiinfo as (
         select * from {{ ref('tmp20_dm_figbunkaiinfo') }}
