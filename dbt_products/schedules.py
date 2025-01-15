1133db69702b8b3650549bf4601968aafbf494b2
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-01-15 09:33:36 +0900

    fix schedules.py

--- dbt_products/schedules.py
+++ dbt_products/schedules.py
@@ -23,6 +23,8 @@ from .schedules_items.valuechain.public.schedule_stg_t_linx_epc import schedule_
 
 """ E """
 """ model repair """
+from .schedules_items.model.repair.schedule_dm_cataloghinmei import schedule_dm_cataloghinmei
+from .schedules_items.model.repair.schedule_dm_figbunkaiinfo import schedule_dm_figbunkaiinfo
 from .schedules_items.model.repair.schedule_dm_vehicle_generation import schedule_dm_vehicle_generation
 """ vinhis maint """
 from .schedules_items.vinhis.maint.schedule_dm_vinhis_maint import schedule_dm_vinhis_maint
@@ -75,6 +77,10 @@ schedules.append(schedule_stg_t_linx_epc)
 
 """ E層作成ジョブ """
 """ model repair """
+# EPCカタログ品名
+schedules.append(schedule_dm_cataloghinmei)
+# FIG分解情報
+schedules.append(schedule_dm_figbunkaiinfo)
 # 車種世代
 schedules.append(schedule_dm_vehicle_generation)
 """ vinhis maint """
