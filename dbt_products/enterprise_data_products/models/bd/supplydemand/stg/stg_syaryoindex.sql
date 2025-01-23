71f31f89e20ee51b6dcc070844c58bc36d18d5d0
da-dxpf-taisei_goto_za <taisei_goto_za@mail.toyota.co.jp>
2025-01-23 09:46:16 +0900

    車両INDEX-結合処理追加

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_syaryoindex.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_syaryoindex.sql
@@ -30,113 +30,113 @@ with
         from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep1001index') }}
         where _fivetran_deleted = 'false'
 
-{% if is_incremental() %}
-    and _fivetran_synced > (select max(ldts) from {{ this }})
-{% endif %}
-
+        {% if is_incremental() %}
+            and _fivetran_synced > (select max(ldts) from {{ this }})
+        {% endif %}
+        
     )
 select  
-stg_syaryoindex.index_cls::varchar(3) as index_cls, --なし
-rtrim(stg_syaryoindex.frame_cls, ' 　')::varchar(6) as syadai_kt, -- 右ブランク
-stg_syaryoindex.frame_no::varchar(7) as frmno, --なし
-stg_syaryoindex.vin_wmi::varchar(3) as wmi, --なし
-stg_syaryoindex.vin_vds::varchar(6) as vds, --なし
-stg_syaryoindex.vin_vis::varchar(8) as vis, --なし
-stg_syaryoindex.vin_my::varchar(1) as my, --なし
-stg_syaryoindex.vin_cd::varchar(1) as cd, --なし
-wmi || vds || vis as vin, --結合
-syadai_kt || '-' || frmno as frame_cls_no, --結合
-wmi || vds || vis || syadai_kt || '-' || frmno as vin_frame_cls_no, --結合
-stg_syaryoindex.lo_date_y::varchar(4) as lo_date_y, --なし
-stg_syaryoindex.lo_date_m::varchar(2) as lo_date_m, --なし
-stg_syaryoindex.lo_date_d::varchar(2) as lo_date_d, --なし
-try_to_date(lo_date_y || lo_date_m || lo_date_d, 'yyyymmdd' ) as lo_date_ymd, -- date型
-stg_syaryoindex.lo_reg_month::number(3) as lo_reg_month, --なし
-stg_syaryoindex.model_major_cls_code::varchar(2) as model_major_cls_code, --なし
-stg_syaryoindex.sms_veh_code::varchar(4) as sms_syasyu_cd, --なし
-stg_syaryoindex.model_veh_code::varchar(4) as model_syasyu_cd, --なし
-stg_syaryoindex.ident_no::varchar(10) as idtno, --なし
-rtrim(stg_syaryoindex.katashiki, ' 　')::varchar(15) as haisya_kt, -- 右ブランク
-rtrim(stg_syaryoindex.sales_katashiki, ' 　')::varchar(20) as  hanbai_kt, -- 右ブランク
-stg_syaryoindex.body_type::varchar(1) as body_type, --なし
-stg_syaryoindex.eng_no::varchar(7) as eng_no, --なし
-stg_syaryoindex.eng_type::varchar(20) as eng_type, --なし
-stg_syaryoindex.tm_type::varchar(6) as tm_type, --なし
-stg_syaryoindex.rdf_type::varchar(3) as rdf_type, --なし
-stg_syaryoindex.spec_bit::varchar(6) as spec_bit, --なし
-stg_syaryoindex.drive_system::varchar(5) as drive_system, --なし
-stg_syaryoindex.airbag_ecu_part_no::varchar(5) as airbag_ecu_part_no, --なし
-stg_syaryoindex.eng_plnt_code::varchar(2) as eng_plnt_code, --なし
-stg_syaryoindex.tire_maker::varchar(2) as tire_maker, --なし
-stg_syaryoindex.production_area_cls::varchar(1) as production_area_cls, --なし
-stg_syaryoindex.production_country_code::varchar(3) as production_country_code, --なし
-stg_syaryoindex.onec_line_code::varchar(2) as ichiken_line_code, --なし
-stg_syaryoindex.onec_date_y::varchar(4) as ichikenyy, --なし
-stg_syaryoindex.onec_date_m::varchar(2) as ichikenmm, --なし
-stg_syaryoindex.onec_date_d::varchar(2) as ichikendd, --なし
-try_to_date(ichikenyy || ichikenmm || ichikendd, 'yyyymmdd' ) as ichikenymd, -- date型
-stg_syaryoindex.endc_line_code::varchar(2) as syuken_line_code, --なし
-stg_syaryoindex.endc_date_y::varchar(4) as syukenyy, --なし
-stg_syaryoindex.endc_date_m::varchar(2) as syukenmm, --なし
-stg_syaryoindex.endc_date_d::varchar(2) as syukendd, --なし
-try_to_date(syukenyy || syukenmm || syukendd, 'yyyymmdd' ) as syukenymd, -- date型
-stg_syaryoindex.endc_time::varchar(4) as syuken_time, --なし
-stg_syaryoindex.pack_line_code::varchar(2) as konpo_line_code, --なし
-stg_syaryoindex.pack_date_y::varchar(4) as konpoyy, --なし
-stg_syaryoindex.pack_date_m::varchar(2) as konpomm, --なし
-stg_syaryoindex.pack_date_d::varchar(2) as konpodd, --なし
-try_to_date(konpoyy ||  konpomm || konpodd, 'yyyymmdd' ) as konpoymd, -- date型
-stg_syaryoindex.ident_line_code::varchar(2) as ident_line_code, --なし
-stg_syaryoindex.pack_form::varchar(1) as pack_form, --なし
-stg_syaryoindex.export_cls::varchar(1) as export_cls, --なし
-stg_syaryoindex.use_cls::varchar(1) as use_cls, --なし
-stg_syaryoindex.region_cls::varchar(1) as region_cls, --なし
-stg_syaryoindex.dest_code::varchar(3) as dest_code, --なし
-stg_syaryoindex.deliv_deal_code::varchar(5) as deliv_deal_code, --なし
-stg_syaryoindex.deliv_date_y::varchar(4) as haisyayy, --なし
-stg_syaryoindex.deliv_date_m::varchar(2) as haisyamm, --なし
-stg_syaryoindex.deliv_date_d::varchar(2) as haisyadd, --なし
-try_to_date(haisyayy ||  haisyamm || haisyadd, 'yyyymmdd' ) as haisyaymd, -- date型
-stg_syaryoindex.reg_state_code::varchar(3) as reg_state_code, --なし
-stg_syaryoindex.reg_country_code::varchar(5) as reg_country_code, --なし
-stg_syaryoindex.reg_deal_code::varchar(8) as reg_deal_code, --なし
-stg_syaryoindex.veh_reg_date_y::varchar(4) as torokuyy, --なし
-stg_syaryoindex.veh_reg_date_m::varchar(2) as torokumm, --なし
-stg_syaryoindex.veh_reg_date_d::varchar(2) as torokudd, --なし
-try_to_date(torokuyy ||  torokumm || torokudd, 'yyyymmdd' ) as torokuyymmdd, -- date型
-stg_syaryoindex.dest_detail::varchar(1) as dest_detail, --なし
-stg_syaryoindex.b_veh_cls::varchar(1) as b_veh_cls, --なし
-stg_syaryoindex.b_veh_date::varchar(8) as b_veh_date, --なし
-stg_syaryoindex.prod_data_id::varchar(1) as prod_data_id, --なし
-stg_syaryoindex.frame_recog_cls::varchar(1) as frame_recog_cls, --なし
-stg_syaryoindex.veh_name_code::varchar(2) as veh_name_code, --なし
-stg_syaryoindex.ext_color_code::varchar(4) as ext_color_code, --なし
-stg_syaryoindex.int_code::varchar(4) as int_code, --なし
-stg_syaryoindex.brand_cls::varchar(1) as brand_cls, --なし
-stg_syaryoindex.tm_plnt_code::varchar(2) as tm_plnt_code, --なし
-stg_syaryoindex.reg_date_y_airis::varchar(4) as reg_date_y_airis, --なし
-stg_syaryoindex.reg_date_m_airis::varchar(2) as reg_date_m_airis, --なし
-stg_syaryoindex.reg_date_d_airis::varchar(2) as reg_date_d_airis, --なし
-stg_syaryoindex.reg_date_y_tcom::varchar(4) as reg_date_y_tcom, --なし
-stg_syaryoindex.reg_date_m_tcom::varchar(2) as reg_date_m_tcom, --なし
-stg_syaryoindex.reg_date_d_tcom::varchar(2) as reg_date_d_tcom, --なし
-stg_syaryoindex.use_cls_airis::varchar(1) as use_cls_airis, --なし
-stg_syaryoindex.cmpnycode::varchar(3) as cmpnycode, --なし
-stg_syaryoindex.region_cls_new::varchar(1) as region_cls_new, --なし
-stg_syaryoindex.lo_date::varchar(8) as lo_date, --なし
-stg_syaryoindex.lo_date_time::varchar(4) as lo_date_time, --なし
-stg_syaryoindex.veh_plnt_code::varchar(3) as veh_plnt_code, --なし
-stg_syaryoindex.use_country_code::varchar(3) as use_country_code, --なし
-stg_syaryoindex.rism_del_cls::varchar(1) as rism_del_cls, --なし
-stg_syaryoindex.assm_seq::varchar(3) as assm_seq, --なし
-stg_syaryoindex.mtuser::varchar(18) as mtuser, --なし
-stg_syaryoindex.mttime::timestamp_ntz as mttime, --timestamp型
-stg_syaryoindex._fivetran_synced::timestamp_ntz as ldts -- timestamp型 
+    stg_syaryoindex.index_cls::varchar(3) as index_cls, --なし
+    rtrim(stg_syaryoindex.frame_cls, ' 　')::varchar(6) as syadai_kt, -- 右ブランク
+    stg_syaryoindex.frame_no::varchar(7) as frmno, --なし
+    stg_syaryoindex.vin_wmi::varchar(3) as wmi, --なし
+    stg_syaryoindex.vin_vds::varchar(6) as vds, --なし
+    stg_syaryoindex.vin_vis::varchar(8) as vis, --なし
+    stg_syaryoindex.vin_my::varchar(1) as my, --なし
+    stg_syaryoindex.vin_cd::varchar(1) as cd, --なし
+    wmi || vds || vis as vin, --結合
+    syadai_kt || '-' || frmno as frame_cls_no, --結合
+    wmi || vds || vis || syadai_kt || '-' || frmno as vin_frame_cls_no, --結合
+    stg_syaryoindex.lo_date_y::varchar(4) as lo_date_y, --なし
+    stg_syaryoindex.lo_date_m::varchar(2) as lo_date_m, --なし
+    stg_syaryoindex.lo_date_d::varchar(2) as lo_date_d, --なし
+    try_to_date(lo_date_y || lo_date_m || lo_date_d, 'yyyymmdd' ) as lo_date_ymd, -- date型
+    stg_syaryoindex.lo_reg_month::number(3) as lo_reg_month, --なし
+    stg_syaryoindex.model_major_cls_code::varchar(2) as model_major_cls_code, --なし
+    stg_syaryoindex.sms_veh_code::varchar(4) as sms_syasyu_cd, --なし
+    stg_syaryoindex.model_veh_code::varchar(4) as model_syasyu_cd, --なし
+    stg_syaryoindex.ident_no::varchar(10) as idtno, --なし
+    rtrim(stg_syaryoindex.katashiki, ' 　')::varchar(15) as haisya_kt, -- 右ブランク
+    rtrim(stg_syaryoindex.sales_katashiki, ' 　')::varchar(20) as  hanbai_kt, -- 右ブランク
+    stg_syaryoindex.body_type::varchar(1) as body_type, --なし
+    stg_syaryoindex.eng_no::varchar(7) as eng_no, --なし
+    stg_syaryoindex.eng_type::varchar(20) as eng_type, --なし
+    stg_syaryoindex.tm_type::varchar(6) as tm_type, --なし
+    stg_syaryoindex.rdf_type::varchar(3) as rdf_type, --なし
+    stg_syaryoindex.spec_bit::varchar(6) as spec_bit, --なし
+    stg_syaryoindex.drive_system::varchar(5) as drive_system, --なし
+    stg_syaryoindex.airbag_ecu_part_no::varchar(5) as airbag_ecu_part_no, --なし
+    stg_syaryoindex.eng_plnt_code::varchar(2) as eng_plnt_code, --なし
+    stg_syaryoindex.tire_maker::varchar(2) as tire_maker, --なし
+    stg_syaryoindex.production_area_cls::varchar(1) as production_area_cls, --なし
+    stg_syaryoindex.production_country_code::varchar(3) as production_country_code, --なし
+    stg_syaryoindex.onec_line_code::varchar(2) as ichiken_line_code, --なし
+    stg_syaryoindex.onec_date_y::varchar(4) as ichikenyy, --なし
+    stg_syaryoindex.onec_date_m::varchar(2) as ichikenmm, --なし
+    stg_syaryoindex.onec_date_d::varchar(2) as ichikendd, --なし
+    try_to_date(ichikenyy || ichikenmm || ichikendd, 'yyyymmdd' ) as ichikenymd, -- date型
+    stg_syaryoindex.endc_line_code::varchar(2) as syuken_line_code, --なし
+    stg_syaryoindex.endc_date_y::varchar(4) as syukenyy, --なし
+    stg_syaryoindex.endc_date_m::varchar(2) as syukenmm, --なし
+    stg_syaryoindex.endc_date_d::varchar(2) as syukendd, --なし
+    try_to_date(syukenyy || syukenmm || syukendd, 'yyyymmdd' ) as syukenymd, -- date型
+    stg_syaryoindex.endc_time::varchar(4) as syuken_time, --なし
+    stg_syaryoindex.pack_line_code::varchar(2) as konpo_line_code, --なし
+    stg_syaryoindex.pack_date_y::varchar(4) as konpoyy, --なし
+    stg_syaryoindex.pack_date_m::varchar(2) as konpomm, --なし
+    stg_syaryoindex.pack_date_d::varchar(2) as konpodd, --なし
+    try_to_date(konpoyy ||  konpomm || konpodd, 'yyyymmdd' ) as konpoymd, -- date型
+    stg_syaryoindex.ident_line_code::varchar(2) as ident_line_code, --なし
+    stg_syaryoindex.pack_form::varchar(1) as pack_form, --なし
+    stg_syaryoindex.export_cls::varchar(1) as export_cls, --なし
+    stg_syaryoindex.use_cls::varchar(1) as use_cls, --なし
+    stg_syaryoindex.region_cls::varchar(1) as region_cls, --なし
+    stg_syaryoindex.dest_code::varchar(3) as dest_code, --なし
+    stg_syaryoindex.deliv_deal_code::varchar(5) as deliv_deal_code, --なし
+    stg_syaryoindex.deliv_date_y::varchar(4) as haisyayy, --なし
+    stg_syaryoindex.deliv_date_m::varchar(2) as haisyamm, --なし
+    stg_syaryoindex.deliv_date_d::varchar(2) as haisyadd, --なし
+    try_to_date(haisyayy ||  haisyamm || haisyadd, 'yyyymmdd' ) as haisyaymd, -- date型
+    stg_syaryoindex.reg_state_code::varchar(3) as reg_state_code, --なし
+    stg_syaryoindex.reg_country_code::varchar(5) as reg_country_code, --なし
+    stg_syaryoindex.reg_deal_code::varchar(8) as reg_deal_code, --なし
+    stg_syaryoindex.veh_reg_date_y::varchar(4) as torokuyy, --なし
+    stg_syaryoindex.veh_reg_date_m::varchar(2) as torokumm, --なし
+    stg_syaryoindex.veh_reg_date_d::varchar(2) as torokudd, --なし
+    try_to_date(torokuyy ||  torokumm || torokudd, 'yyyymmdd' ) as torokuyymmdd, -- date型
+    stg_syaryoindex.dest_detail::varchar(1) as dest_detail, --なし
+    stg_syaryoindex.b_veh_cls::varchar(1) as b_veh_cls, --なし
+    stg_syaryoindex.b_veh_date::varchar(8) as b_veh_date, --なし
+    stg_syaryoindex.prod_data_id::varchar(1) as prod_data_id, --なし
+    stg_syaryoindex.frame_recog_cls::varchar(1) as frame_recog_cls, --なし
+    stg_syaryoindex.veh_name_code::varchar(2) as veh_name_code, --なし
+    stg_syaryoindex.ext_color_code::varchar(4) as ext_color_code, --なし
+    stg_syaryoindex.int_code::varchar(4) as int_code, --なし
+    stg_syaryoindex.brand_cls::varchar(1) as brand_cls, --なし
+    stg_syaryoindex.tm_plnt_code::varchar(2) as tm_plnt_code, --なし
+    stg_syaryoindex.reg_date_y_airis::varchar(4) as reg_date_y_airis, --なし
+    stg_syaryoindex.reg_date_m_airis::varchar(2) as reg_date_m_airis, --なし
+    stg_syaryoindex.reg_date_d_airis::varchar(2) as reg_date_d_airis, --なし
+    stg_syaryoindex.reg_date_y_tcom::varchar(4) as reg_date_y_tcom, --なし
+    stg_syaryoindex.reg_date_m_tcom::varchar(2) as reg_date_m_tcom, --なし
+    stg_syaryoindex.reg_date_d_tcom::varchar(2) as reg_date_d_tcom, --なし
+    stg_syaryoindex.use_cls_airis::varchar(1) as use_cls_airis, --なし
+    stg_syaryoindex.cmpnycode::varchar(3) as cmpnycode, --なし
+    stg_syaryoindex.region_cls_new::varchar(1) as region_cls_new, --なし
+    stg_syaryoindex.lo_date::varchar(8) as lo_date, --なし
+    stg_syaryoindex.lo_date_time::varchar(4) as lo_date_time, --なし
+    stg_syaryoindex.veh_plnt_code::varchar(3) as veh_plnt_code, --なし
+    stg_syaryoindex.use_country_code::varchar(3) as use_country_code, --なし
+    stg_syaryoindex.rism_del_cls::varchar(1) as rism_del_cls, --なし
+    stg_syaryoindex.assm_seq::varchar(3) as assm_seq, --なし
+    stg_syaryoindex.mtuser::varchar(18) as mtuser, --なし
+    stg_syaryoindex.mttime::timestamp_ntz as mttime, --timestamp型
+    stg_syaryoindex._fivetran_synced::timestamp_ntz as ldts -- timestamp型 
 from stg_syaryoindex
 inner join cep1001index_r
-on stg_syaryoindex.index_cls = cep1001index_r.index_cls_r
-and stg_syaryoindex.frame_cls = cep1001index_r.frame_cls
-and stg_syaryoindex.frame_no = cep1001index_r.frame_no
-and stg_syaryoindex.vin_wmi = cep1001index_r.vin_wmi
-and stg_syaryoindex.vin_vds = cep1001index_r.vin_vds
-and stg_syaryoindex.vin_vis = cep1001index_r.vin_vis
+    on stg_syaryoindex.index_cls = cep1001index_r.index_cls_r
+    and stg_syaryoindex.frame_cls = cep1001index_r.frame_cls
+    and stg_syaryoindex.frame_no = cep1001index_r.frame_no
+    and stg_syaryoindex.vin_wmi = cep1001index_r.vin_wmi
+    and stg_syaryoindex.vin_vds = cep1001index_r.vin_vds
+    and stg_syaryoindex.vin_vis = cep1001index_r.vin_vis
