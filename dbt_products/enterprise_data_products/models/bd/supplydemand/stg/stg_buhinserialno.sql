6b0003d54ddc75f0d2b5e81fef1ab8d167dd62b6
Nishino <saya_nishino_zb@mail.toyota.co.jp>
2025-01-24 10:29:54 +0900

    IQAS-BD-部品シリアルNo-結合追加

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_buhinserialno.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_buhinserialno.sql
@@ -4,27 +4,61 @@
       incremental_strategy = 'merge'
    ) }}
 
-with stg_buhinserialno as (
-    select
-        index_cls::varchar(3) as index_cls, -- なし
-        rtrim(frame_cls, ' 　')::varchar(6) as syadai_kt, -- 右ブランク
-        frame_no::varchar(7) as frmno, -- なし
-        vin_wmi::varchar(3) as wmi,-- なし
-        vin_vds::varchar(6) as vds,-- なし
-        vin_vis::varchar(8) as vis, -- なし
-        parts_id::varchar(4) as parts_id,-- なし
-        parts_repl_seq::varchar(2) as parts_repl_seq, -- なし
-        rtrim(parts_ser_no, ' 　')::varchar(50) as parts_ser_no, -- 右ブランク
-        parts_ser_no_cls::varchar(1) as parts_ser_no_cls, -- なし
-        product_msg_no::varchar(4) as product_msg_no, -- なし
-        parts_repl_date::varchar(8) as parts_repl_date, -- なし
-        mtuser::varchar(18) as mtuser, -- なし
-        mttime::timestamp_ntz as mttime, --timestamp型
-        _fivetran_synced::timestamp_ntz as ldts -- timestamp型
-    from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep1004serno') }}
-    where _fivetran_deleted = 'false'
-           {% if is_incremental() %}
-               and ldts > (select max(ldts) from {{this}})
-           {% endif %}
-)
-select * from stg_buhinserialno
+with 
+    cep1004serno_r as (
+      select 
+        index_cls as index_cls_r,
+        frame_cls,
+        frame_no,
+        vin_wmi,
+        vin_vds,
+        vin_vis,
+        parts_repl_seq
+      from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep1004serno_r') }}
+      where _fivetran_deleted = 'false'
+      and mode_cls <> 'D' 
+      group by
+        index_cls,
+        frame_cls,
+        frame_no,
+        vin_wmi,
+        vin_vds,
+        vin_vis,
+        parts_repl_seq
+    ),
+
+    stg_buhinserialno as (
+      select *
+      from {{ source('fivetran_database_oraclerds_orcl_iqas_osepcs0000db20', 'raw_cep1004serno') }}
+      where _fivetran_deleted = 'false'
+   
+{% if is_incremental() %}
+    and _fivetran_synced > (select max(ldts) from {{this}})
+{% endif %}   
+    
+    )    
+select
+    stg_buhinserialno.index_cls::varchar(3) as index_cls, -- なし
+    rtrim(stg_buhinserialno.frame_cls, ' 　')::varchar(6) as syadai_kt, -- 右ブランク
+    stg_buhinserialno.frame_no::varchar(7) as frmno, -- なし
+    stg_buhinserialno.vin_wmi::varchar(3) as wmi,-- なし
+    stg_buhinserialno.vin_vds::varchar(6) as vds,-- なし
+    stg_buhinserialno.vin_vis::varchar(8) as vis, -- なし
+    stg_buhinserialno.parts_id::varchar(4) as parts_id,-- なし
+    stg_buhinserialno.parts_repl_seq::varchar(2) as parts_repl_seq, -- なし
+    rtrim(stg_buhinserialno.parts_ser_no, ' 　')::varchar(50) as parts_ser_no, -- 右ブランク
+    stg_buhinserialno.parts_ser_no_cls::varchar(1) as parts_ser_no_cls, -- なし
+    stg_buhinserialno.product_msg_no::varchar(4) as product_msg_no, -- なし
+    stg_buhinserialno.parts_repl_date::varchar(8) as parts_repl_date, -- なし
+    stg_buhinserialno.mtuser::varchar(18) as mtuser, -- なし
+    stg_buhinserialno.mttime::timestamp_ntz as mttime, --timestamp型
+    stg_buhinserialno._fivetran_synced::timestamp_ntz as ldts -- timestamp型
+from stg_buhinserialno
+inner join cep1004serno_r
+on stg_buhinserialno.index_cls = cep1004serno_r.index_cls_r
+and stg_buhinserialno.frame_cls = cep1004serno_r.frame_cls
+and stg_buhinserialno.frame_no = cep1004serno_r.frame_no
+and stg_buhinserialno.vin_wmi = cep1004serno_r.vin_wmi
+and stg_buhinserialno.vin_vds = cep1004serno_r.vin_vds
+and stg_buhinserialno.vin_vis = cep1004serno_r.vin_vis
+and stg_buhinserialno.parts_repl_seq = cep1004serno_r.parts_repl_seq
