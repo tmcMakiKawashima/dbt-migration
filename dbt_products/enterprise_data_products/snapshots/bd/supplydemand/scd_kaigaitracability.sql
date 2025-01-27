714ee3974d7cb51f9e141f50e2688ab14bd368b2
tomoyuki.okada_ctc <tomoyuki.okada.487@ctc-g.co.jp>
2025-01-27 20:07:50 +0900

    AQUA_海外トレサビ

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_kaigaitracability.sql
@@ -0,0 +1,23 @@
+{% snapshot scd_kaigaitracability %}
+
+{{
+    config(
+        unique_key="concat_ws('-',
+                    syadai_kt,
+                    frmno,
+                    wmi,
+                    vds,
+                    mdlyr,
+                    vin_vds_cd,
+                    lok_y,
+                    msgno)",
+
+        strategy='timestamp',
+        updated_at='updatetime',
+        invalidate_hard_deletes=True,
+    )
+}}
+
+select * from {{ ref('stg_kaigaitracability') }}
+
+{% endsnapshot %}
