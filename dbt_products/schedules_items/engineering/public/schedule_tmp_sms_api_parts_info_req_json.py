cd350d976a2344d6d3a2388c005d5169073d70d0
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-01-30 09:10:20 +0900

    DD-hinban fix

--- dbt_products/schedules_items/engineering/public/schedule_tmp_sms_api_parts_info_req_json.py
+++ dbt_products/schedules_items/engineering/public/schedule_tmp_sms_api_parts_info_req_json.py
@@ -1,24 +1,15 @@
-from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
-from dagster import DefaultScheduleStatus, RunConfig
-from dagster_dbt import build_schedule_from_dbt_selection
+from ....jobs_items.dd.jobs_sms_api import job_sms_api_parts_info_req
+from dagster import ScheduleDefinition
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 # SMS API(部品情報) REQUEST
-schedule_tmp_sms_api_parts_info_req_json = build_schedule_from_dbt_selection(
-    [dbt_products_assets],
-    job_name="job_build_tmp_sms_api_parts_info_req_json",
-    schedule_name="SMS_API_PARTS_INFO_REQUEST",
+schedule_tmp_sms_api_parts_info_req_json = ScheduleDefinition(
+    name="SMS_API_PARTS_INFO_REQUEST",
     cron_schedule="00 05 * * *",
     execution_timezone="Asia/Tokyo",
-    dbt_select="+tmp_sms_api_parts_info_req_json",
-    config=RunConfig(ops={"dbt_products_assets":
-                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_tmp_sms_api_parts_info_req_json"},
-                                    source_test_list=["source:*,+tmp_sms_api_parts_info_req_json"])
-                          }
-                     ),
-    default_status=DefaultScheduleStatus.RUNNING,
+    job=job_sms_api_parts_info_req[0],
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
           "job_name": "job_build_tmp_sms_api_parts_info_req_json"},
 )
--- dbt_products/sensors_items/dd/sensors_sms_api.py
+++ dbt_products/sensors_items/dd/sensors_sms_api.py
@@ -1,6 +1,6 @@
 from dagster import run_status_sensor, RunRequest, DagsterRunStatus, RunStatusSensorContext, JobSelector
-from ..sensor_util import tags_base, mini_interval_sec, loc_name
-from ...jobs_items.dd.jobs_sms_api import job_list_sms_api_parts_info
+from ..sensor_util import tags_base, mini_interval_sec
+from ...jobs_items.dd.jobs_sms_api import mnt_jobs_sms_api_parts_info, req_jobs_sms_api_parts_info
 
 
 # SMS API（品番情報）
@@ -10,18 +10,14 @@ dict_sms_api_parts_info = {
     "job_res": "job_build_tmp_sms_api_parts_info_res_relational"
 
 }
-job_list_mnt_sms_api_parts_info=[
-    JobSelector(location_name=loc_name, job_name=dict_sms_api_parts_info["job_req"]),
-    JobSelector(location_name=loc_name, job_name=dict_sms_api_parts_info["job_api"])
-]
 
 # run_status_sensorは１回の評価で１個しかトリガーできない
 
 # SMS API（品番情報）センサー
 @run_status_sensor(
         run_status=DagsterRunStatus.SUCCESS,
-        monitored_jobs=job_list_mnt_sms_api_parts_info,
-        request_jobs=job_list_sms_api_parts_info,
+        monitored_jobs=mnt_jobs_sms_api_parts_info,
+        request_jobs=req_jobs_sms_api_parts_info,
         minimum_interval_seconds=mini_interval_sec)
 def dd_sms_api_parts_info_sensor(context: RunStatusSensorContext):
     job_name = context.dagster_run.job_name
