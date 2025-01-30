58136ed77bbfd7b057f8efdae2d4f5eb501f9af0
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-01-30 15:23:13 +0900

    カラム名を囲むダブルクォーテーションの削除

--- dbt_products/enterprise_data_products/models/bd/customerservice/stg/stg_riyoframemaster.sql
+++ dbt_products/enterprise_data_products/models/bd/customerservice/stg/stg_riyoframemaster.sql
@@ -26,16 +26,16 @@ with
         select
             rtrim(no_vin_frameno,' 　')::varchar(17) as no_vin_frameno, -- 右blank
             kb_frameno::varchar(1) as kb_frameno,
-            rtrim("cd_fll_mdl",' 　')::varchar(20) as kosyo_kt, -- 右blank
+            rtrim(cd_fll_mdl,' 　')::varchar(20) as kosyo_kt, -- 右blank
             rtrim(cd_pubbnd,' 　')::varchar(6) as cd_pubbnd, -- 右blank
-            rtrim("cd_model_nm",' 　')::varchar(5) as syamei, -- 右blank
+            rtrim(cd_model_nm,' 　')::varchar(5) as syamei, -- 右blank
             rtrim(cd_vtype,' 　')::varchar(6) as cd_vtype, -- 右blank
             dd_pubbnd_trmfrym::varchar(6) as dd_pubbnd_trmfrym,
-            "dd_mdly"::varchar(4) as my,
+            dd_mdly::varchar(4) as my,
             dd_sectest::varchar(8) as dd_sectest,
-            rtrim("cd_sms",' 　')::varchar(4) as sms_syasyu_cd, -- 右blank
-            rtrim("cd_dispatch_mdl",' 　')::varchar(20) as haisya_kt, -- 右blank
-            rtrim("cd_sale_mdl",' 　')::varchar(20) as hanbai_kt, -- 右blank
+            rtrim(cd_sms,' 　')::varchar(4) as sms_syasyu_cd, -- 右blank
+            rtrim(cd_dispatch_mdl,' 　')::varchar(20) as haisya_kt, -- 右blank
+            rtrim(cd_sale_mdl,' 　')::varchar(20) as hanbai_kt, -- 右blank
             dt_frameno_capture::varchar(8) as dt_frameno_capture,
             ldts, -- b層のldts
             line_number,
