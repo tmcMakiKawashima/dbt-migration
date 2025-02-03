037e67dcfd6d9dabbb0f9d818af6c754daf0a829
e.kuboi <e-kuboi@toyotasystes.com>
2025-02-03 07:11:52 +0000

    change job_snapshot cron

--- dbt_products/schedules_items/schedule_snapshot.py
+++ dbt_products/schedules_items/schedule_snapshot.py
@@ -10,7 +10,7 @@ schedule_snapshot = build_schedule_from_dbt_selection(
     [dbt_products_assets],
     job_name="job_snapshots",
     schedule_name="snapshot",
-    cron_schedule="00 16 * * *",
+    cron_schedule="00 21 * * *",
     execution_timezone="Asia/Tokyo",
     dbt_select="resource_type:snapshot",
     dbt_exclude="scd_tbsmksk_noki",
