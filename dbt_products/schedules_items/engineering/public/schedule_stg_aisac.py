37f7a56bd84fe56faa767cb0dc66ce69c8288a02
da-kazumawashi-01.00 <da-kazumawashi-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-03-06 15:18:16 +0900

    AISAC 車種テーブル追加

--- dbt_products/schedules_items/engineering/public/schedule_stg_aisac.py
+++ dbt_products/schedules_items/engineering/public/schedule_stg_aisac.py
@@ -15,7 +15,6 @@ schedule_stg_aisac = build_schedule_from_dbt_selection(
                                     source_test_list=["source:*,+stg_syasyu"])
                           }
                      ),
-    default_status=DefaultScheduleStatus.RUNNING,
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
           "job_name": "job_build_stg_aisac"},
 )
