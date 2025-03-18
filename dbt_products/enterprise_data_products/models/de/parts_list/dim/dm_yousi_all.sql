9628bf6d436fb87a31d30502e094369983e3dcb5
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-03-18 13:54:54 +0900

    設変書 X 要旨(ALL)_新規作成

---
+++ dbt_products/enterprise_data_products/models/de/parts_list/dim/dm_yousi_all.sql
@@ -0,0 +1,25 @@
+{{ 
+  config(
+    materialized='incremental',
+    unique_key = ['seppenno','jun'],
+    incremental_strategy = 'append',
+    transient = false,
+    pre_hook="
+      {% if is_incremental() %}
+      delete from {{this}}
+      {% endif %}
+    "
+  )
+ }}
+-- 洗い替えであるため、pre_hookで全件削除を行う。
+
+with dm_yousi_all as (
+    select
+        seppenno::varchar(10) as seppenno, -- 設変№
+        jun::varchar(3) as jun, -- 順
+        eciyousijp::varchar(25) as eciyousijp, -- 要旨日本語
+        eciyousien::varchar(50) as eciyousien, -- 要旨英語
+        current_timestamp()::timestamp_ntz(9) as load_date -- 最終更新日時
+    from {{ source('engineering_db_public', 'raw_stg_eci_yousi') }}
+)
+select * from dm_yousi_all
