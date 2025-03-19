b7f633f164026d924b42313ad3f3652213cafd8d
da-lisshuai-07.00 <da-dxpf-li.sshuai@fujitsu.com>
2025-03-19 15:37:19 +0900

    物流管理KPI schedule_stg_galc_kpi.py 正規表現修正0319

--- dbt_products/schedules_items/supplydemand/public/schedule_stg_galc_kpi.py
+++ dbt_products/schedules_items/supplydemand/public/schedule_stg_galc_kpi.py
@@ -4,7 +4,7 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-
+ 
 # 物流管理KPI
 schedule_stg_galc_kpi = build_schedule_from_dbt_selection(
     [dbt_products_assets],
@@ -12,15 +12,15 @@ schedule_stg_galc_kpi = build_schedule_from_dbt_selection(
     schedule_name="GALC_KPI",
     cron_schedule="00 23 * * *",
     execution_timezone="Asia/Tokyo",
-
-    dbt_select="+*stg_hostalc_1?_erasure_achievements +*stg_hostalc_1?_production_progress +*stg_hostalc_1?_reject_vehicle +*stg_hostalc_1?_vehicle_specification +*stg_hostalc_1?_virtual_line +stg_hostalc_allsfh +stg_hostalc_allsfp +stg_hostalc_allsfl +stg_hostalc_allsft +stg_hostalc_allsfvam",
+ 
+    dbt_select="+file:stg_hostalc_1?_erasure_achievements +file:stg_hostalc_1?_production_progress +file:stg_hostalc_1?_reject_vehicle +file:stg_hostalc_1?_vehicle_specification +file:stg_hostalc_1?_virtual_line +stg_hostalc_allsfh +stg_hostalc_allsfp +stg_hostalc_allsfl +stg_hostalc_allsft +stg_hostalc_allsfvam",
     config=RunConfig(ops={"dbt_products_assets":
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_galc_kpi"},
-                                    source_test_list=["source:*,+*stg_hostalc_1?_erasure_achievements",
-                                                      "source:*,+*stg_hostalc_1?_production_progress",
-                                                      "source:*,+*stg_hostalc_1?_reject_vehicle",
-                                                      "source:*,+*stg_hostalc_1?_vehicle_specification",
-                                                      "source:*,+*stg_hostalc_1?_virtual_line",
+                                    source_test_list=["source:*,+file:stg_hostalc_1?_erasure_achievements",
+                                                      "source:*,+file:stg_hostalc_1?_production_progress",
+                                                      "source:*,+file:stg_hostalc_1?_reject_vehicle",
+                                                      "source:*,+file:stg_hostalc_1?_vehicle_specification",
+                                                      "source:*,+file:stg_hostalc_1?_virtual_line",
                                                       "source:*,+stg_hostalc_allsfh",
                                                       "source:*,+stg_hostalc_allsfp",
                                                       "source:*,+stg_hostalc_allsfl",
@@ -28,7 +28,7 @@ schedule_stg_galc_kpi = build_schedule_from_dbt_selection(
                                                       "source:*,+stg_hostalc_allsfvam"])
                           }
                      ),
-
+ 
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
           "job_name": "job_build_stg_galc_kpi"},
 )
