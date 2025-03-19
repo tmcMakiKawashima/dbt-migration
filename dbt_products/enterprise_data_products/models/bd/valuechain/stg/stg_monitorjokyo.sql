34f2a2b3267c79a3c49928800672516b6ce5ff62
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2025-03-19 18:47:41 +0900

    フォーマット文字列小文字化修正

--- dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_monitorjokyo.sql
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_monitorjokyo.sql
@@ -12,9 +12,9 @@ with stg_monitorjokyo as (
         to_number(productioncapacityupdateflg,1,0)::number(1,0) as productioncapacityupdateflg,
         mailsendkbn::varchar(2) as mailsendkbn, 
         createuserid::varchar(15) as createuserid, 
-        to_timestamp(createdatetime,'YYYY-MM-DD HH24:MI:SS.FF6')::timestamp(6) as createdatetime,
+        to_timestamp(createdatetime,'yyyy-mm-dd hh24:mi:ss.ff6')::timestamp(6) as createdatetime,
         updateuserid::varchar(15) as updateuserid,
-        to_timestamp(updatedatetime,'YYYY-MM-DD HH24:MI:SS.FF6')::timestamp(6) as updatedatetime,
+        to_timestamp(updatedatetime,'yyyy-mm-dd hh24:mi:ss.ff6')::timestamp(6) as updatedatetime,
         to_number(enforcementnum,10,0)::number(10,0) as enforcementnum,
         ldts::timestamp_ntz(9) as ldts,
         row_number() over (
