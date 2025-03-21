dcf810256046e96f7b0a607ae5d242372a0418f5
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-03-21 16:00:16 +0900

    設変書 X 要旨(ALL)_新規作成

--- dbt_products/schedules_items/parts_list/public/schedule_dm_gijutsu_sms.py
+++ dbt_products/schedules_items/parts_list/public/schedule_dm_gijutsu_sms.py
@@ -3,18 +3,18 @@ from dagster import DefaultScheduleStatus, RunConfig
 from dagster_dbt import build_schedule_from_dbt_selection
 
 # UDC　設変情報追加　認証不正対応
-schedule_dm_gijutsu_sms_0700 = build_schedule_from_dbt_selection(
+schedule_dm_gijutsu_sms = build_schedule_from_dbt_selection(
     [dbt_products_assets],
-    job_name="job_build_dm_gijutsu_sms_0700",
-    schedule_name="gijutsu_sms_0700",
+    job_name="job_build_dm_gijutsu_sms",
+    schedule_name="gijutsu_sms",
     cron_schedule="00 07 * * *",
     execution_timezone="Asia/Tokyo",
     dbt_select="+dm_yousi_all",
     config=RunConfig(ops={"dbt_products_assets":
-                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_gijutsu_sms_0700"},
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_gijutsu_sms"},
                                     source_test_list=["source:*,+dm_yousi_all"])
                           }
                      ),
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
-          "job_name": "job_build_dm_gijutsu_sms_0700"},
+          "job_name": "job_build_dm_gijutsu_sms"},
 )
