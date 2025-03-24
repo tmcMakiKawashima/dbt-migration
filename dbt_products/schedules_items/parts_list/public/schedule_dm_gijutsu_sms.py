69632c3679843458914d2c2d245c4a2361a23ce1
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-03-24 15:24:18 +0900

    設変書 X 要旨(ALL)_標準実装不足対応

--- dbt_products/schedules_items/parts_list/public/schedule_dm_gijutsu_sms.py
+++ dbt_products/schedules_items/parts_list/public/schedule_dm_gijutsu_sms.py
@@ -1,6 +1,9 @@
 from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
 from dagster import DefaultScheduleStatus, RunConfig
 from dagster_dbt import build_schedule_from_dbt_selection
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 # UDC　設変情報追加　認証不正対応
 schedule_dm_gijutsu_sms = build_schedule_from_dbt_selection(
