ce7ecdae1f6e635bfbeea1b7f10c29bede33fe5b
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-03-27 09:39:36 +0900

    trim追加

--- dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_emokujikensaku.sql
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_emokujikensaku.sql
@@ -1,9 +1,9 @@
 with stg_emokujikensaku as (
     select
         mntkbn::varchar(1) as mntkbn,
-        ctlgcd::varchar(6) as ctlgcd,
-        emkjgrpcd::varchar(1) as emkjgrpcd,
-        figno::varchar(4) as figno,
+        rtrim(ctlgcd,' 　')::varchar(6) as ctlgcd,
+        rtrim(emkjgrpcd,' 　')::varchar(1) as emkjgrpcd,
+        rtrim(figno,' 　')::varchar(4) as figno,
         rtrim(hansno,' 　')::varchar(7) as hansno, -- 右blank
         emkjimgflmei::varchar(20) as emkjimgflmei,
         ldts, -- b層のldts
