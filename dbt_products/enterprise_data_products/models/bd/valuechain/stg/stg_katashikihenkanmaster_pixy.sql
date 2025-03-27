ace4721f28a0c313ca6bd6d349aba7cb97973801
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-03-27 16:43:17 +0900

    BD_型式返還マスター

---
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_katashikihenkanmaster_pixy.sql
@@ -0,0 +1,17 @@
+with stg_katashikihenkanmaster_pixy as (
+    select 
+        syameino::varchar(3) as syameino, 
+        ctlkata::varchar(16) as ctlkata, 
+        hanbaicd::varchar(4) as hanbaicd, 
+        bitno::varchar(3) as bitno, 
+        reqop::varchar(1) as reqop, 
+        reqac::varchar(1) as reqac, 
+        reqad::varchar(1) as reqad, 
+        touroku::varchar(8) as touroku, 
+        kousin::varchar(8) as kousin, 
+        tantou::varchar(7) as tantou,  
+        _fivetran_synced::timestamp_ntz as ldts
+    from {{ source('fivetran_database_idr_pixy','raw_cva97katahen') }}
+    where _fivetran_deleted = 'false'
+)
+select * from stg_katashikihenkanmaster_pixy
