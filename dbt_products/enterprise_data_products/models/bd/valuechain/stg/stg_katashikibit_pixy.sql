75566c909525777a004c29358338538361c9170c
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-04-02 10:07:52 +0900

    BD_DX用品マスター型式BIT

---
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_katashikibit_pixy.sql
@@ -0,0 +1,13 @@
+with stg_katashikibit_pixy as (
+    select 
+        syameino::varchar(3) as syameino, 
+        tuikou::varchar(8) as tuikou, 
+        seg::varchar(3) as seg, 
+        seq::varchar(3) as seq, 
+        bitno::varchar(3) as bitno, 
+        touroku::varchar(8) as touroku, 
+        _fivetran_synced::timestamp_ntz as ldts
+    from {{ source('fivetran_database_idr_pixy','raw_cva97dxyohinmktbit') }}
+    where _fivetran_deleted = 'false'
+)
+select * from stg_katashikibit_pixy
