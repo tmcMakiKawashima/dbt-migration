e25125f9d824a901b68eaf2923bf5380d86fda7d
atsuhito_kawachi_za@mail.toyota.co.jp <atsuhito_kawachi_za@mail.toyota.co.jp>
2025-04-07 10:16:30 +0900

    #197 MIKADO 新Judge-V
    B→D 少量債権伝票情報
    ・where条件の誤り
    　誤)where to_varchar(timestamp,'yyyymmdd')
    　正)where to_varchar(ldts,'yyyymmdd')

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_saikendenpyo_joho_judgev.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_saikendenpyo_joho_judgev.sql
@@ -64,10 +64,10 @@ with stg_saikendenpyo_joho_judgev as (
         mtuser::varchar(13) as mtuser, -- 更新ユーザーID
         mttime::varchar(26) as mttime, -- 更新日時
         recvdate::varchar(8) as recvdate, -- 受信日
-        current_timestamp()::timestamp_ntz as timestamp -- タイムスタンプ
-    from {{source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta005')}}
-    {% if is_incremental() %}
-        where to_varchar(timestamp,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta005')}})
-    {% endif %}
+        current_timestamp()::timestamp_ntz as timestamp, -- タイムスタンプ
+        from {{source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta005')}}
+      {% if is_incremental() %}
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta005')}})
+      {% endif %}
 )
 select *  from stg_saikendenpyo_joho_judgev
