dea419a9ffd4114a575e2fabbb66006978975661
da-dxpf-ogawa.yuya.4tx@jp.nssol.nipponsteel.com <ogawa.yuya.4tx@jp.nssol.nipponsteel.com>
2025-04-10 13:15:56 +0900

    stg_bunya実装

---
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bunya.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = ['nysketcd'],
+    incremental_strategy = 'append',
+    pre_hook=[
+      "{{ dbt_snow_mask.create_masking_policy('models')}}",
+      "
+      {% if is_incremental() %}
+      delete from {{this}}
+      {% endif %}
+      "
+    ]
+  )
+}}
+
+with stg_bunya as (
+    select
+        bnycd::varchar(3) as bnycd, -- 分野コード
+        bnynam::varchar(24) as bnynam, -- 分野名称
+        rskbnynam::varchar(8) as rskbnynam, -- 略式分野名称
+        bnytktbucd::varchar(2) as bnytktbucd, -- 分野統括部コード
+        ldts::timestamp as ldts, -- B層取込日時
+        row_number() over(partition by bnycd
+                       order by ldts desc, line_number desc) as aggkey
+    from {{ source('snowpipe_db_administration', 'raw_ktrla015zz0kh20076') }}
+    {% if is_incremental() %}
+        where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla015zz0kh20076')}})
+    {% endif %}
+)
+select *  exclude(aggkey) from stg_bunya where aggkey = 1
