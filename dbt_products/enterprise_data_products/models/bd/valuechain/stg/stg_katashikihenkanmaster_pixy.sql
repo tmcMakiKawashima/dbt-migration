20016e33e9ea151b8b1b5415ae839706297f93cf
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-04-14 17:15:46 +0900

    BD_型式変換マスター_桁数変更

--- dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_katashikihenkanmaster_pixy.sql
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_katashikihenkanmaster_pixy.sql
@@ -1,15 +1,15 @@
 with stg_katashikihenkanmaster_pixy as (
     select 
-        syameino::varchar(3) as syameino, 
-        ctlkata::varchar(16) as ctlkata, 
-        hanbaicd::varchar(4) as hanbaicd, 
-        bitno::varchar(3) as bitno, 
-        reqop::varchar(1) as reqop, 
-        reqac::varchar(1) as reqac, 
-        reqad::varchar(1) as reqad, 
-        touroku::varchar(8) as touroku, 
-        kousin::varchar(8) as kousin, 
-        tantou::varchar(7) as tantou,  
+        syameino::varchar(12) as syameino, 
+        ctlkata::varchar(64) as ctlkata, 
+        hanbaicd::varchar(16) as hanbaicd, 
+        bitno::varchar(12) as bitno, 
+        reqop::varchar(4) as reqop, 
+        reqac::varchar(4) as reqac, 
+        reqad::varchar(4) as reqad, 
+        touroku::varchar(32) as touroku, 
+        kousin::varchar(32) as kousin, 
+        tantou::varchar(28) as tantou, 
         _fivetran_synced::timestamp_ntz as ldts
     from {{ source('fivetran_database_idr_pixy_dxpfy2d','raw_cva97katahen') }}
     where _fivetran_deleted = 'false'
