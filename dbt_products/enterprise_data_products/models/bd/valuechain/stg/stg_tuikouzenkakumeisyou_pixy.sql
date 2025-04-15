2e10b6e24bf813da3ddca86f24f58d738ca9506a
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-04-15 09:19:56 +0900

    BD_DX用品ﾏｽﾀｰ追工全角名称TBL_桁数変更

--- dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_tuikouzenkakumeisyou_pixy.sql
+++ dbt_products/enterprise_data_products/models/bd/valuechain/stg/stg_tuikouzenkakumeisyou_pixy.sql
@@ -1,11 +1,11 @@
 with stg_tuikouzenkakumeisyou_pixy as (
     select 
-        syameino::varchar(3) as syameino, 
-        tuikou::varchar(8) as tuikou, 
-        tuimeig::varchar(51) as tuimeig, 
-        touroku::varchar(8) as touroku, 
+        syameino::varchar(12) as syameino, 
+        tuikou::varchar(32) as tuikou, 
+        tuimeig::varchar(204) as tuimeig, 
+        touroku::varchar(32) as touroku, 
         _fivetran_synced::timestamp_ntz as ldts
-    from {{ source('fivetran_database_idr_pixy','raw_cva97dxyohintknmz') }}
+    from {{ source('fivetran_database_idr_pixy_dxpfy2d','raw_cva97dxyohintknmz') }}
     where _fivetran_deleted = 'false'
 )
 select * from stg_tuikouzenkakumeisyou_pixy
