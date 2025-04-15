25b6c35c9763d4b6b5565b63aeeddf0e3b307133
daiki_watanabe_zg <daiki_watanabe_zg@mail.toyota.co.jp>
2025-04-15 21:39:31 +0900

    生管EUC_kodomo_制約フォーマット資産更新

--- dbt_products/schedules_items/supplydemand/public/schedule_stg_kodomo.py
+++ dbt_products/schedules_items/supplydemand/public/schedule_stg_kodomo.py
@@ -1,20 +1,21 @@
-from ....enterprise_data_products_assets.assets import dbtconfig, dbt_products_assets
-from dagster import defaultschedulestatus, runconfig
+from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
+from dagster import DefaultScheduleStatus, RunConfig
 from dagster_dbt import build_schedule_from_dbt_selection
-from pathlib import path
+from pathlib import Path
 import sys
-sys.path.append(str(path(__file__).resolve().parent.parent))
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 
 # kodomo
 schedule_stg_kodomo = build_schedule_from_dbt_selection(
     [dbt_products_assets],
     job_name="job_build_stg_kodomo",
-    schedule_name="kodomo",
-    # cron_schedule="0 6 * * *",
-    execution_timezone="asia/tokyo",
-    dbt_select="++stg_seiyakuformat_kodomo",
-    config=runconfig(ops={"dbt_products_assets":
-                          dbtconfig(dbt_vars={"dbt_job_name": "_stg_kodomo"},
+    schedule_name="KODOMO",
+    cron_schedule="00 06 * * *",
+    execution_timezone="Asia/Tokyo",
+    dbt_select="+stg_seiyakuformat_kodomo",
+    config=RunConfig(ops={"dbt_products_assets":
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_kodomo"},
                                     source_test_list=["source:*,+stg_seiyakuformat_kodomo"])
                           }
                      ),
