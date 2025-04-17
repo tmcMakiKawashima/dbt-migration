a733377452c4d994c0a8199456d3af72a7b59e19
ohta.kazuki_kuc <kazuki_ohta_zb@mail.toyota.co.jp>
2025-04-17 13:31:28 +0900

    インデント修正

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_seiyakuformat_kodomo.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_seiyakuformat_kodomo.sql
@@ -44,8 +44,8 @@ with stg_seiyakuformat_kodomo as (
         ) aggkey
    from {{ source('snowpipe_db_supplydemand','raw_t_restriction_formats') }}
    ),
-stg_seiyakuformat_kodomo_max as (
- --ほぼ同時に着弾するとldtsでは最新の判断が困難な為、file_nameを使用する(現行コドモの仕様と合わせる)
+   stg_seiyakuformat_kodomo_max as (
+   --ほぼ同時に着弾するとldtsでは最新の判断が困難な為、file_nameを使用する(現行コドモの仕様と合わせる)
    select
      getudo,
      max(file_name) as max_ldts
@@ -53,12 +53,10 @@ stg_seiyakuformat_kodomo_max as (
    group by 
      getudo 
    )
-
-   select
-     stg_seiyakuformat_kodomo.* 
-     exclude(aggkey,file_name) 
-   from stg_seiyakuformat_kodomo
-   inner join stg_seiyakuformat_kodomo_max
-     on stg_seiyakuformat_kodomo.getudo = stg_seiyakuformat_kodomo_max.getudo
-     and stg_seiyakuformat_kodomo.ldts = stg_seiyakuformat_kodomo_max.max_ldts
-   where stg_seiyakuformat_kodomo.aggkey = 1
+select
+  stg_seiyakuformat_kodomo.* exclude(aggkey,file_name) 
+from stg_seiyakuformat_kodomo
+inner join stg_seiyakuformat_kodomo_max
+  on stg_seiyakuformat_kodomo.getudo = stg_seiyakuformat_kodomo_max.getudo
+  and stg_seiyakuformat_kodomo.ldts = stg_seiyakuformat_kodomo_max.max_ldts
+where stg_seiyakuformat_kodomo.aggkey = 1
