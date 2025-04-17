df291bccbc20d0e93c18e260eea5911b8efbe901
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2025-04-17 09:47:18 +0900

    where文修正

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_yosankmk_masta_judgev.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_yosankmk_masta_judgev.sql
@@ -14,6 +14,6 @@ with stg_yosankmk_masta_judgev as (
         recvdate::varchar(8) as recvdate, -- 受信日
         current_timestamp::timestamp as timestamp -- タイムスタンプ
     from {{ source('snowpipe_db_administration', 'ktrla02kzz0kgta025') }}
-   where ldts = (select max(ldts) from {{ source('snowpipe_db_administration', 'ktrla02kzz0kgta025') }}) 
+   where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'ktrla02kzz0kgta025')}})
 )
 select * from stg_yosankmk_masta_judgev
