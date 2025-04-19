9ffbe86550d3abb3d25ac447dbb14432b0155bf7
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2025-04-19 16:26:49 +0900

    stg_bumonctl_masta_judgev修正

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bumonctl_masta_judgev.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_bumonctl_masta_judgev.sql
@@ -1,3 +1,17 @@
+{{ 
+  config(
+    materialized='incremental',
+    incremental_strategy = 'append',
+    transient = false,
+    pre_hook="
+      {% if is_incremental() %}
+      delete from {{this}}
+      {% endif %}
+    "
+  )
+ }}
+-- 洗い替え処理で常に最新断面のみとするためpre_hookで全件削除する処理を記載
+
 with stg_bumonctl_masta_judgev as (
     select
         ysnkb::varchar(2) as ysnkb, -- 予算区分
@@ -11,7 +25,7 @@ with stg_bumonctl_masta_judgev as (
         ksnbi::varchar(8) as ksnbi, -- 更新日
         recvdate::varchar(8) as recvdate, -- 受信日
         current_timestamp::timestamp as timestamp -- タイムスタンプ
-    from {{ source('snowpipe_db_administration', 'ktrla02kzz0kgta023') }}
-   where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'ktrla02kzz0kgta023')}})
+    from {{ source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta023') }}
+   where to_varchar(ldts,'yyyymmdd') = (select to_varchar(max(ldts),'yyyymmdd') from {{source('snowpipe_db_administration', 'raw_ktrla02kzz0kgta023')}})
 )
 select * from stg_bumonctl_masta_judgev
