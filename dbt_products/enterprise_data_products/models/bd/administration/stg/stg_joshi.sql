f0e4aebcee5100b311a4bd9060fcfd1b69134bfc
da-dxpf-ogawa.yuya.4tx@jp.nssol.nipponsteel.com <ogawa.yuya.4tx@jp.nssol.nipponsteel.com>
2025-04-21 09:41:31 +0900

    sql修正

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_joshi.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_joshi.sql
@@ -4,7 +4,6 @@
     unique_key = ['syozoku_cd'],
     incremental_strategy = 'append',
     pre_hook=[
-      "{{ dbt_snow_mask.create_masking_policy('models')}}",
       "
       {% if is_incremental() %}
       delete from {{this}}
@@ -13,7 +12,7 @@
     ]
   )
 }}
-
+-- 洗い替え処理で常に最新断面のみとするためpre_hookで全件削除する処理を記載
 with stg_joshi as (
     select
         szcd::varchar(5) as syozoku_cd, -- 所属コード
