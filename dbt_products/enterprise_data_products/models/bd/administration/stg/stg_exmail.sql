69bef8823a592a2450107549f14b01078b2ed830
da-dxpf-ogawa.yuya.4tx@jp.nssol.nipponsteel.com <ogawa.yuya.4tx@jp.nssol.nipponsteel.com>
2025-04-21 11:22:49 +0900

    sql修正

--- dbt_products/enterprise_data_products/models/bd/administration/stg/stg_exmail.sql
+++ dbt_products/enterprise_data_products/models/bd/administration/stg/stg_exmail.sql
@@ -4,7 +4,6 @@
     unique_key = ['employee_cd'],
     incremental_strategy = 'append',
     pre_hook=[
-      "{{ dbt_snow_mask.create_masking_policy('models')}}",
       "
       {% if is_incremental() %}
       delete from {{this}}
@@ -13,7 +12,7 @@
     ]
   )
 }}
-
+-- 洗い替え処理で常に最新断面のみとするためpre_hookで全件削除する処理を記載
 with stg_exmail as (
     select
         jcd::varchar(7) as employee_cd, -- 従業員コード
