8a77bea2e56199c9e4ea28652ebf2fa9de1d2479
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-04-23 15:16:34 +0900

    設変書 X 適用範囲(ALL)_sourceからrefへ修正

--- dbt_products/enterprise_data_products/models/de/parts_list/seppen/dim/dm_tekiyohani_all.sql
+++ dbt_products/enterprise_data_products/models/de/parts_list/seppen/dim/dm_tekiyohani_all.sql
@@ -18,7 +18,7 @@ with thn as (
 ), bmts as (
   select * from {{source('engineering_db_public','raw_tmp_m_cyp02mokujibuijun')}}
 ), sy as (
-  select * from {{ref('raw_stg_syasyu')}}
+  select * from {{ref('stg_syasyu')}}
 ), kkj as (
   select * from {{ref('stg_eci_kirikaejiki')}}
 ), thn_bmts as (
