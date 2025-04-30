0d7b713069776ce0ab6de8c3dda8cc3aff9c0580
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-04-30 18:15:25 +0900

    設変書 X 適用範囲(ALL)_項目順変更

--- dbt_products/enterprise_data_products/models/de/parts_list/seppen/temporary/dm_tekiyohani_all/tmp10_dm_tekiyohani_all.sql
+++ dbt_products/enterprise_data_products/models/de/parts_list/seppen/temporary/dm_tekiyohani_all/tmp10_dm_tekiyohani_all.sql
@@ -13,11 +13,10 @@ select
   substr(eth.tekiyo,5,2) as bui, -- 部位
   substr(eth.tekiyo,7,4) as variation, -- バリエーション
   eth.sinhaikbn as sinhaikbn, -- 新廃区分
-  'ECI' as ecikbn, -- 設変書区分
   coalesce(tsy.tousai, rpad('', 4)) as tousai, -- 搭載車種
-  rpad('', 30) as cond, -- Condition
+  'ECI' as ecikubun, -- 設変書区分
   rpad('', 40) as meisyo, -- 名称
-  current_timestamp() as ldts -- 最終更新日時
+  rpad('', 30) as cond -- Condition
 from eth
 left join tsy
 on (
