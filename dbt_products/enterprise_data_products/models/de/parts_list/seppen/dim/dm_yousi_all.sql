fb2a43b7615eeb50eaed5e0ed9f4cfe738ddbdc3
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-05-09 13:17:54 +0900

    認証不正防止対応(設変分析)_規約対応

--- dbt_products/enterprise_data_products/models/de/parts_list/seppen/dim/dm_yousi_all.sql
+++ dbt_products/enterprise_data_products/models/de/parts_list/seppen/dim/dm_yousi_all.sql
@@ -14,9 +14,14 @@
 -- 洗い替えであるため、pre_hookで全件削除を行う。
 
 with yousi_all as (
-  select * from {{ ref('stg_eci_yousi') }}
+  select
+    seppenno, -- 設変№
+    jun, -- 順
+    eciyousijp, -- 要旨日本語
+    eciyousien -- 要旨英語
+  from {{ ref('stg_eci_yousi') }}
 )
 select 
-  yousi_all.* exclude(mttime, ldts),
+  yousi_all.*,
   current_timestamp()::timestamp_ntz(9) as ldts -- 最終更新日時
 from yousi_all
--- dbt_products/schedules_items/parts_list/seppen/schedule_dm_seppen_bunseki.py
+++ dbt_products/schedules_items/parts_list/seppen/schedule_dm_seppen_bunseki.py
@@ -10,6 +10,7 @@ schedule_dm_seppen_bunseki = build_schedule_from_dbt_selection(
     cron_schedule="00 07 * * *",
     execution_timezone="Asia/Tokyo",
     dbt_select="+dm_hinban_all +dm_yousi_all +dm_zokusei_all +dm_tekiyohani_all",
+    dbt_exclude="stg_syasyu",
     config=RunConfig(ops={"dbt_products_assets":
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_seppen_bunseki"},
                                     source_test_list=["source:*,+dm_hinban_all",
