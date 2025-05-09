81d02e47c21641d5e6cf94c2cbe61cbc6748c0a8
da-daisukeyosh-01.00 <da-daisukeyosh-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-05-09 13:21:41 +0900

    feature-344-BD-syasyu_siyousho

---
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/substr/substr_ktrla024zz0ka21901.sql
@@ -0,0 +1,22 @@
+with substr_ktrla024zz0ka21901 as (
+    select
+        substr(raw_data, 1, 1)::varchar(1) as mtkbn,
+        substr(raw_data, 2, 4)::varchar(4) as syasyu,
+        substr(raw_data, 6, 2)::varchar(2) as sno,
+        substr(raw_data, 8, 1)::varchar(1) as jidokbn,
+        substr(raw_data, 9, 1)::varchar(1) as sktire,
+        substr(raw_data, 10, 1)::varchar(1) as skdoitu,
+        substr(raw_data, 11, 1)::varchar(1) as tokusyu,
+        substr(raw_data, 12, 1)::varchar(1) as syflg,
+        substr(raw_data, 13, 4)::varchar(4) as maesyasyu,
+        substr(raw_data, 17, 3)::varchar(3) as tutisyo,
+        substr(raw_data, 20, 2)::varchar(2) as tutisuf,
+        substr(raw_data, 22, 1)::varchar(1) as lang,
+        substr(raw_data, 23, 1)::varchar(1) as lang2,
+        substr(raw_data, 24, 14)::varchar(14) as mtdate,
+        substr(raw_data, 38, 13)::varchar(13) as dummy,
+        line_number,
+        ldts
+    from {{source('snowpipe_db_supplydemand', 'raw_ktrla024zz0ka21901')}}
+)
+select * from substr_ktrla024zz0ka21901
