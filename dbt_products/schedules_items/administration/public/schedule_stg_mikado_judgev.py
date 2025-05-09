1d3f607dc6469f9f9d1e69a37b8b28459d8a6cb9
atsuhito_kawachi_za@mail.toyota.co.jp <atsuhito_kawachi_za@mail.toyota.co.jp>
2025-05-09 19:13:42 +0900

    #197 MIKADO 新Judge-V 規約レビュー修正

--- dbt_products/schedules_items/administration/public/schedule_stg_mikado_judgev.py
+++ dbt_products/schedules_items/administration/public/schedule_stg_mikado_judgev.py
@@ -11,29 +11,7 @@ schedule_stg_mikado_judgev = build_schedule_from_dbt_selection(
     execution_timezone="Asia/Tokyo",
     dbt_select="+stg_saikendenpyo_joho_judgev +stg_saikenmaeuke_meisai_judgev +stg_saikentorihikisaki_meisai_judgev +stg_saimudenpyo_meisai_judgev +stg_hinmoku_meisai_judgev +stg_torihikisaki_masta_judgev +stg_furikomikoza_masta_judgev +stg_torihikitanto_kaigai_masta_judgev +stg_seikyusofu_kokunai_masta_judgev +stg_shinsei_header_judgev +stg_torihiki_tekiyo_judgev +stg_torihiki_meisai_judgev +stg_kosaihijizen_meisai_judgev +stg_busyo_meisai_judgev +stg_zeimu_himoduke_judgev +stg_zeimu_meisai_judgev +stg_jugyoin_masta_judgev +stg_jcard_meisai_judgev +stg_bumonctl_masta_judgev +stg_yosankbn_masta_judgev +stg_yosankmk_masta_judgev +stg_yosanhmk_masta_judgev",
     config=RunConfig(ops={"dbt_products_assets":
-                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_judgev"},
-                                    source_test_list=["source:*,+stg_saikendenpyo_joho_judgev",
-                                                      "source:*,+stg_saikenmaeuke_meisai_judgev",
-                                                      "source:*,+stg_saikentorihikisaki_meisai_judgev",
-                                                      "source:*,+stg_saimudenpyo_meisai_judgev",
-                                                      "source:*,+stg_hinmoku_meisai_judgev",
-                                                      "source:*,+stg_torihikisaki_masta_judgev",
-                                                      "source:*,+stg_furikomikoza_masta_judgev",
-                                                      "source:*,+stg_torihikitanto_kaigai_masta_judgev",
-                                                      "source:*,+stg_seikyusofu_kokunai_masta_judgev",
-                                                      "source:*,+stg_shinsei_header_judgev",
-                                                      "source:*,+stg_torihiki_tekiyo_judgev",
-                                                      "source:*,+stg_torihiki_meisai_judgev",
-                                                      "source:*,+stg_kosaihijizen_meisai_judgev",
-                                                      "source:*,+stg_busyo_meisai_judgev",
-                                                      "source:*,+stg_zeimu_himoduke_judgev",
-                                                      "source:*,+stg_zeimu_meisai_judgev",
-                                                      "source:*,+stg_jugyoin_masta_judgev",
-                                                      "source:*,+stg_jcard_meisai_judgev",
-                                                      "source:*,+stg_bumonctl_masta_judgev",
-                                                      "source:*,+stg_yosankbn_masta_judgev",
-                                                      "source:*,+stg_yosankmk_masta_judgev",
-                                                      "source:*,+stg_yosanhmk_masta_judgev"])
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_mikado_judgev"})
 
                           }
                      ),
