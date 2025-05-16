9a331d7540dfe4c582a50bfb6b61b4973ecd6bec
li.sshuai <li.sshuai@fujitsu.com>
2025-05-16 16:35:32 +0900

    maps stg_sizainaiji_maps_viewでsource使用

--- dbt_products/enterprise_data_products/models/dd/manufacturing/stg/stg_sizainaiji_maps_view.sql
+++ dbt_products/enterprise_data_products/models/dd/manufacturing/stg/stg_sizainaiji_maps_view.sql
@@ -3,8 +3,6 @@
    )
 }}
 
-{% set source_table = source('manufacturing_db_public', 'raw_stg_sizainaiji') %}
-
 with stg_sizainaiji_maps_view as (
   select
     mtl_mtl_rqi_id::varchar(15) as mtl_mtl_rqi_id, -- 資材内示ID
@@ -196,8 +194,7 @@ with stg_sizainaiji_maps_view as (
     program_application_id::varchar(15) as program_application_id, -- コンカレント・プログラム・アプリケーションID
     program_id::varchar(15) as program_id, -- コンカレント・プログラムID
     program_update_date::varchar(14) as program_update_date, -- プログラム更新日時
-    "ldts" -- B層取込日時
-  from {{ source_table.database }}.{{ source_table.schema }}."{{ source_table.name | lower }}"
+    ldts -- B層取込日時
+  from {{source('manufacturing_db_public', 'raw_stg_sizainaiji')}}
 )
-
 select * from stg_sizainaiji_maps_view
