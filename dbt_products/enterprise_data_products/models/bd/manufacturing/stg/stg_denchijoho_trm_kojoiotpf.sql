51df027c9419db58a68bfb55cfb8c385fa686257
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-05-16 17:38:00 +0900

    BD_電池情報（TR-M）

---
+++ dbt_products/enterprise_data_products/models/bd/manufacturing/stg/stg_denchijoho_trm_kojoiotpf.sql
@@ -0,0 +1,27 @@
+{{ config(
+      materialized='incremental',
+      unique_key = ['traceability_id','module_id'],
+      incremental_strategy = 'merge'
+   ) }}
+
+with stg_denchijoho_trm_kojoiotpf as (
+    select
+        rtrim(traceability_id,' 　')::varchar(30) as traceability_id,  -- 右blank
+        rtrim(module_id,' 　')::varchar(30) as module_id,  -- 右blank
+        rtrim(module_manufacturing_date,' 　')::varchar(20) as module_manufacturing_date,  -- 右blank
+        rtrim(manufacturer_cd,' 　')::varchar(4) as manufacturer_cd,  -- 右blank
+        rtrim(receive_file_name,' 　')::varchar(200) as receive_file_name,  -- 右blank
+        try_to_timestamp_ntz(receive_date , 'yyyy-mm-dd hh24:mi:ss.ff6') as receive_date,  -- timestamp
+        try_to_boolean(delete_flag) as delete_flag,  -- booleans
+        try_to_timestamp_ntz(regist_date , 'yyyy-mm-dd hh24:mi:ss.ff6') as regist_date,  -- timestamp
+        try_to_timestamp_ntz(update_date , 'yyyy-mm-dd hh24:mi:ss.ff6') as update_date,  -- timestamp
+        ldts,
+        row_number() over(partition by traceability_id, module_id order by ldts desc, line_number desc) aggkey
+    from {{ source('snowpipe_db_manufacturing', 'raw_iotpmcbtinftrm') }}
+           
+  {% if is_incremental() %}
+      where ldts > (select max(ldts) from {{this}})
+  {% endif %}
+)
+select * exclude(aggkey) from stg_denchijoho_trm_kojoiotpf
+where aggkey = 1
