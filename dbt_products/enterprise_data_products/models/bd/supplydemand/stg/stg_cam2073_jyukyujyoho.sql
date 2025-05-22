77210ffb7aab3bdce65ad13490b9bab6826fe7ab
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-05-23 08:42:59 +0900

    add stg_cam2073_jyukyujyoho

---
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_cam2073_jyukyujyoho.sql
@@ -0,0 +1,27 @@
+with stg_cam2073_jyukyujyoho as (
+    select
+                ojyy::varchar(4) as ojyy, 
+        dum1::varchar(1) as dum1, 
+        segmei::varchar(8) as segmei, 
+        dlrcd::varchar(5) as dlrcd, 
+        odno::varchar(12) as odno, 
+        kubn::varchar(1) as kubn, 
+        dum2::varchar(103) as dum2, 
+        kubn2::varchar(1) as kubn2, 
+        yusosk2::varchar(8) as yusosk2, 
+        tsdate::varchar(8) as tsdate, 
+        jifuri::varchar(1) as jifuri, 
+        tokcd::varchar(1) as tokcd, 
+        kmaker::varchar(5) as kmaker, 
+        hassou::varchar(1) as hassou, 
+        seikyu::varchar(5) as seikyu, 
+        dum3::varchar(60) as dum3, 
+        create_user::varchar(15) as create_user, 
+        create_date,
+        update_user::varchar(15) as update_user, 
+        update_date,
+        _fivetran_synced as ldts
+    from {{ source('fivetran_database_oraclerds_orcl_jukyu_osamsp01sam202', 'raw_cam2073') }}
+    where _fivetran_deleted = 'false'
+)
+select * from stg_cam2073_jyukyujyoho
