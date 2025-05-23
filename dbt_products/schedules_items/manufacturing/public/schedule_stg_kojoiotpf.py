30c9642ec9b58de534fa133dcdf21d3c0bb2db41
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-05-23 13:20:06 +0900

    スケジュー_修正

--- dbt_products/schedules_items/manufacturing/public/schedule_stg_kojoiotpf.py
+++ dbt_products/schedules_items/manufacturing/public/schedule_stg_kojoiotpf.py
@@ -14,9 +14,7 @@ schedule_stg_kojoiotpf = build_schedule_from_dbt_selection(
     execution_timezone="Asia/Tokyo",
     dbt_select="+stg_denchijoho_trm_kojoiotpf +stg_denchijoho_ptr_kojoiotpf",
     config=RunConfig(ops={"dbt_products_assets":
-                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_kojoiotpf"},
-                                    source_test_list=["source:*,+stg_denchijoho_trm_kojoiotpf",
-                                                      "source:*,+stg_denchijoho_ptr_kojoiotpf"])
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_kojoiotpf"})
                           }
                      ),
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
