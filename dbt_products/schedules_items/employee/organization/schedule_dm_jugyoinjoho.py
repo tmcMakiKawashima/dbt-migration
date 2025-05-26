b51fb806d6aa025cd7aec0d594c130d6d12cdb04
ohta.kazuki_kuc <kazuki_ohta_zb@mail.toyota.co.jp>
2025-05-26 14:45:33 +0900

    従業員情報修正

--- dbt_products/schedules_items/employee/organization/schedule_dm_jugyoinjoho.py
+++ dbt_products/schedules_items/employee/organization/schedule_dm_jugyoinjoho.py
@@ -1,5 +1,5 @@
 from ....enterprise_data_products_assets.assets import DbtConfig, dbt_products_assets
-from dagster import DefaultScheduleStatus, RunConfig
+from dagster import RunConfig
 from dagster_dbt import build_schedule_from_dbt_selection
 from pathlib import Path
 import sys
@@ -13,6 +13,10 @@ schedule_dm_jugyoinjoho = build_schedule_from_dbt_selection(
     cron_schedule="00 03 * * *",
     execution_timezone="Asia/Tokyo",
     dbt_select="+dm_empmasterknm_view +dm_empmastershm_view",
+    config=RunConfig(ops={"dbt_products_assets":
+                          DbtConfig(dbt_vars={"DBT_JOB_NAME": "_dm_jugyoinjoho"})
+                          }
+                     ),
     tags={"ecs/cpu": "256", "ecs/memory": "1024",
           "job_name": "job_build_dm_jugyoinjoho"},
 )
