de66874dc8446acfcb854167016c6897de838b3b
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-06-09 13:29:15 +0900

    スケジュール追加

--- dbt_products/schedules.py
+++ dbt_products/schedules.py
@@ -23,6 +23,7 @@ from .schedules_items.supplydemand.public.schedule_stg_aqua import schedule_stg_
 from .schedules_items.supplydemand.public.schedule_stg_dly_rpt_mst import schedule_stg_dly_rpt_mst
 from .schedules_items.supplydemand.public.schedule_stg_dly_rpt import schedule_stg_dly_rpt
 from .schedules_items.supplydemand.public.schedule_stg_galc_kpi import schedule_stg_galc_kpi
+from .schedules_items.supplydemand.public.schedule_stg_jpass import schedule_stg_jpass
 from .schedules_items.supplydemand.public.schedule_stg_kodomo import schedule_stg_kodomo
 from .schedules_items.supplydemand.public.schedule_stg_siyou_soubi_siyoumst import schedule_stg_siyou_soubi_siyoumst
 from .schedules_items.supplydemand.public.schedule_stg_siyou_soubi_syasyu_siyousyo import schedule_stg_siyou_soubi_syasyu_siyousyo
@@ -95,6 +96,8 @@ schedules.append(schedule_stg_dly_rpt_mst)
 schedules.append(schedule_stg_dly_rpt)
 # 物流管理KPI(GALC_KPI)
 schedules.append(schedule_stg_galc_kpi)
+# JPASS
+schedules.append(schedule_stg_jpass)
 # コドモ
 schedules.append(schedule_stg_kodomo)
 # 仕様装備
