d2ddacd4434016108e560622cc1fc4e98670fcc1
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-06-18 23:25:10 +0900

    add stg_cmf_syaryosys

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_cmf_syaryosys.sql
@@ -0,0 +1,17 @@
+{% snapshot scd_cmf_syaryosys %}
+
+{{
+    config(
+        unique_key="concat_ws('-',
+                    idntno,
+                    load_date)",
+
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=True,
+    )
+}}
+
+select * from {{ ref('stg_cmf_syaryosys') }}
+
+{% endsnapshot %}
