5c985dda0c954e9536617708421c82b103e58a3f
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-06-30 18:14:39 +0900

    BD_バッチ処理状況TBL

---
+++ dbt_products/enterprise_data_products/snapshots/bd/valuechain/scd_batchsyorijokyo_pixy.sql
@@ -0,0 +1,15 @@
+{% snapshot scd_batchsyorijokyo_pixy %}
+{{
+    config(
+        unique_key="concat_ws('-',
+                    kinouid,
+                    syasyug
+                    )",
+
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=true,
+    )
+}}
+select * from {{ ref('stg_batchsyorijokyo_pixy') }}
+{% endsnapshot %}
