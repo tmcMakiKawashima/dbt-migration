c152f90f355b7113de7991aafd7776d7580c7e37
tomoyuki.okada_ctc <tomoyuki.okada.487@ctc-g.co.jp>
2025-07-15 15:41:22 +0900

    J-SLIM_車両PL物流拠点移動実績情報

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_cubc029logkyotenresult_vlc.sql
@@ -0,0 +1,19 @@
+{% snapshot scd_cubc029logkyotenresult_vlc %}
+
+{{
+    config(
+        unique_key="concat_ws('-',
+                    vhc_key,
+                    kyoten_cd,
+                    kyoten_kaisu,
+                    tp_kbn)",
+
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=true,
+    )
+}}
+
+select * from {{ ref('stg_cubc029logkyotenresult_vlc') }}
+
+{% endsnapshot %}
