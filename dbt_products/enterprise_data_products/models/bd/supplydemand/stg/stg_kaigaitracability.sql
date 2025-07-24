0f22a329c109171f285cce0a24d82da505b9a746
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-07-25 05:29:40 +0900

    AQUA_海外トレサビ_削除処理追加

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_kaigaitracability.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_kaigaitracability.sql
@@ -11,9 +11,20 @@
             'lok_y',
             'msgno'
             ],
-        incremental_strategy = 'merge'
+        incremental_strategy = 'merge',
+        post_hook="
+            {% if is_incremental() %}
+                delete from {{this}}
+                where (syadai_kt, frmno, wmi, vds, mdlyr, vin_vds_cd, lok_y, msgno)
+                in (select syadai_kt, frmno, wmi, vds, mdlyr, vin_vds_cd, lok_y, msgno
+                from {{ ref('substr_da5a214a') }}
+                where mtkbn = 'D'
+                and ldts >= (select max(ldts) from {{ this }}))
+            {% endif %}
+        "
     )
 }}
+-- MT区分が'D'に更新されたレコードを削除
 
 with stg_kaigaitracability as (
     select
