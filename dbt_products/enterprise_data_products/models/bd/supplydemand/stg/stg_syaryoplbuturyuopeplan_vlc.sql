2a2f0bfd5a2a846de85c5d15c79ab54134e794be
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-08-06 17:33:47 +0900

    J-SLIM_車両PL物流構内作業架装計画情報_テーブル名修正

--- dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_syaryoplbuturyuopeplan_vlc.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_syaryoplbuturyuopeplan_vlc.sql
@@ -16,7 +16,7 @@
 }}
 -- 削除フラグがtrueに更新されたレコードを削除
 
-with stg_cubc026logopeplan_vlc as (
+with stg_syaryoplbuturyuopeplan_vlc as (
     select
         vhc_key::varchar(15) as vhc_key, 
         plan_section::varchar(2) as plan_section, 
@@ -45,7 +45,7 @@ with stg_cubc026logopeplan_vlc as (
     from {{ source('fivetran_database_ogg_vlc_osubsp0200db20', 'raw_cubc026logopeplan') }}
     where _fivetran_deleted = 'false'
 )
-select * from stg_cubc026logopeplan_vlc
+select * from stg_syaryoplbuturyuopeplan_vlc
 
 {% if is_incremental() %}
     where ldts > (select max(ldts) from {{ this }})
