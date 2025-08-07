f9b40fb9bb1be46a89f9349153a9674e26b70351
atsuhito_kawachi_za@mail.toyota.co.jp <atsuhito_kawachi_za@mail.toyota.co.jp>
2025-08-07 10:11:03 +0900

    生産仕様実績の見える化_01_SPEC生産実績台数_=の前後の空白等の修正

--- dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_seisan_daisu.sql
+++ dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_seisan_daisu.sql
@@ -1,6 +1,6 @@
 {{ 
   config(
-    materialized='incremental',
+    materialized = 'incremental',
     incremental_strategy = 'append',
     transient = false,
     pre_hook = "
@@ -14,26 +14,26 @@
 
 with dm_siyo_seisan_daisu as (
     select
-        syasyu,-- 車種コード
-        haisya_kt,-- 配車型式
-        spec200,-- SPEC200桁組合せ
-        max(daisai200) as daisai200,-- SPEC対応4桁仕様
-        coalesce(int_cd, '') as int_cd,-- 内張コード
-        max(int_cd_iromei) as int_cd_iromei,-- 内張名
-        coalesce(ext_cd, '') as ext_cd,-- 鈑色コード
-        max(ext_cd_iromei) as ext_cd_iromei,-- 外鈑色名
-        coalesce(dest_cd, '') as dest_cd,-- 仕向地コード
-        max(dest) as dest,-- 仕向地
-        coalesce(koujyou_cd, '') as koujyou_cd,-- 工場コード
-        coalesce(eng_kt, '') as eng_kt,-- エンジン型式（生産管理）
-        coalesce(prodkuni_cd, '') as prodkuni_cd,-- 生産国コード
-        coalesce(psc, '') as psc,-- PSC
-        coalesce(o_idline, '') as o_idline,-- アイデントライン
-        coalesce(sk_y, '') as sk_y,-- 終検日年
-        coalesce(sk_m, '') as sk_m,-- 終検日月
+        syasyu, -- 車種コード
+        haisya_kt, -- 配車型式
+        spec200, -- SPEC200桁組合せ
+        max(daisai200) as daisai200, -- SPEC対応4桁仕様
+        coalesce(int_cd, '') as int_cd, -- 内張コード
+        max(int_cd_iromei) as int_cd_iromei, -- 内張名
+        coalesce(ext_cd, '') as ext_cd, -- 鈑色コード
+        max(ext_cd_iromei) as ext_cd_iromei, -- 外鈑色名
+        coalesce(dest_cd, '') as dest_cd, -- 仕向地コード
+        max(dest) as dest, -- 仕向地
+        coalesce(koujyou_cd, '') as koujyou_cd, -- 工場コード
+        coalesce(eng_kt, '') as eng_kt, -- エンジン型式（生産管理）
+        coalesce(prodkuni_cd, '') as prodkuni_cd, -- 生産国コード
+        coalesce(psc, '') as psc, -- PSC
+        coalesce(o_idline, '') as o_idline, -- アイデントライン
+        coalesce(sk_y, '') as sk_y, -- 終検日年
+        coalesce(sk_m, '') as sk_m, -- 終検日月
         count(*)::number(13,0) as daisu -- 台数
     from
-        {{ source('vinhis_db_spec','raw_dm_vinhis_spec200_allsalecar') }}
+        {{source('vinhis_db_spec','raw_dm_vinhis_spec200_allsalecar')}}
     group by
         syasyu,
         haisya_kt,
