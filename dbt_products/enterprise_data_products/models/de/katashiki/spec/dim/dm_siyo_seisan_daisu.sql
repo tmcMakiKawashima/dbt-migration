04ef2ac083d613d6098652274ab0f215ac3aa428
atsuhito_kawachi_za@mail.toyota.co.jp <atsuhito_kawachi_za@mail.toyota.co.jp>
2025-08-07 18:23:28 +0900

    133 01_SPEC生産実績台数 列別名誤りの修正 dais→daisu

--- dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_seisan_daisu.sql
+++ dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_seisan_daisu.sql
@@ -32,7 +32,7 @@ with dm_siyo_seisan_daisu as (
         coalesce(o_idline, '') as o_idline, -- アイデントライン
         coalesce(sk_y, '') as sk_y, -- 終検日年
         coalesce(sk_m, '') as sk_m, -- 終検日月
-        count(*)::number(13,0) as dais, -- 台数
+        count(*)::number(13,0) as daisu, -- 台数
         current_timestamp::timestamp_ntz as ldts -- 作成日時
     from
         {{source('vinhis_db_spec','raw_dm_vinhis_spec200_allsalecar')}}
