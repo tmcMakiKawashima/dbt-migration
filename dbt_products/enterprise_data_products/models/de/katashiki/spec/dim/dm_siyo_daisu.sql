172d78f43b19ba58e97560ad861f8f8466a5be5a
atsuhito_kawachi_za@mail.toyota.co.jp <atsuhito_kawachi_za@mail.toyota.co.jp>
2025-08-07 11:48:51 +0900

    133_生産仕様実績の見える化_02_SPEC台数_規約チェック

--- dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_daisu.sql
+++ dbt_products/enterprise_data_products/models/de/katashiki/spec/dim/dm_siyo_daisu.sql
@@ -1,9 +1,9 @@
 {{ 
   config(
-    materialized='incremental',
+    materialized = 'incremental',
     incremental_strategy = 'append',
     transient = false,
-    pre_hook="
+    pre_hook = "
       {% if is_incremental() %}
       delete from {{this}}
       {% endif %}
@@ -14,16 +14,16 @@
 
 with dm_siyo_daisu as (
     select
-        syasyu,
-        haisya_kt,
-        max(r_sfx_code) as r_sfx_code,
-        spec200,
-        max(daisai200) as daisai200,
-        sk_y,
-        sk_m,
-        sum(daisu) as daisu
+        syasyu,  -- 車種ｺｰﾄﾞ
+        haisya_kt, -- 配車型式
+        max(r_sfx_code) as r_sfx_code, -- SFXコード
+        spec200, -- SPEC200桁組合せ
+        max(daisai200) as daisai200, -- SPEC対応4桁仕様
+        sk_y, -- 終検日年
+        sk_m, -- 終検日月
+        sum(daisu)::number(13,0) as daisu -- 台数
     from
-        {{ ref('dm_siyo_seisan_daisu') }}
+        {{ref('dm_siyo_seisan_daisu')}}
     group by
         syasyu,
         haisya_kt,
