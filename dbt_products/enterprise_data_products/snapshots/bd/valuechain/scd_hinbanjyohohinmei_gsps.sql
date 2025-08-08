167cb03cef8daed9da02f964152ab7151c5d851f
da-masatosaeki-04.00 <masato.saeki@ctc-g.co.jp>
2025-08-08 13:14:41 +0900

    補給品納入LT　オープン化(補給DX)_品番情報.品名_テーブル名修正

--- dbt_products/enterprise_data_products/snapshots/bd/valuechain/scd_hinbanjyohohinmei_gsps.sql
+++ dbt_products/enterprise_data_products/snapshots/bd/valuechain/scd_hinbanjyohohinmei_gsps.sql
@@ -1,4 +1,4 @@
-{% snapshot scd_cvk01hinmeit_gsps %}
+{% snapshot scd_hinbanjyohohinmei_gsps %}
 
 {{
     config(
@@ -13,6 +13,6 @@
     )
 }}
 
-select * from {{ ref('stg_cvk01hinmeit_gsps') }}
+select * from {{ ref('stg_hinbanjyohohinmei_gsps') }}
 
 {% endsnapshot %}
