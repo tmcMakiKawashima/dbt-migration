bfb2b917f2828fbea31096728507aaa8e1c5aad6
YoshimiDaisuke <daisuke_yoshimi_zb@mail.toyota.co.jp>
2025-08-18 16:02:08 +0900

    feature-516-BD-stg_grade_info_tbl_syaryosys

---
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/stg/stg_grade_info_tbl_syaryosys.sql
@@ -0,0 +1,22 @@
+with stg_grade_info_tbl_syaryosys as (
+    select
+        sle_model::varchar(30) as sle_model,
+        carnm_cd::varchar(2) as carnm_cd,
+        spec_no::varchar(2) as spec_no,
+        capacity::number(2,0) as capacity,
+        eng_disp::number(5,0) as eng_disp,
+        fuel_cd::varchar(2) as fuel_cd,
+        eng_model::varchar(9) as eng_model,
+        tire_cd::varchar(3) as tire_cd,
+        ldts::timestamp_ntz(9) as ldts,
+        row_number() over (
+            partition by sle_model
+            order by ldts desc, line_number desc
+        ) aggkey
+    from {{ref('substr_ktrla038zz0kadcb82')}}
+    where mtkbn = 'I'
+    and ldts = (select max(ldts) from {{ref('substr_ktrla038zz0kadcb82')}})
+)
+select * exclude(aggkey)
+from stg_grade_info_tbl_syaryosys
+where aggkey = 1
