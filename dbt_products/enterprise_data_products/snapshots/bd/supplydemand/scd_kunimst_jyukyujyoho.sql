1d3e27fcc500796b25da7077735f78f4e77b329b
tomoyuki.okada_ctc <tomoyuki.okada.487@ctc-g.co.jp>
2025-08-22 15:30:18 +0900

    生産日報_標準規約に従いsnapshot修正

--- dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_kunimst_jyukyujyoho.sql
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_kunimst_jyukyujyoho.sql
@@ -8,7 +8,7 @@
 
         strategy='timestamp',
         updated_at='ldts',
-        invalidate_hard_deletes=True,
+        invalidate_hard_deletes=true,
     )
 }}
 
--- dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_vlt_syaryosys.sql
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_vlt_syaryosys.sql
@@ -14,7 +14,7 @@
 
         strategy='timestamp',
         updated_at='ldts',
-        invalidate_hard_deletes=True,
+        invalidate_hard_deletes=true,
     )
 }}
 
