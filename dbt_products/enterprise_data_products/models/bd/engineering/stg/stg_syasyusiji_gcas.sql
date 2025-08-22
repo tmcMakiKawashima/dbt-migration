5bdb223f95afd83bbbb4e518df05cd163a8271e9
da-yutateradaz-01.00 <da-yutateradaz-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-08-22 10:45:40 +0900

    洗い替えに変更

--- dbt_products/enterprise_data_products/models/bd/engineering/stg/stg_syasyusiji_gcas.sql
+++ dbt_products/enterprise_data_products/models/bd/engineering/stg/stg_syasyusiji_gcas.sql
@@ -1,23 +1,3 @@
-{{
-    config (
-        materialized = 'incremental',
-        unique_key = ['syasyu', 'hansiyono8', 'jyokenno'],
-        incremental_strategy = 'merge',
-        pre_hook ="
-            {% if is_incremental() %}
-                delete from {{ this }} stg
-                using 
-                  {{ source('fivetran_database_ogg_gcas','raw_cyyg9syasyusiji') }} raw
-                   where 
-                    stg.syasyu = raw.syasyu
-                    and stg.hansiyono8 = raw.hansiyono8
-                    and stg.jyokenno = raw.jyokenno
-                    and raw._fivetran_deleted = 'true'
-            {% endif %}
-        "
-    )
-}}
---pre_hookによる不要データ削除処理を実装
 with stg_syasyusiji_gcas as (
     select 
         syasyu::varchar(4) as syasyu,
@@ -39,10 +19,5 @@ with stg_syasyusiji_gcas as (
         _fivetran_synced::timestamp_ntz as ldts
     from {{ source('fivetran_database_ogg_gcas','raw_cyyg9syasyusiji') }}
     where _fivetran_deleted = 'false'
-
-    {% if is_incremental() %}
-        and _fivetran_synced > (select max(ldts) from {{ this }})
-    {% endif %}
-
 )
 select * from stg_syasyusiji_gcas
