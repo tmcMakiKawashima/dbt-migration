f1d10c8e21c0020933b4d0ed66f9f37df47c6b3a
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-09-12 13:08:35 +0900

    siyou-soubiスケジュールfix

--- dbt_products/schedules.py
+++ dbt_products/schedules.py
@@ -21,6 +21,7 @@ from .schedules_items.supplydemand.public.schedule_stg_aqua import schedule_stg_
 from .schedules_items.supplydemand.public.schedule_stg_dly_rpt_mst import schedule_stg_dly_rpt_mst
 from .schedules_items.supplydemand.public.schedule_stg_dly_rpt import schedule_stg_dly_rpt
 from .schedules_items.supplydemand.public.schedule_stg_galc_kpi import schedule_stg_galc_kpi
+from .schedules_items.supplydemand.public.schedule_stg_jyukyujyoho_0730 import schedule_stg_jyukyujyoho_0730
 from .schedules_items.supplydemand.public.schedule_stg_kodomo import schedule_stg_kodomo
 from .schedules_items.supplydemand.public.schedule_stg_siyou_soubi_siyoumst import schedule_stg_siyou_soubi_siyoumst
 from .schedules_items.supplydemand.public.schedule_stg_siyou_soubi_syasyu_siyousyo import schedule_stg_siyou_soubi_syasyu_siyousyo
@@ -88,6 +89,8 @@ schedules.append(schedule_stg_dly_rpt_mst)
 schedules.append(schedule_stg_dly_rpt)
 # 物流管理KPI(GALC_KPI)
 schedules.append(schedule_stg_galc_kpi)
+# 需給情報基盤_0730
+schedules.append(schedule_stg_jyukyujyoho_0730)
 # コドモ
 schedules.append(schedule_stg_kodomo)
 # 仕様装備
