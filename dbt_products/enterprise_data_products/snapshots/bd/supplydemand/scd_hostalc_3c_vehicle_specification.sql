03878a815f1dc50788dd75437445874ad0291b25
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-09-19 11:50:30 +0900

    BD_物流管理KPI_北米_車両仕様_2u-3c

---
+++ dbt_products/enterprise_data_products/snapshots/bd/supplydemand/scd_hostalc_3c_vehicle_specification.sql
@@ -0,0 +1,18 @@
+{% snapshot scd_hostalc_3c_vehicle_specification %}
+
+{{
+  config(
+    unique_key="concat_ws('-',
+          pscexlk,
+          plantcode,
+          urn)",
+
+    strategy='timestamp',
+    updated_at='ldts',
+    invalidate_hard_deletes=true,
+  )
+}}
+
+select * from {{ ref('stg_hostalc_3c_vehicle_specification') }}
+
+{% endsnapshot %}
