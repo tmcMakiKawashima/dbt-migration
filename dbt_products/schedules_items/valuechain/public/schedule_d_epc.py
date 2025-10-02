1284f9190c7dc66a24ff8966f3b57ca0c69d8e3a
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-02 10:11:12 +0900

    schedule hybirddelete fix

--- dbt_products/schedules_items/valuechain/public/schedule_d_epc.py
+++ dbt_products/schedules_items/valuechain/public/schedule_d_epc.py
@@ -12,7 +12,7 @@ schedule_d_epc = build_schedule_from_dbt_selection(
     schedule_name="D_SOU_EPC",
     cron_schedule="00 05 * * *",
     execution_timezone="Asia/Tokyo",
-    dbt_select="+stg_shiyopattern +stg_kirikaecodekensaku +stg_trimcodejoho +stg_tyotatsuhinbanjoho +stg_daitaihinbanjoho +stg_emokujikensaku +stg_figkensaku +stg_katashikipattern +stg_tactihinbanjoho +stg_tokuchokigojoho +stg_buidaibunruikensakujoho +stg_buicyubunruikensakujoho +stg_buigroupkensakujoho +stg_buisyobunruikensakujoho",
+    dbt_select="+stg_shiyopattern +stg_kirikaecodekensaku +stg_trimcodejoho +stg_tyotatsuhinbanjoho +stg_daitaihinbanjoho +stg_emokujikensaku +stg_figkensaku +stg_katashikipattern +stg_tactihinbanjoho +stg_tokuchokigojoho +stg_buidaibunruikensakujoho +stg_buicyubunruikensakujoho +stg_buigroupkensakujoho +stg_buisyobunruikensakujoho +stg_bunkaizuimagekanri +stg_bunkaizukensaku +stg_figmeisyo +stg_framekatashiki +stg_hinbankensakutype1 +stg_hinbankensakutype2 +stg_hinmei +stg_illustcomment +stg_syamei +stg_syaryokatashikijoho +stg_syaryoshiyosobijoho",
     config=RunConfig(ops={"dbt_products_assets":
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_d_epc"},
                                     source_test_list=["source:*,+stg_shiyopattern",
@@ -28,7 +28,18 @@ schedule_d_epc = build_schedule_from_dbt_selection(
                                                       "source:*,+stg_buidaibunruikensakujoho",
                                                       "source:*,+stg_buicyubunruikensakujoho",
                                                       "source:*,+stg_buigroupkensakujoho",
-                                                      "source:*,+stg_buisyobunruikensakujoho"])
+                                                      "source:*,+stg_buisyobunruikensakujoho",
+                                                      "source:*,+stg_bunkaizuimagekanri",
+                                                      "source:*,+stg_bunkaizukensaku",
+                                                      "source:*,+stg_figmeisyo",
+                                                      "source:*,+stg_framekatashiki",
+                                                      "source:*,+stg_hinbankensakutype1",
+                                                      "source:*,+stg_hinbankensakutype2",
+                                                      "source:*,+stg_hinmei",
+                                                      "source:*,+stg_illustcomment",
+                                                      "source:*,+stg_syamei",
+                                                      "source:*,+stg_syaryokatashikijoho",
+                                                      "source:*,+stg_syaryoshiyosobijoho"])
                           }
                      ),
     # default_status=DefaultScheduleStatus.RUNNING,
