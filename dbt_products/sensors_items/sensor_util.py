b1145efd59a22aac2c2a069d360a6708a3bce5d0
Kuboi Erika <e-kuboi@toyotasystems.com>
2025-10-02 04:05:32 +0000

    change directories for kestra

--- dbt_products/sensors_items/sensor_util.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-tags_base = {"ecs/cpu": "256", "ecs/memory": "1024"}
-# sensorのポーリングインターバル
-mini_interval_sec = int(os.getenv('SENSOR_MINI_INTERVAL_SEC'))
-# code location name
-loc_name = "dbt_products.definitions"
