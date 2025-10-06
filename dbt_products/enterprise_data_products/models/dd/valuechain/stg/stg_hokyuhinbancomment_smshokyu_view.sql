f5b9ea66e4b9754c1f8c12b23bf7336087175b9c
da-yutateradaz-01.00 <da-yutateradaz-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-06 15:00:12 +0900

    単テレビュー指摘事項の対応

--- dbt_products/enterprise_data_products/models/dd/valuechain/stg/stg_hokyuhinbancomment_smshokyu_view.sql
+++ dbt_products/enterprise_data_products/models/dd/valuechain/stg/stg_hokyuhinbancomment_smshokyu_view.sql
@@ -17,6 +17,6 @@ with stg_hokyuhinbancomment_smshokyu_view as (
     kosinsya::varchar(16) as kosinsya, -- 更新者
     ldts -- B層取込日時
   from {{ ref('stg_hokyuhinbancomment_smshokyu') }}
+  where syusbetu = '01TM'
 )
 select * from stg_hokyuhinbancomment_smshokyu_view
-where syusbetu = '01TM'
