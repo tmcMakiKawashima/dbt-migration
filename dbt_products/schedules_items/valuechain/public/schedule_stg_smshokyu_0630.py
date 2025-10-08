75a243ae944160d011e40315a0d21dfb75841d2e
ohta.kazuki_kuc <kazuki_ohta_zb@mail.toyota.co.jp>
2025-10-08 10:31:51 +0900

    補給sms_schedule修正

--- dbt_products/schedules_items/valuechain/public/schedule_stg_smshokyu_0630.py
+++ dbt_products/schedules_items/valuechain/public/schedule_stg_smshokyu_0630.py
@@ -17,6 +17,6 @@ schedule_stg_smshokyu_0630 = build_schedule_from_dbt_selection(
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_smshokyu_0630"})
                           }
                      ),
-    tags={"ecs/cpu": "256", "ecs/memory": "1024",
+    tags={"ecs/cpu": "512", "ecs/memory": "2048",
           "job_name": "job_build_stg_smshokyu_0630"},
 )
