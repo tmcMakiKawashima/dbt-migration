e41943db748530853949e8134017e369c9467ba2
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-08 18:04:56 +0900

    schedulue specup fix

--- dbt_products/schedules_items/customerservice/public/schedule_stg_iqas_0000.py
+++ dbt_products/schedules_items/customerservice/public/schedule_stg_iqas_0000.py
@@ -17,6 +17,6 @@ schedule_stg_iqas_0000 = build_schedule_from_dbt_selection(
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_iqas_0000"})
                           }
                      ),
-    tags={"ecs/cpu": "256", "ecs/memory": "1024",
+    tags={"ecs/cpu": "512", "ecs/memory": "2048",
           "job_name": "job_build_stg_iqas_0000"},
 )
