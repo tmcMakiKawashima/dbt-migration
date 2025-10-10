d6debe6ce9feebd38a11a4ceea418986fb4a7f9b
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-10 09:32:17 +0900

    bytecheck fix

--- dbt_products/enterprise_data_products/models/bd/supplydemand/substr/substr_ktrla01dzz0kalc015.sql
+++ dbt_products/enterprise_data_products/models/bd/supplydemand/substr/substr_ktrla01dzz0kalc015.sql
@@ -1,13 +1,14 @@
 with substr_ktrla01dzz0kalc015 as (
-    select
-        substr(raw_data, 1, 7)::varchar(7) as kncode,
-        substr(raw_data, 8, 60)::varchar(60) as sknmmei,
-        substr(raw_data, 68, 1)::varchar(1) as knmsortjun,
-        substr(raw_data, 69, 8)::varchar(8) as creymd,
-        substr(raw_data, 77, 18)::varchar(18) as mtuser,
-        substr(raw_data, 95, 26)::varchar(26) as mttime,
-        ldts,
-        line_number
-    from {{source('snowpipe_db_supplydemand', 'raw_ktrla01dzz0kalc015')}}
+  select
+    to_varchar(substr(raw_data, 1, 7), 'utf-8')::varchar(7) as kncode,
+    to_varchar(substr(raw_data, 8, 60), 'utf-8')::varchar(60) as sknmmei,
+    to_varchar(substr(raw_data, 68, 1), 'utf-8')::varchar(1) as knmsortjun,
+    to_varchar(substr(raw_data, 69, 8), 'utf-8')::varchar(8) as creymd,
+    to_varchar(substr(raw_data, 77, 18), 'utf-8')::varchar(18) as mtuser,
+    to_varchar(substr(raw_data, 95, 26), 'utf-8')::varchar(26) as mttime,
+    ldts,
+    line_number
+  from {{ ref('byte_ktrla01dzz0kalc015') }}
 )
+
 select * from substr_ktrla01dzz0kalc015
