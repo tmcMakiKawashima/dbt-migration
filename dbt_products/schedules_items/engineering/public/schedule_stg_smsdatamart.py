99064c7b881d1d98f06a31fe3d28ae3a98273860
da-namiki-01.00 <da-namiki-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-13 18:01:55 +0900

    メモリスペックアップ fix

--- dbt_products/schedules_items/engineering/public/schedule_stg_smsdatamart.py
+++ dbt_products/schedules_items/engineering/public/schedule_stg_smsdatamart.py
@@ -17,6 +17,6 @@ schedule_stg_smsdatamart = build_schedule_from_dbt_selection(
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_smsdatamart"})
                           }
                      ),
-    tags={"ecs/cpu": "256", "ecs/memory": "1024",
+    tags={"ecs/cpu": "512", "ecs/memory": "2048",
           "job_name": "job_build_stg_smsdatamart"},
 )
