b93d5facbf09cf1728c849f11d9edd8f66f99974
後藤太聖 <taisei_goto_za@mail.toyota.co.jp>
2025-10-16 18:05:35 +0900

    スケジュールファイル修正

--- dbt_products/schedules_items/valuechain/public/schedule_stg_atop_0400.py
+++ dbt_products/schedules_items/valuechain/public/schedule_stg_atop_0400.py
@@ -12,7 +12,7 @@ schedule_stg_atop_0400 = build_schedule_from_dbt_selection(
     schedule_name="ATOP_0400",
     cron_schedule="00 04 * * *",
     execution_timezone="Asia/Tokyo",
-    dbt_select="+stg_sonekikanri_atop +stg_sonekikanri_atop",
+    dbt_select="+stg_sonekikanri_atop +stg_ukeirejisseki_atop",
     config=RunConfig(ops={"dbt_products_assets":
                           DbtConfig(dbt_vars={"DBT_JOB_NAME": "_stg_atop_0400"},
                                     source_test_list=["source:*,stg_sonekikanri_atop",
