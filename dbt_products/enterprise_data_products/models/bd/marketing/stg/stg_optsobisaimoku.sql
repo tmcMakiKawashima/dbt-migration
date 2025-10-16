c8c248df33ca6c69150399b0f7e2a41724093dd0
da-yutateradaz-01.00 <da-yutateradaz-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-16 09:38:37 +0900

    レビュー指摘事項修正

--- dbt_products/enterprise_data_products/models/bd/marketing/stg/stg_optsobisaimoku.sql
+++ dbt_products/enterprise_data_products/models/bd/marketing/stg/stg_optsobisaimoku.sql
@@ -1,9 +1,16 @@
+{{
+    config (
+        materialized = 'incremental',
+        unique_key = ['syasyu_cd', 'ctg_cd', 'chg_no', 'shamei', 'cnl_cd','eqp_mgr_id','eqp_cd','eqp_dtl_cd'],
+        incremental_strategy = 'merge',
+    )
+}}
 with stg_optsobisaimoku as (
     select
-        syasyu_cd::varchar(4) as syasyu_cd,
+        car_kind_cd::varchar(4) as syasyu_cd,
         ctg_cd::varchar(1) as ctg_cd,
         chg_no::varchar(3) as chg_no,
-        shamei::varchar(2) as shamei,
+        car_name_cd::varchar(2) as shamei,
         cnl_cd::varchar(1) as cnl_cd,
         eqp_mgr_id::varchar(9) as eqp_mgr_id,
         eqp_cd::varchar(8) as eqp_cd,
@@ -12,12 +19,12 @@ with stg_optsobisaimoku as (
         eqp_dtl_org::varchar(150) as eqp_dtl_org,
         eqp_dtl_chg_kanji::varchar(768) as eqp_dtl_chg_kanji,
         eqp_dtl_chg::varchar(180) as eqp_dtl_chg,
-        shiyodai::varchar(3) as shiyodai,
-        shiyosai::varchar(4) as shiyosai,
+        veh_eqp_cd::varchar(3) as shiyodai,
+        veh_eqp_dtl_cd::varchar(4) as shiyosai,
         dsp_odr::varchar(5) as dsp_odr,
         ldts,
         row_number() over (
-            partition by syasyu_cd, ctg_cd, chg_no, shamei, cnl_cd, eqp_mgr_id, eqp_cd, eqp_dtl_cd
+            partition by car_kind_cd, ctg_cd, chg_no, car_name_cd, cnl_cd, eqp_mgr_id, eqp_cd, eqp_dtl_cd
             order by ldts desc, line_number desc
         ) aggkey
     from {{ ref('substr_n8jfim06') }}
