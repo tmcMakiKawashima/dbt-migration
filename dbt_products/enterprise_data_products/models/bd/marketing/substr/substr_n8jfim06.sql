d2585be078180d4baaf640ee439413727e8ecb77
da-yutateradaz-01.00 <da-yutateradaz-01.00@dajp1-dxpf-ws-bstn2.co.jp>
2025-10-17 11:08:52 +0900

    バイト分割に修正

--- dbt_products/enterprise_data_products/models/bd/marketing/substr/substr_n8jfim06.sql
+++ dbt_products/enterprise_data_products/models/bd/marketing/substr/substr_n8jfim06.sql
@@ -1,22 +1,22 @@
 with substr_n8jfim06 as (
   select
-    substr(raw_data, 2, 4)::varchar(4) as car_kind_cd,
-    substr(raw_data, 6, 1)::varchar(1) as ctg_cd,
-    substr(raw_data, 7, 3)::varchar(3) as chg_no,
-    substr(raw_data, 10, 2)::varchar(2) as car_name_cd,
-    substr(raw_data, 12, 1)::varchar(1) as cnl_cd,
-    substr(raw_data, 13, 9)::varchar(9) as eqp_mgr_id,
-    substr(raw_data, 22, 8)::varchar(8) as eqp_cd,
-    substr(raw_data, 30, 4)::varchar(4) as eqp_dtl_cd,
-    substr(raw_data, 34, 1)::varchar(1) as data_kbn,
-    substr(raw_data, 35, 150)::varchar(150) as eqp_dtl_org,
-    substr(raw_data, 185, 768)::varchar(768) as eqp_dtl_chg_kanji,
-    substr(raw_data, 953, 180)::varchar(180) as eqp_dtl_chg,
-    substr(raw_data, 1133, 3)::varchar(3) as veh_eqp_cd,
-    substr(raw_data, 1136, 4)::varchar(4) as veh_eqp_dtl_cd,
-    substr(raw_data, 1140, 5)::varchar(5) as dsp_odr,
+    to_varchar(substr(raw_data, 2, 4), 'utf-8')::varchar(4) as car_kind_cd,
+    to_varchar(substr(raw_data, 6, 1), 'utf-8')::varchar(1) as ctg_cd,
+    to_varchar(substr(raw_data, 7, 3), 'utf-8')::varchar(3) as chg_no,
+    to_varchar(substr(raw_data, 10, 2), 'utf-8')::varchar(2) as car_name_cd,
+    to_varchar(substr(raw_data, 12, 1), 'utf-8')::varchar(1) as cnl_cd,
+    to_varchar(substr(raw_data, 13, 9), 'utf-8')::varchar(9) as eqp_mgr_id,
+    to_varchar(substr(raw_data, 22, 8), 'utf-8')::varchar(8) as eqp_cd,
+    to_varchar(substr(raw_data, 30, 4), 'utf-8')::varchar(4) as eqp_dtl_cd,
+    to_varchar(substr(raw_data, 34, 1), 'utf-8')::varchar(1) as data_kbn,
+    to_varchar(substr(raw_data, 35, 150), 'utf-8')::varchar(150) as eqp_dtl_org,
+    to_varchar(substr(raw_data, 185, 768), 'utf-8')::varchar(768) as eqp_dtl_chg_kanji,
+    to_varchar(substr(raw_data, 953, 180), 'utf-8')::varchar(180) as eqp_dtl_chg,
+    to_varchar(substr(raw_data, 1133, 3), 'utf-8')::varchar(3) as veh_eqp_cd,
+    to_varchar(substr(raw_data, 1136, 4), 'utf-8')::varchar(4) as veh_eqp_dtl_cd,
+    to_varchar(substr(raw_data, 1140, 5), 'utf-8')::varchar(5) as dsp_odr,
     ldts,
     line_number
-  from {{ source('snowpipe_db_marketing', 'raw_n8jfim06')}}
+  from {{ ref('byte_n8jfim06')}}
 )
 select * from substr_n8jfim06
