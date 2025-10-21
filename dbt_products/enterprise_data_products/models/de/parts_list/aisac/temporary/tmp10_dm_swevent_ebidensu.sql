c4ab88782d150a19469c72806b312b4818ee6d34
da-dxpf-tetsuya_kondo_zj@mail.toyota.co.jp <tetsuya_kondo_zj.mail.toyota.co.jp>
2025-10-21 11:19:06 +0900

    指摘対応

--- dbt_products/enterprise_data_products/models/de/parts_list/aisac/temporary/tmp10_dm_swevent_ebidensu.sql
+++ dbt_products/enterprise_data_products/models/de/parts_list/aisac/temporary/tmp10_dm_swevent_ebidensu.sql
@@ -1,21 +1,33 @@
+with 
+    ev as (
+            select 
+                  eventno,                       -- イベントNo
+                  syasyu,                        -- 車種コード
+                  eventmei,                      -- イベント名
+                  syonintime,                    -- 承認日時
+                  seppenhinkou,                  -- 設変内容-品番構成変更
+                  seppensyaryohiki,              -- 設変内容-車両引当変更
+                  evidenceumu,                   -- エビデンス有無
+                  systorokutime,                 -- システム登録日時
+                  systorokukaisyacode,           -- システム登録会社コード
+                  systorokuuserid,               -- システム登録ユーザID
+                  systorokuaplid,                -- システム登録アプリID
+                  syskosintime,                  -- システム更新日時
+                  syskosinkaisyacode,            -- システム更新会社コード
+                  syskosinuserid,                -- システム更新ユーザID
+                  syskosinaplid,                 -- システム更新アプリID
+              from {{ ref('stg_swevent_ebidensu_aisac') }} 
+    ),
+    sy as (
+            select syasyu,  -- 車種ユニットコード
+                   syukan   -- 主管会社
+              from {{ ref('stg_syasyu') }}
+    )
+
 select 
-    ev.eventno,                       -- イベントNo
-    ev.syasyu,                        -- 車種コード
-    ev.eventmei,                      -- イベント名
-    ev.syonintime,                    -- 承認日時
-    ev.seppenhinkou,                  -- 設変内容-品番構成変更
-    ev.seppensyaryohiki,              -- 設変内容-車両引当変更
-    ev.evidenceumu,                   -- エビデンス有無
-    ev.systorokutime,                 -- システム登録日時
-    ev.systorokukaisyacode,           -- システム登録会社コード
-    ev.systorokuuserid,               -- システム登録ユーザID
-    ev.systorokuaplid,                -- システム登録アプリID
-    ev.syskosintime,                  -- システム更新日時
-    ev.syskosinkaisyacode,            -- システム更新会社コード
-    ev.syskosinuserid,                -- システム更新ユーザID
-    ev.syskosinaplid,                 -- システム更新アプリID
-    coalesce(sy.syukan,'')::varchar(2) as syukan,  -- 主管会社
+    ev.*,
+    coalesce(sy.syukan,'')::varchar(2) as syukan,
     current_timestamp()::timestamp_ntz(9) as ldts  -- 最終更新日時
-  from {{ ref('stg_swevent_ebidensu') }} as ev
-  left outer join {{ ref('stg_syasyu') }} as sy
+  from ev
+  left outer join sy
     on sy.syasyu = ev.syasyu
