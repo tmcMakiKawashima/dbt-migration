2f7f590b3c2557ccc5be01e92d813d035ffdca4f
Kuboi Erika <e-kuboi@toyotasystems.com>
2025-11-03 14:28:17 +0900

    chenge directries

--- dbt_products/sensors_items/sensor_util.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-tags_base = {"ecs/cpu": "512", "ecs/memory": "2048"}
-# sensorのポーリングインターバル
-mini_interval_sec = int(os.getenv('SENSOR_MINI_INTERVAL_SEC'))
-# code location name
-loc_name = "dbt_products.definitions"
