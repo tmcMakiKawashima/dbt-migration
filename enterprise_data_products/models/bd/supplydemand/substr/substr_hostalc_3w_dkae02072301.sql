c0c8fa02e6361a26aa335983ff3c4e25621772b2
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-12-04 14:48:31 +0900

    生産進度_資産格納

--- enterprise_data_products/models/bd/supplydemand/substr/substr_hostalc_3w_dkae02072301.sql
+++ enterprise_data_products/models/bd/supplydemand/substr/substr_hostalc_3w_dkae02072301.sql
@@ -17,8 +17,8 @@ with substr_hostalc_3w_dkae02072301 as (
     substr(raw_data, 57, 1)::varchar(1) as odrtype,
     substr(raw_data, 58, 2)::varchar(2) as vehcategorycode,
     substr(raw_data, 60, 14)::varchar(14) as updateymdel14dg,
-    ldts
+    ldts,
+    line_number
   from {{source('snowpipe_db_supplydemand', 'raw_hostalc_3w_dkae02072301')}}
 )
-
 select * from substr_hostalc_3w_dkae02072301
