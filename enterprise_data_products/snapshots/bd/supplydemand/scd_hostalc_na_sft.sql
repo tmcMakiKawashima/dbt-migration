ac2e3a7ed58f226d8bf87f854340f6ca80ed4aee
teramura <tsukina_teramura_za@mail.toyota.co.jp>
2025-12-05 23:29:01 +0900

    SFデータ_資産格納

--- enterprise_data_products/snapshots/bd/supplydemand/scd_hostalc_na_sft.sql
+++ enterprise_data_products/snapshots/bd/supplydemand/scd_hostalc_na_sft.sql
@@ -1,19 +1,18 @@
 {% snapshot scd_hostalc_na_sft %}
 
 {{
-  config(
-    unique_key="concat_ws('-',
-          psc,
-          plantcode,
-          lodate,
-          idno)",
+    config(
+        unique_key="concat_ws('-',
+                    psc,
+                    plantcode,
+                    lodate,
+                    idno)",
 
-    strategy='timestamp',
-    updated_at='ldts',
-    invalidate_hard_deletes=true,
-  )
+        strategy='timestamp',
+        updated_at='ldts',
+        invalidate_hard_deletes=true,
+    )
 }}
-
 select * from {{ ref('stg_hostalc_na_sft') }}
 
 {% endsnapshot %}
