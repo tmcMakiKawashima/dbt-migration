9f2bb21611ed0d7576fd9c6eca4b828793da2e65
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2025-12-08 09:40:15 +0900

    PK項目のNULL変換対応

--- enterprise_data_products/models/de/katashiki/spec/temporary/dm_siyo_seisan_daisu/tmp71_dm_siyo_seisan_daisu.sql
+++ enterprise_data_products/models/de/katashiki/spec/temporary/dm_siyo_seisan_daisu/tmp71_dm_siyo_seisan_daisu.sql
@@ -18,7 +18,10 @@ with t70 as (
         daisu,                            -- 台数
         keta_no,                          -- 桁No
         kigo                              -- specを1文字づつ200分割した記号
-    from {{ref('tmp70_dm_siyo_seisan_daisu')}}
+    from {{source('katashiki_db_spec','raw_tmp70_dm_siyo_seisan_daisu')}}
+    {% raw %}
+    -- from {{ref('tmp70_dm_siyo_seisan_daisu')}}
+    {% endraw %}
 ), sh as (
     select
         syasyu,    -- 車種ｺｰﾄﾞ
@@ -26,27 +29,30 @@ with t70 as (
         s1kigo,    -- spec記号
         siyoudai4, -- 仕様コード(4桁)大分類
         siyousai4  -- 仕様コード(4桁)細目
-    from {{ref('stg_siyouhenkan')}}
+    from {{source('supplydemand_db_public','raw_stg_siyouhenkan')}}
+    {% raw %}
+    -- from {{ref('stg_siyouhenkan')}}
+    {% endraw %}
 )
 select
-    t70.syasyu,
-    t70.kata,
+    coalesce(t70.syasyu, '') as syasyu,
+    coalesce(t70.kata, '') as kata,
     null::varchar(2) as r_sfx_code,
-    t70.spec,
+    coalesce(t70.spec, '') as spec,
     listagg(sh.siyoudai4 || sh.siyousai4, '')
     within group (order by t70.keta_no)::varchar(800) as spec200_siyo, -- SPEC対応4桁仕様,
-    t70.intcode,
+    coalesce(t70.intcode, '') as intcode,
     t70.int_cd_iromei,
-    t70.extcode,
+    coalesce(t70.extcode, '') as extcode,
     t70.ext_cd_iromei,
-    t70.dest_cd,
+    coalesce(t70.dest_cd, '') as dest_cd,
     t70.dest,
-    t70.plantcode,
-    t70.enginekata,
-    t70.pscexlk,
-    t70.idline,
-    t70.sk_y,
-    t70.sk_m,
+    coalesce(t70.plantcode, '') as plantcode,
+    coalesce(t70.enginekata, '') as enginekata,
+    coalesce(t70.pscexlk, '') as pscexlk,
+    coalesce(t70.idline, '') as idline,
+    coalesce(t70.sk_y, '') as sk_y,
+    coalesce(t70.sk_m, '') as sk_m,
     t70.daisu,
     '1' as naiji_flg -- 内示実績FLG
 from t70
