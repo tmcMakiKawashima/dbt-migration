b5f2799c25d963dc1b826da3d0ce6d058643c33f
ohta.kazuki_kuc <kazuki_ohta_zb@mail.toyota.co.jp>
2025-12-12 17:17:45 +0900

    sfh資産反映

--- enterprise_data_products/snapshots/bd/supplydemand/scd_jfy1_sfh_history_iotpf.sql
+++ enterprise_data_products/snapshots/bd/supplydemand/scd_jfy1_sfh_history_iotpf.sql
@@ -1,4 +1,4 @@
-{% snapshot scd_jfy1_sfh_history_iotpf_kojoiot_subaru%}
+{% snapshot scd_jfy1_sfh_history_iotpf%}
 
 {{
     config( 
@@ -14,6 +14,6 @@
     )
 }}
 
-select * from {{ ref('stg_jfy1_sfh_history_iotpf_kojoiot_subaru') }} 
+select * from {{ ref('stg_jfy1_sfh_history_iotpf') }} 
 
  {% endsnapshot %}
