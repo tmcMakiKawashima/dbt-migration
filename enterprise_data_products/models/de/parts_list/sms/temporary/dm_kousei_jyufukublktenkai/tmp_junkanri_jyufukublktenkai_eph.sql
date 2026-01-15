a97ff37393da03e0e4371443908f1b1d1d9812bc
kazuma_washino_za <kazuma_washino_za@mail.toyota.co.jp>
2026-01-15 13:53:14 +0900

    規約対応

--- enterprise_data_products/models/de/parts_list/sms/temporary/dm_kousei_jyufukublktenkai/tmp_junkanri_jyufukublktenkai_eph.sql
+++ enterprise_data_products/models/de/parts_list/sms/temporary/dm_kousei_jyufukublktenkai/tmp_junkanri_jyufukublktenkai_eph.sql
@@ -3,6 +3,6 @@
 }}
 -- 循環処理のためE層のテーブルをソースとして扱う
 with raw_tmp_junkanri_jyufukublktenkai as (
-    select * from {{source('parts_list_db_public', 'raw_tmp_junkanri_jyufukublktenkai')}}
+    select * from {{source('parts_list_db_sms', 'raw_tmp_junkanri_jyufukublktenkai')}}
 )
 select * from raw_tmp_junkanri_jyufukublktenkai
