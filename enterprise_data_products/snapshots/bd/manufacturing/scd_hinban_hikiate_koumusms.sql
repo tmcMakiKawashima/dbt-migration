350a82d772ca985bc827db2e7cda8413e82fb672
Motomura Koki <fj6856ad@fujitsu.com>
2026-01-22 11:18:35 +0900

    規約レビュー対応

--- enterprise_data_products/snapshots/bd/manufacturing/scd_hinban_hikiate_koumusms.sql
+++ enterprise_data_products/snapshots/bd/manufacturing/scd_hinban_hikiate_koumusms.sql
@@ -43,7 +43,7 @@
 
         strategy='timestamp',
         updated_at='ldts',
-        invalidate_hard_deletes=True,
+        invalidate_hard_deletes=true,
     )
 }}
 select * from {{ ref('stg_hinban_hikiate_koumusms') }}
