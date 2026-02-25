c94bd31a259dd6f5199dd7878a26cd166fe4f595
yuta_terada_zb <yuta_terada_zb@mail.toyota.co.jp>
2026-02-26 08:56:14 +0900

    旧型組立番号名称　実装

---
+++ enterprise_data_products/models/bd/engineering/stg/stg_legacy_kumitatename_gijutsusms.sql
@@ -0,0 +1,24 @@
+{{
+    config (
+        materialized = 'incremental',
+        incremental_strategy = 'append'
+    )
+}}
+
+with stg_legacy_kumitatename_gijutsusms as (
+    select
+        syasyu::varchar(4) as syasyu_cd,
+        kumitate::varchar(4) as kumitate,
+        kumimei::varchar(25) as kumimei,
+        mttime::varchar(16) as mttime,
+        _fivetran_synced::timestamp_ntz as ldts --timestamp型
+    from {{ source('fivetran_database_idr_gijutsu_sms_dxpfy2d', 'raw_cyl15kumimei') }}
+    where _fivetran_deleted = 'false'
+
+    {% if is_incremental() %}
+        and ldts > (select max(ldts) from {{ this }})
+    {% endif %}
+
+)
+select *
+from stg_legacy_kumitatename_gijutsusms
---
+++ enterprise_data_products/models/dd/engineering/stg/stg_legacy_kumitatename_tmc_gijutsusms.sql
@@ -0,0 +1,15 @@
+with stg_legacy_kumitatename_gijutsusms  as (
+    select
+        *
+    from {{ ref('stg_legacy_kumitatename_gijutsusms') }}
+),
+stg_syasyu_tmc_sms as (
+    select
+        syasyu
+    from {{ ref('stg_syasyu_tmc_sms') }}
+)
+select
+    stg_legacy_kumitatename_gijutsusms.*
+from stg_legacy_kumitatename_gijutsusms
+inner join stg_syasyu_tmc_sms
+    on stg_legacy_kumitatename_gijutsusms.syasyu_cd = stg_syasyu_tmc_sms.syasyu
