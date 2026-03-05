a092ddf16e10f5333ec249114229c6c21ccc36dd
kazuki_ohta_zb <da-dxpf-kazuki_ohta_zb@mail.toyota.co.jp>
2026-03-05 10:45:29 +0900

    マトリックス情報資産反映

---
+++ enterprise_data_products/models/bd/engineering/stg/stg_kmatoinfo_gijutsusms.sql
@@ -0,0 +1,14 @@
+with stg_kmatoinfo_gijutsusms as (
+    select
+        syasyu::varchar(4) as syasyu,
+        mato::varchar(7) as mato,
+        motomato::varchar(7) as motomato,
+        motocl::varchar(3) as motocl,
+        kihaisino::varchar(10) as kihaisino,
+        seppenno::varchar(10) as seppenno,
+        _fivetran_synced::timestamp_ntz as ldts --timestamp型
+    from {{ source('fivetran_database_idr_gijutsu_sms_dxpfy2d', 'raw_cy727kmatoinfo') }}
+    where _fivetran_deleted = 'false'
+)
+select *
+from stg_kmatoinfo_gijutsusms
