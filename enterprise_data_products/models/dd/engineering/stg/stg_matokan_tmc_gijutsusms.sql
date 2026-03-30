33568c4549863cce0df40d2a7c60ca5c7422d8f0
kazuki_ohta_zb <da-dxpf-kazuki_ohta_zb@mail.toyota.co.jp>
2026-03-30 15:55:26 +0900

    管理資産反映

--- enterprise_data_products/models/dd/engineering/stg/stg_matokan_tmc_gijutsusms.sql
+++ enterprise_data_products/models/dd/engineering/stg/stg_matokan_tmc_gijutsusms.sql
@@ -1,16 +1,16 @@
 with
-    matokan as (
+    stg_matokan_gijutsusms as (
         select
             *
         from {{ ref('stg_matokan_gijutsusms') }}
     ),
-    syasyu_tmc as (
+    stg_syasyu_tmc_sms as (
         select
             syasyu
         from {{ ref('stg_syasyu_tmc_sms')S }}
     )
 select
-    matoSkan.*
-from matokan
-inner join syasyu_tmc
-    on matokan.syasyu = syasyu_tmc.syasyu
+    stg_matokan_gijutsusms.*
+from stg_matokan_gijutsusms
+inner join stg_syasyu_tmc_sms
+    on stg_matokan_gijutsusms.syasyu = stg_syasyu_tmc_sms.syasyu
