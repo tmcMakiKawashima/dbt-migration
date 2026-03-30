d5f788c5dd657a8661418becc54019842e78c75d
yuta_terada_zb <yuta_terada_zb@mail.toyota.co.jp>
2026-03-30 13:47:38 +0900

    空白行を修正

--- enterprise_data_products/models/bd/engineering/stg/stg_matrix_gijutsusms.sql
+++ enterprise_data_products/models/bd/engineering/stg/stg_matrix_gijutsusms.sql
@@ -9,9 +9,7 @@ with
             clmade::varchar(3) as clmade,
             kumikekka::varchar(1089) as kumikekka,
             _fivetran_synced::timestamp_ntz as ldts --timestamp型
-
         from {{ source('fivetran_database_idr_gijutsu_sms_dxpfy2d', 'raw_cy727matrix') }}
-        where
-            _fivetran_deleted = 'false'
+        where _fivetran_deleted = 'false'
     )
 select * from stg_matrix_gijutsusms
